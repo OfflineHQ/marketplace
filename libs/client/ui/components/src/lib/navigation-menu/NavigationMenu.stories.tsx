6f3fba98528957a52826a5e08a71b1eae24f7430
sebpalluel <sebpalluel@gmail.com>
2023-03-24 16:09:45 +0100

    fix tailwind config for icons + new component NavigationMenu

---
+++ libs/client/ui/components/src/lib/navigation-menu/NavigationMenu.stories.tsx
@@ -0,0 +1,45 @@
+// NavigationMenu.stories.tsx
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect } from '@storybook/jest';
+import {
+  fireEvent,
+  screen,
+  userEvent,
+  waitForElementToBeRemoved,
+} from '@storybook/testing-library';
+import { NavigationMenu } from './NavigationMenu';
+import { NavigationMenuDemo } from './examples';
+
+const meta = {
+  component: NavigationMenu,
+  title: 'Organisms/NavigationMenu',
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof NavigationMenu>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const DefaultNavigationMenu: Story = {
+  render: () => <NavigationMenuDemo />,
+  play: async () => {
+    const gettingStartedTrigger = await screen.findByRole('button', {
+      name: 'Getting started',
+    });
+    await userEvent.click(gettingStartedTrigger);
+    const gettingStartedContent = await screen.findByText('Introduction');
+    console.log({ gettingStartedContent });
+    expect(gettingStartedContent).toBeVisible();
+
+    const componentsTrigger = await screen.findByRole('button', {
+      name: 'Components',
+    });
+    await userEvent.click(componentsTrigger);
+    await waitForElementToBeRemoved(() => screen.queryByText('Introduction'));
+    const componentsContent = await screen.findByText('Hover Card');
+    console.log({ componentsContent });
+    expect(componentsContent).toBeVisible();
+  },
+};
--- libs/client/ui/components/tailwind.config.js
+++ libs/client/ui/components/tailwind.config.js
@@ -6,6 +6,6 @@ const baseConfig = require('../../../../tailwind.config.base');
  * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
  */
 module.exports = {
-  content: [join(__dirname, 'src/**/!(*.spec).{ts,tsx}')],
+  content: [join(__dirname, '../../ui/**/!(*.spec).{ts,tsx}')],
   ...baseConfig,
 };
