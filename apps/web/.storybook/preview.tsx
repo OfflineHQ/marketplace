38ca59682d29162f46e8c673405b51a9b8ee7a82
sebpalluel <sebpalluel@gmail.com>
2023-04-13 16:55:30 +0100

    🔥 chore(storybook): remove unused global css file 🔧 chore(storybook): update tsconfig.json to include additional types for testing libraries

--- apps/web/.storybook/preview.tsx
+++ apps/web/.storybook/preview.tsx
@@ -1,4 +1,4 @@
-import './globals.css';
+import '../styles/globals.css';
 import { Inter } from '@next/font/google';
 import { Preview } from '@storybook/react';
 import { parameters } from '../../../storybook.preview.base';
