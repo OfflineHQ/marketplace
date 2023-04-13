7f5d64682f4bad10a190d9de71f1ac975202fb21
sebpalluel <sebpalluel@gmail.com>
2023-04-13 16:56:13 +0100

    🚀 chore(ui): add types to storybook tsconfig.json 🚀 chore(test): import @testing-library/jest-dom/extend-expect in jest.setup.ts

--- tools/test/jest.setup.ts
+++ tools/test/jest.setup.ts
@@ -1,3 +1,4 @@
 global.fetch = require('node-fetch');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: './tools/test/.env.test.jest' });
+import '@testing-library/jest-dom/extend-expect';
