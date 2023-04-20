8a846d8bfda9179708ec90ef1bc94eeb9ffd8727
sebpalluel <sebpalluel@gmail.com>
2023-04-20 15:56:57 +0100

    🎨 style(page.tsx): add underline style to h1 element ✨ feat(page.tsx): add server button to page

--- apps/web/app/[lng]/page.tsx
+++ apps/web/app/[lng]/page.tsx
@@ -1,14 +1,16 @@
 import Link from 'next/link';
 import { useTranslation } from '../i18n';
 import Footer from './components/Footer';
+import { Button } from '@ui/components';
 
 export default async function Page({ params: { lng } }) {
   const { t } = await useTranslation(lng);
   return (
     <>
-      <h1>Hi there!</h1>
+      <h1 className="text-lg underline">Hi there!</h1>
       <Link href={`/${lng}/second-page`}> {t('to-second-page')}</Link>
       <Link href={`/${lng}/client-page`}>{t('to-client-page')}</Link>
+      <Button>Server Button</Button>
       <Footer lng={lng} />
     </>
   );
