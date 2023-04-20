376599c7583ca32ef9263d22123addef29a02c8d
sebpalluel <sebpalluel@gmail.com>
2023-04-20 16:54:02 +0100

    🚀 feat(page.tsx): add Text component from @ui/components 🔥 chore(ui/components): remove .babelrc 🔧 chore(ui/components): add .swcrc 🔧 chore(ui/components): add @swc/helpers to dependencies 🔧 chore(ui/components): update project.json to use @swc/helpers and .swcrc

--- apps/web/app/[lng]/page.tsx
+++ apps/web/app/[lng]/page.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
 import { useTranslation } from '../i18n';
 import Footer from './components/Footer';
-import { Button } from '@ui/components';
+import { Button, Text } from '@ui/components';
 
 export default async function Page({ params: { lng } }) {
   const { t } = await useTranslation(lng);
   return (
     <>
-      <h1 className="text-lg underline">Hi there!</h1>
+      <Text variant="h1">Hi there!</Text>
       <Link href={`/${lng}/second-page`}> {t('to-second-page')}</Link>
       <Link href={`/${lng}/client-page`}>{t('to-client-page')}</Link>
       <Button>Server Button</Button>
