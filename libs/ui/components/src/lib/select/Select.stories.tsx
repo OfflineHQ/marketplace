6ee7903ee73e7ed78f9506f63a2b59d32a886a07
sebpalluel <sebpalluel@gmail.com>
2023-04-21 18:40:00 +0100

    🎨 style(Select.stories.tsx): move SelectDemo to a separate file and import it ✨ feat(Select.stories.tsx): add render prop to Meta to use SelectDemo component in storybook 🚚 chore(Select.stories.tsx): remove render prop from Default story 🚚 chore(SelectedItem): remove render prop from story

--- libs/ui/components/src/lib/select/Select.stories.tsx
+++ libs/ui/components/src/lib/select/Select.stories.tsx
@@ -2,72 +2,23 @@ import { expect } from '@storybook/jest';
 // Select.stories.tsx
 import { Meta, StoryObj } from '@storybook/react';
 import { screen, userEvent, within } from '@storybook/testing-library';
+import { SelectDemo } from './examples';
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectSeparator,
-  SelectTrigger,
-  SelectValue,
-} from './Select';
+import { Select } from './Select';
 
 const meta = {
   title: 'Molecules/Select',
   component: Select,
+  render: SelectDemo,
 } satisfies Meta<typeof Select>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-function SelectDemo() {
-  return (
-    <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-        <SelectSeparator />
-        <SelectGroup>
-          <SelectLabel>Vegetables</SelectLabel>
-          <SelectItem value="aubergine">Aubergine</SelectItem>
-          <SelectItem value="broccoli">Broccoli</SelectItem>
-          <SelectItem value="carrot" disabled>
-            Carrot
-          </SelectItem>
-          <SelectItem value="courgette">Courgette</SelectItem>
-          <SelectItem value="leek">Leek</SelectItem>
-        </SelectGroup>
-        <SelectSeparator />
-        <SelectGroup>
-          <SelectLabel>Meat</SelectLabel>
-          <SelectItem value="beef">Beef</SelectItem>
-          <SelectItem value="chicken">Chicken</SelectItem>
-          <SelectItem value="lamb">Lamb</SelectItem>
-          <SelectItem value="pork">Pork</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  );
-}
-
-export const Default: Story = {
-  render: () => <SelectDemo />,
-};
+export const Default: Story = {};
 
 export const SelectedItem: Story = {
-  render: () => <SelectDemo />,
   play: async () => {
     await userEvent.click(screen.getByRole('combobox'));
     const optionList = await screen.findByRole('listbox');
