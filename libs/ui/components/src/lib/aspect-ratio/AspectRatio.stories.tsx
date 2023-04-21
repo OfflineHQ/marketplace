7b4bb9c2573444f35eeb61844131fcbf3bce7d23
sebpalluel <sebpalluel@gmail.com>
2023-04-21 18:39:17 +0100

    🎨 style(Logo.tsx): remove eslint-disable-next-line comment 🎨 style(AspectRatio.stories.tsx): change class name from 'ma-5' to 'm-5'

--- libs/ui/components/src/lib/aspect-ratio/AspectRatio.stories.tsx
+++ libs/ui/components/src/lib/aspect-ratio/AspectRatio.stories.tsx
@@ -4,7 +4,7 @@ import { AspectRatio } from './AspectRatio';
 const variantOptions = ['square', 'classic', 'widescreen', 'ultrawide'];
 
 const renderImage = (args: any) => (
-  <AspectRatio {...args} className="ma-5">
+  <AspectRatio {...args} className="m-5">
     <img
       src="https://images.unsplash.com/photo-1576075796033-848c2a5f3696?w=800&dpr=2&q=80"
       alt="Photo by Alvaro Pinot"
