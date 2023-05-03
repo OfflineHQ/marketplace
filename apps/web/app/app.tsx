aa4972b8252c2e01053fa260b142426c75b78829
sebpalluel <sebpalluel@gmail.com>
2023-05-03 17:26:48 +0200

    🚀 feat(app.tsx): create App component to wrap the application and add AuthProvider and Toaster components 🔄 refactor(layout.tsx): replace Header component with App component in RootLayout component

---
+++ apps/web/app/app.tsx
@@ -0,0 +1,19 @@
+import Header from '@web/components/header/Header';
+import { Toaster } from '@ui/components';
+import { AuthProvider } from '@web/lib/authProvider';
+
+interface AppProps {
+  children: React.ReactNode;
+}
+
+export default function App({ children }: AppProps) {
+  return (
+    <AuthProvider>
+      <div className="relative flex min-h-screen flex-col">
+        <Header />
+        <div className="flex-1">{children}</div>
+      </div>
+      <Toaster />
+    </AuthProvider>
+  );
+}
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,10 +7,7 @@ import { siteConfig } from '@web/config/site';
 import { Analytics } from '@web/components/Analytics';
 import { ThemeProvider } from '@ui/theme';
 import { cn } from '@ui/shared';
-import { Toaster } from '@ui/components';
-import { WagmiProvider, AuthProvider } from '@web/lib/providers';
-
-import Header from '@web/components/header/Header';
+import App from './app';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -81,22 +78,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <WagmiProvider>
-            <AuthProvider>
-              {/* <SSXProvider> */}
-              <div className="relative flex min-h-screen flex-col">
-                {/* <Header /> */}
-                {/* <SiteHeader /> */}
-                <div className="flex-1">{children}</div>
-                {/* <SiteFooter /> */}
-              </div>
-              {/* <TailwindIndicator /> */}
-              {/* </SSXProvider> */}
-            </AuthProvider>
-          </WagmiProvider>
+          <App>{children}</App>
         </ThemeProvider>
         <Analytics />
-        <Toaster />
       </body>
     </html>
   );
