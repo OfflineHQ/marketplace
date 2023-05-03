edec2591a095ec1b23007d18f1cc78619da690cc
sebpalluel <sebpalluel@gmail.com>
2023-05-03 18:43:35 +0200

    🔧 chore(next.config.js): remove webpackBuildWorker option 🔒 chore(SiweProvider): add Sentry error tracking to authorize function

--- libs/client/siwe/provider/src/index.ts
+++ libs/client/siwe/provider/src/index.ts
@@ -1,9 +1,9 @@
-// https://docs.login.xyz/integrations/nextauth.js
 import { getNextAppURL } from '@client/next-auth/common';
 import { logger } from '@logger';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { getCsrfToken } from 'next-auth/react';
 import { SiweMessage } from 'siwe';
+import * as Sentry from '@sentry/nextjs';
 
 export const SiweProvider = () =>
   CredentialsProvider({
@@ -21,24 +21,29 @@ export const SiweProvider = () =>
       },
     },
     async authorize(credentials, req) {
-      logger.debug({ credentials, req });
-      const siwe = new SiweMessage(JSON.parse(credentials?.message || '{}'));
-      const nextAuthUrl = new URL(getNextAppURL());
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const nonce = await getCsrfToken({ req: { headers: req.headers } });
-      logger.info({ nonce });
-      const result = await siwe.verify({
-        signature: credentials?.signature || '',
-        domain: nextAuthUrl.host,
-        nonce,
-      });
-      console.log({ result, siwe });
-      if (result.success) {
-        return {
-          id: siwe.address,
-          address: siwe.address,
-          chainId: siwe.chainId,
-        };
-      } else throw new Error('Invalid signature');
+      try {
+        logger.debug({ credentials, req });
+        const siwe = new SiweMessage(JSON.parse(credentials?.message || '{}'));
+        const nextAuthUrl = new URL(getNextAppURL());
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const nonce = await getCsrfToken({ req: { headers: req.headers } });
+        logger.info({ nonce });
+        const result = await siwe.verify({
+          signature: credentials?.signature || '',
+          domain: nextAuthUrl.host,
+          nonce,
+        });
+        console.log({ result, siwe });
+        if (result.success) {
+          return {
+            id: siwe.address,
+            address: siwe.address,
+            chainId: siwe.chainId,
+          };
+        } else throw new Error('Invalid signature');
+      } catch (error) {
+        Sentry.captureException(error);
+        console.error({ error });
+      }
     },
   });
