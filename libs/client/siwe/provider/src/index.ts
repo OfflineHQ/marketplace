b64a919101fb3204ff02ea438622e23237a74d47
sebpalluel <sebpalluel@gmail.com>
2023-05-03 17:26:04 +0200

    🚸 chore(safeAuthSetup.ts): add TODO comment to handle error and show toast 🐛 fix(siwe/provider): throw error when signature is invalid instead of returning null

--- libs/client/siwe/provider/src/index.ts
+++ libs/client/siwe/provider/src/index.ts
@@ -21,29 +21,24 @@ export const SiweProvider = () =>
       },
     },
     async authorize(credentials, req) {
-      try {
-        logger.debug({ credentials, req });
-        const siwe = new SiweMessage(JSON.parse(credentials?.message || '{}'));
-        const nextAuthUrl = new URL(getNextAppURL());
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const nonce = await getCsrfToken({ req: { headers: req.headers } });
-        logger.info({ nonce });
-        const result = await siwe.verify({
-          signature: credentials?.signature || '',
-          domain: nextAuthUrl.host,
-          nonce,
-        });
-        console.log({ result, siwe });
-        if (result.success) {
-          return {
-            id: siwe.address,
-            address: siwe.address,
-            chainId: siwe.chainId,
-          };
-        }
-        return null;
-      } catch (e) {
-        return null;
-      }
+      logger.debug({ credentials, req });
+      const siwe = new SiweMessage(JSON.parse(credentials?.message || '{}'));
+      const nextAuthUrl = new URL(getNextAppURL());
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const nonce = await getCsrfToken({ req: { headers: req.headers } });
+      logger.info({ nonce });
+      const result = await siwe.verify({
+        signature: credentials?.signature || '',
+        domain: nextAuthUrl.host,
+        nonce,
+      });
+      console.log({ result, siwe });
+      if (result.success) {
+        return {
+          id: siwe.address,
+          address: siwe.address,
+          chainId: siwe.chainId,
+        };
+      } else throw new Error('Invalid signature');
     },
   });
