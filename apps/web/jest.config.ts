89a5ca08b7e18b88a85ab4bebb5dcf26cc7282be
sebpalluel <sebpalluel@gmail.com>
2023-05-09 12:36:46 +0200

    🚀 feat(HeaderClient.tsx): export Header component 🚨 chore(jest.config.ts): comment out unused transform preset 🚨 chore(jest.setup.ts): remove unused mock of AutoAnimate component 🧪 test(HeaderClient.spec.tsx): add tests for Header component

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -3,7 +3,8 @@ export default {
   displayName: 'web',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.(t|j)sx?$': [
+    // '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(js|ts|tsx)?$': [
       '@swc/jest',
       {
         jsc: {
@@ -19,6 +20,7 @@ export default {
   moduleNameMapper: {
     '@formkit/auto-animate/react':
       '<rootDir>/../../__mocks__/@formkit/auto-animate/react.js',
+    'auto-animate': '<rootDir>/../../__mocks__/auto-animate.tsx',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/web',
