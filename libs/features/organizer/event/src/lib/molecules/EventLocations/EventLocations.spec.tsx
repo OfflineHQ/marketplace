f9b59e401b8a3003ec5d1586834e920ec60f9faf
sebpalluel <sebpalluel@gmail.com>
2023-05-17 14:32:47 +0100

    🚀 chore(vscode): add bradlc.vscode-tailwindcss extension to improve development experience ✨ feat(EventLocations): add support for detailed and one location views, add button component to improve accessibility The bradlc.vscode-tailwindcss extension was added to improve the development experience. The EventLocations component now supports detailed and one location views, which can be toggled via the `detailed` prop. A button component was added to improve accessibility and replace the previous `p` elements.

--- libs/features/organizer/event/src/lib/molecules/EventLocations/EventLocations.spec.tsx
+++ libs/features/organizer/event/src/lib/molecules/EventLocations/EventLocations.spec.tsx
@@ -15,7 +15,7 @@ Object.defineProperty(window, 'open', {
   },
 });
 
-const { Default } = composeStories(stories);
+const { Default, Detailed, OneLocation } = composeStories(stories);
 
 describe('EventLocations', () => {
   test('renders EventLocations with initial values', () => {
@@ -27,6 +27,41 @@ describe('EventLocations', () => {
   test('location element opens Google Maps URL on click', () => {
     render(<Default />);
     const locationElement = screen.getByText(/New York, USA/i);
+    fireEvent.click(locationElement);
+    expect(url).toEqual(
+      'https://www.google.com/maps/search/?api=1&query=Google&query_place_id=ChIJd8BlQ2BZwokRAFUEcm_qrcA'
+    );
+    url = ''; // Reset the url for the next test
+  });
+
+  test('renders Detailed EventLocations', () => {
+    render(<Detailed />);
+    const locationElements = screen.getAllByText(/New York, NY/i);
+    expect(locationElements).toHaveLength(2); // Should render two location elements
+  });
+
+  test('Detailed location elements open Google Maps URL on click', () => {
+    render(<Detailed />);
+    const locationElements = screen.getAllByText(/New York, NY/i);
+    fireEvent.click(locationElements[0]);
+    expect(url).toEqual(
+      'https://www.google.com/maps/search/?api=1&query=Google&query_place_id=ChIJd8BlQ2BZwokRAFUEcm_qrcA'
+    );
+    url = ''; // Reset the url for the next test
+    fireEvent.click(locationElements[1]);
+    expect(url).toEqual(
+      'https://www.google.com/maps/search/?api=1&query=Google&query_place_id=ChIJhRwB-yFawokRv_x-4eLe3WI'
+    );
+  });
+
+  test('renders OneLocation EventLocations', () => {
+    render(<OneLocation />);
+    screen.getByText(/New York, NY/i);
+  });
+
+  test('OneLocation location element opens Google Maps URL on click', () => {
+    render(<OneLocation />);
+    const locationElement = screen.getByText(/New York, NY/i);
     fireEvent.click(locationElement);
     expect(url).toEqual(
       'https://www.google.com/maps/search/?api=1&query=Google&query_place_id=ChIJd8BlQ2BZwokRAFUEcm_qrcA'
