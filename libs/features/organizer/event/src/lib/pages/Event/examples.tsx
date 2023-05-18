0cb1ee41918dd50b49c37c6f26b54432cb411717
sebpalluel <sebpalluel@gmail.com>
2023-05-18 14:53:10 +0100

    🎨 style(EventDetails.stories.tsx): add a new story for EventDetails with a long description 🎨 style(EventDetails.examples.tsx): add a long description variable to be used in the new story 🎨 style(Event.examples.tsx): update the eventProps to use the long description variable The new story for EventDetails now has a long description to test the component's ability to handle large amounts of text. The long description variable was added to the examples file to be used in the new story. The eventProps in the Event examples file were updated to use the long description variable instead of the previous short description.

--- libs/features/organizer/event/src/lib/pages/Event/examples.tsx
+++ libs/features/organizer/event/src/lib/pages/Event/examples.tsx
@@ -1,10 +1,14 @@
 import { eventHeroProps } from '../../molecules/EventHero/examples';
-import { eventDetailsProps } from '../../molecules/EventDetails/examples';
+import {
+  eventDetailsProps,
+  long_description,
+} from '../../molecules/EventDetails/examples';
 import { type EventProps } from './Event';
 
 export const eventProps: EventProps = {
   ...eventHeroProps,
   ...eventDetailsProps,
+  description: long_description,
   buyFunction: () => null,
   buyText: 'Select Passes',
 };
