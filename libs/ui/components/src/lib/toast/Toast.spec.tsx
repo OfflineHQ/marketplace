55d059ee44a0248afed28e9c3f4e4a863b1f2f4f
sebpalluel <sebpalluel@gmail.com>
2023-05-24 12:49:30 +0100

    ✅ test(Toast.spec.tsx): add test to check if Toast component is closable by adding a close button and clicking it. Also, refactor test to use TextClose story instead of Toast component directly.

--- libs/ui/components/src/lib/toast/Toast.spec.tsx
+++ libs/ui/components/src/lib/toast/Toast.spec.tsx
@@ -1,10 +1,14 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { composeStories } from '@storybook/react';
+import * as stories from './Toast.stories';
 
-import { Toast } from './Toast';
+const { TextClose } = composeStories(stories);
 
 describe('Toast', () => {
-  it.skip('should render successfully', () => {
-    const { baseElement } = render(<Toast />);
+  it('should render successfully and be closable', () => {
+    const { baseElement } = render(<TextClose />);
     expect(baseElement).toBeTruthy();
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
   });
 });
