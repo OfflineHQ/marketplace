c53847e0f9cb569cd3f5e83ee123ca73224aa2ca
sebpalluel <sebpalluel@gmail.com>
2023-06-02 12:18:38 +0100

    🚀 chore(AppNavLayout.stories.tsx): rename props to improve semantics 🚀 chore(NoUserCart/examples.tsx): add NextAuthProvider to wrap AuthProvider 🚀 chore(NoUserPass/examples.tsx): add NextAuthProvider to wrap AuthProvider 🚀 chore(Settings.stories.tsx): add fullscreen layout parameter to meta data

--- libs/features/pass/src/lib/NoUserPass/examples.tsx
+++ libs/features/pass/src/lib/NoUserPass/examples.tsx
@@ -1,12 +1,14 @@
-import { AuthProvider } from '@client/auth';
+import { AuthProvider, NextAuthProvider } from '@client/auth';
 import { WithNoUser, WithNormalUser } from '@features/appNav/ui/stories';
 import { AppNavLayout } from '@features/appNav/ui';
 import { NoUserPass } from './NoUserPass';
 
 export function NoUserPassExample() {
   return (
-    <AuthProvider>
-      <AppNavLayout {...WithNoUser.args} children={<NoUserPass />} />{' '}
-    </AuthProvider>
+    <NextAuthProvider>
+      <AuthProvider>
+        <AppNavLayout {...WithNoUser.args} children={<NoUserPass />} />{' '}
+      </AuthProvider>
+    </NextAuthProvider>
   );
 }
