7a63b1cee1b7589251e15daa73cc3d23c0c161d7
sebpalluel <sebpalluel@gmail.com>
2023-06-05 13:20:40 +0100

    🔨 refactor(utils): extract deepPick and deepMerge functions from index.ts to improve code organization and reusability
    🔨 chore(types): add NestedValueOf, NestedKeyOf, NamespaceKeys and MessageKeys types to improve type safety
    📈 chore(package.json): update remeda package to version 1.19.0 to get the latest features and bug fixes

--- libs/utils/src/types/index.ts
+++ libs/utils/src/types/index.ts
@@ -1,3 +1,34 @@
 export type Maybe<T> = T | null | undefined;
 
 export type Merge<P, T> = Omit<P, keyof T> & T;
+
+export type NestedValueOf<
+  ObjectType,
+  Property extends string
+> = Property extends `${infer Key}.${infer Rest}`
+  ? Key extends keyof ObjectType
+    ? NestedValueOf<ObjectType[Key], Rest>
+    : never
+  : Property extends keyof ObjectType
+  ? ObjectType[Property]
+  : never;
+
+export type NestedKeyOf<ObjectType> = ObjectType extends object
+  ? {
+      [Key in keyof ObjectType]:
+        | `${Key & string}`
+        | `${Key & string}.${NestedKeyOf<ObjectType[Key]>}`;
+    }[keyof ObjectType]
+  : never;
+
+export type NamespaceKeys<ObjectType, Keys extends string> = {
+  [Property in Keys]: NestedValueOf<ObjectType, Property> extends string
+    ? never
+    : Property;
+}[Keys];
+
+export type MessageKeys<ObjectType, Keys extends string> = {
+  [Property in Keys]: NestedValueOf<ObjectType, Property> extends string
+    ? Property
+    : never;
+}[Keys];
