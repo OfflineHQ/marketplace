ef75d6a8f6e38e97eb0ba93a4bc99fc88b3d7725
sebpalluel <sebpalluel@gmail.com>
2023-06-09 11:07:50 +0100

    🎨 style(LocalPassList.tsx): add TODO comment to improve UX by adding image or animation when cart is empty 🚀 refactor(NoUserCart.tsx): extract LocalPassList component to reuse it in NoUserCart component 🚀 refactor(NoUserCart.tsx): make NoUserCart component accept LocalPassListProps to pass them to LocalPassList component 🚀 refactor(examples.tsx): use FakeEventPassesServer as a prop for NoUserCart component instead of passing it to LocalPassList component 🚀 feat(LocalPassList.tsx): add type definition for LocalPassListProps 🚀 feat(examples.tsx): add SetupPassesCartLocal, FakeEventPassesServer, and FakeEventPassesServerLoading components to be used in tests
    🔧 chore(tsconfig.base.json): remove unnecessary whitespaces and align lib paths to improve readability

--- libs/features/cart/src/lib/NoUserCart/examples.tsx
+++ libs/features/cart/src/lib/NoUserCart/examples.tsx
@@ -2,12 +2,19 @@ import { AuthProvider, NextAuthProvider } from '@client/auth';
 import { WithNoUser, WithNormalUser } from '@features/appNav/ui/stories';
 import { AppNavLayout } from '@features/appNav/ui';
 import { NoUserCart } from './NoUserCart';
+import {
+  SetupPassesCartLocal,
+  FakeEventPassesServer,
+  FakeEventPassesServerLoading,
+} from '../LocalPassList/examples';
 
 export function NoUserCartExample() {
   return (
     <NextAuthProvider>
       <AuthProvider>
-        <AppNavLayout {...WithNoUser.args} children={<NoUserCart />} />{' '}
+        <AppNavLayout {...WithNoUser.args}>
+          <NoUserCart EventPassServer={FakeEventPassesServer} />
+        </AppNavLayout>
       </AuthProvider>
     </NextAuthProvider>
   );
