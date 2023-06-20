f5ee0d659f484f913146a369bb43097f68011358
sebpalluel <sebpalluel@gmail.com>
2023-06-20 14:49:04 +0100

    🚚 chore(siweProvider.ts): replace handleUser with handleAccount function to reflect the correct feature being handled 🔧 refactor(siweProvider.ts): remove unused imports and variables to improve code quality and readability 🔧 test(getAccount.spec.ts): add unit tests for getAccount function 🔧 test(handleAccount.spec.ts): add unit tests for handleAccount function
    🔧 chore(tsconfig.base.json): reformat file to improve readability and maintainability

---
+++ libs/features/account/api/src/lib/handleAccount.spec.ts
@@ -0,0 +1,62 @@
+import { getAccount } from './getAccount';
+import { createAccount } from './createAccount';
+import { handleAccount } from './handleAccount';
+
+jest.mock('./getAccount');
+jest.mock('./createAccount');
+
+describe('handleAccount test', () => {
+  const account = {
+    id: 'some-unique-id',
+    address: '0x123',
+    email: 'test@safsaf.safsaf',
+    emailVerified: false,
+  };
+
+  afterEach(async () => {
+    // Clean up after each test
+    jest.resetAllMocks();
+  });
+
+  it('should create account when it does not exist', async () => {
+    (
+      getAccount as jest.MockedFunction<typeof getAccount>
+    ).mockResolvedValueOnce(null);
+    (
+      createAccount as jest.MockedFunction<typeof createAccount>
+    ).mockResolvedValueOnce(account);
+    const result = await handleAccount(account);
+    expect(result).toEqual(account);
+    expect(createAccount).toHaveBeenCalledTimes(1);
+    expect(createAccount).toHaveBeenCalledWith(account);
+  });
+
+  it('should return existing account without creating new one', async () => {
+    (
+      getAccount as jest.MockedFunction<typeof getAccount>
+    ).mockResolvedValueOnce(account);
+    const result = await handleAccount(account);
+    expect(result).toEqual(account);
+    expect(createAccount).not.toHaveBeenCalled();
+  });
+
+  it('should throw error when getAccount throws error', async () => {
+    const error = new Error('Failed to get account');
+    (
+      getAccount as jest.MockedFunction<typeof getAccount>
+    ).mockRejectedValueOnce(error);
+    await expect(handleAccount(account)).rejects.toThrow(error);
+    expect(createAccount).not.toHaveBeenCalled();
+  });
+
+  it('should throw error when createAccount throws error', async () => {
+    const error = new Error('Failed to create account');
+    (
+      getAccount as jest.MockedFunction<typeof getAccount>
+    ).mockResolvedValueOnce(null);
+    (
+      createAccount as jest.MockedFunction<typeof createAccount>
+    ).mockRejectedValueOnce(error);
+    await expect(handleAccount(account)).rejects.toThrow(error);
+  });
+});
