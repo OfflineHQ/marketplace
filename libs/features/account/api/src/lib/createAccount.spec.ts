6cbb88e9acd5e5d60a06de79b6ce482583680008
sebpalluel <sebpalluel@gmail.com>
2023-06-20 14:30:25 +0100

    🔨 refactor(createAccount.ts): remove try-catch block and error logging to improve code readability 🐛 fix(createAccount.spec.ts): add tests to cover edge cases and improve test coverage 🔨 refactor(server.ts): remove logging of errors when running in Jest to improve test output clarity

--- libs/features/account/api/src/lib/createAccount.spec.ts
+++ libs/features/account/api/src/lib/createAccount.spec.ts
@@ -8,6 +8,7 @@ describe('createAccount test', () => {
   afterAll(async () => {
     await deleteAccounts();
   });
+
   it('should create account', async () => {
     const account = {
       address: '0x123',
@@ -16,6 +17,49 @@ describe('createAccount test', () => {
     const data = await createAccount(account);
     expect(data.address).toEqual(account.address);
     expect(data.email).toEqual(account.email);
+    expect(data.id).toBeDefined();
+  });
+
+  it('should throw an error when address already exists', async () => {
+    const account1 = {
+      address: '0x123',
+      email: 'test1@safsaf.safsaf',
+    };
+    const account2 = {
+      address: '0x123',
+      email: 'test2@safsaf.safsaf',
+    };
+    await createAccount(account1);
+    await expect(createAccount(account2)).rejects.toThrow();
+  });
+  it('should throw an error when address is not provided', async () => {
+    const account = {
+      email: 'test@safsaf.safsaf',
+    };
+    await expect(createAccount(account as any)).rejects.toThrow();
+  });
+  it("shouldn't throw an error when email already exists", async () => {
+    const account1 = {
+      address: '0x123',
+      email: 'test@safsaf.safsaf',
+    };
+    const account2 = {
+      address: '0x124',
+      email: 'test@safsaf.safsaf',
+    };
+    const _account1 = await createAccount(account1);
+    const _account2 = await createAccount(account2);
+    expect(_account1.email).toEqual(account1.email);
+    expect(_account2.email).toEqual(account2.email);
+  });
+
+  it('should create account without email', async () => {
+    const account = {
+      address: '0x123',
+    };
+    const data = await createAccount(account);
+    expect(data.address).toEqual(account.address);
+    expect(data.email).toBeNull();
     expect(data.id).toBeDefined();
   });
 });
--- libs/features/account/api/src/lib/createAccount.ts
+++ libs/features/account/api/src/lib/createAccount.ts
@@ -7,12 +7,7 @@ import {
 export const createAccount = async (
   account: Account_Insert_Input
 ): Promise<CreateAccountMutation['insert_account_one']> => {
-  try {
-    // TODO add segment analytics
-    const data = await adminSdk.CreateAccount({ account });
-    return data?.insert_account_one || null;
-  } catch (error) {
-    console.error({ error });
-    throw error;
-  }
+  // TODO add segment analytics
+  const data = await adminSdk.CreateAccount({ account });
+  return data?.insert_account_one || null;
 };
