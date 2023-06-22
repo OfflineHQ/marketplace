a29f492a33d38ec3e17094cd257a9f0c9648078e
sebpalluel <sebpalluel@gmail.com>
2023-06-22 20:14:30 +0100

    🚀 feat(account): update imports to use types from @next/gql/admin/types to improve type safety and avoid potential errors

--- libs/features/account/api/src/lib/createAccount.ts
+++ libs/features/account/api/src/lib/createAccount.ts
@@ -1,8 +1,8 @@
-import {
-  adminSdk,
-  type Account_Insert_Input,
+import { adminSdk } from '@next/gql/admin';
+import type {
+  Account_Insert_Input,
   CreateAccountMutation,
-} from '@next/gql/admin';
+} from '@next/gql/admin/types';
 
 export const createAccount = async (
   account: Account_Insert_Input
