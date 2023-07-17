0fbb4ec611e6b6f9c8c932445b1546f35b1ec69e
sebpalluel <sebpalluel@gmail.com>
2023-07-17 18:55:06 +0100

    🔧 fix(globals.css): fix text-primary class not being applied to anchor tags on hover, focus, and active states 🔧 fix(up.sql): add conditional check before updating eventPassOrderSums table to handle case when eventPassId doesn't exist in eventPassOrder table 🔧 fix(down.sql): add down migration to drop constraint "eventPassPricing_eventPassId_key" from eventPassPricing table 🔧 fix(up.sql): add up migration to add unique constraint "eventPassPricing_eventPassId_key" to eventPassPricing table 🔧 fix(down.sql): add down migration for check_event_pass_order_quantity function ✨ feat(up.sql): add up migration for check_event_pass_order_quantity function ✨ feat(eventPassPricing.query.gql): add mutation for creating event pass pricing entry
    🔧 chore(db): refactor and improve test-utils/db module
    
    🔧 chore(db): refactor createDbClient function to use a more explicit and configurable Client constructor
    
    🔧 chore(db): refactor createDb function to accept a Client instance as a parameter
    
    🔧 chore(db): refactor clearDb function to accept a Client instance as a parameter
    
    🔧 chore(db): refactor deleteAccounts function to accept a Client instance as a parameter
    
    🔧 chore(db): refactor deleteTables function to accept a Client instance and a table name as parameters
    
    🔧 chore(db): refactor deleteAccount function to accept a Client instance and an email as parameters
    
    🔧 chore(db): refactor seedDb function to accept a Client instance and a file path as parameters
    
    🔧 chore(db): refactor queryDb function to accept a Client instance and an SQL query as parameters

--- hasura/app/migrations/default/1689610201989_check_event_pass_order_quantity/down.sql
+++ hasura/app/migrations/default/1689610201989_check_event_pass_order_quantity/down.sql
@@ -5,23 +5,29 @@
 -- DECLARE
 --   total_quantity INTEGER;
 --   total_quantity_per_user INTEGER;
+--   original_quantity INTEGER;
 --   max_amount INTEGER;
 --   max_amount_per_user INTEGER;
 -- BEGIN
---   -- Calculate the total quantity of non-cancelled eventPassOrders for the eventPassId
+--   -- Get the original quantity of the order being updated
+--   SELECT "quantity" INTO original_quantity
+--   FROM "eventPassOrder"
+--   WHERE "id" = NEW."id";
+--
+--   -- Calculate the total quantity of non-cancelled eventPassOrders for the eventPassId, excluding the order being updated
 --   SELECT SUM("quantity") INTO total_quantity
 --   FROM "eventPassOrder"
---   WHERE "eventPassId" = NEW."eventPassId" AND "status" != 'CANCELLED';
+--   WHERE "eventPassId" = NEW."eventPassId" AND "status" != 'CANCELLED' AND "id" != NEW."id";
 --
---   -- Calculate the total quantity of non-cancelled eventPassOrders for the eventPassId and accountId
+--   -- Calculate the total quantity of non-cancelled eventPassOrders for the eventPassId and accountId, excluding the order being updated
 --   SELECT SUM("quantity") INTO total_quantity_per_user
 --   FROM "eventPassOrder"
---   WHERE "eventPassId" = NEW."eventPassId" AND "accountId" = NEW."accountId" AND "status" != 'CANCELLED';
+--   WHERE "eventPassId" = NEW."eventPassId" AND "accountId" = NEW."accountId" AND "status" != 'CANCELLED' AND "id" != NEW."id";
 --
 --   -- Fetch the maxAmount and maxAmountPerUser for the eventPassId
 --   SELECT "maxAmount", "maxAmountPerUser" INTO max_amount, max_amount_per_user
 --   FROM "eventPassPricing"
---   WHERE "id" = NEW."eventPassId";
+--   WHERE "eventPassId" = NEW."eventPassId";
 --
 --   -- Check if the total quantity plus the new quantity exceeds the maxAmount or maxAmountPerUser
 --   IF total_quantity + NEW."quantity" > max_amount THEN
@@ -35,13 +41,3 @@
 --   RETURN NEW;
 -- END;
 -- $$ LANGUAGE plpgsql;
---
--- CREATE TRIGGER check_event_pass_order_quantity_insert_trigger
--- BEFORE INSERT ON "eventPassOrder"
--- FOR EACH ROW
--- EXECUTE PROCEDURE check_event_pass_order_quantity();
---
--- CREATE TRIGGER check_event_pass_order_quantity_update_trigger
--- BEFORE UPDATE OF "quantity" ON "eventPassOrder"
--- FOR EACH ROW
--- EXECUTE PROCEDURE check_event_pass_order_quantity();
