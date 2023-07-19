6614a9034a7f5df7d49d4a4e000abe650574ca27
sebpalluel <sebpalluel@gmail.com>
2023-07-19 14:10:38 +0100

    ✨ feat(eventPassPendingOrder.sql): add seed data for eventPassPendingOrder table to populate with initial data ✨ feat(eventPassPendingOrder.query.gql): add eventPassId and address fields to the query to retrieve additional information ✨ feat(eventPassPendingOrder.spec.ts): add tests for retrieving eventPassPendingOrder data for admin and user roles

---
+++ hasura/app/seeds/default/eventPassPendingOrder.sql
@@ -0,0 +1,4 @@
+SET check_function_bodies = false;
+INSERT INTO public."eventPassPendingOrder" (id, "eventPassId", "accountId", quantity, created_at) VALUES ('d4951f86-1a8f-410a-bbc1-607f1c7933b9', 'fake-event-pass-1', '679f92d6-a01e-4ab7-93f8-10840d22b0a5', 2, '2023-07-19 12:55:53.506236+00');
+INSERT INTO public."eventPassPendingOrder" (id, "eventPassId", "accountId", quantity, created_at) VALUES ('c44e9122-7818-47c2-8818-508121c9843d', 'fake-event-pass-2', '679f92d6-a01e-4ab7-93f8-10840d22b0a5', 2, '2023-07-19 12:58:46.636737+00');
+INSERT INTO public."eventPassPendingOrder" (id, "eventPassId", "accountId", quantity, created_at) VALUES ('d3ce4e64-f405-4b98-bcb9-e6b0222ad60a', 'fake-event-pass-1', '76189546-6368-4325-8aad-220e03837b7e', 12, '2023-07-19 12:59:19.072182+00');
