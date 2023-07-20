f88acec6b07c747b34f3c43bf2b5d8889f05f916
sebpalluel <sebpalluel@gmail.com>
2023-07-20 17:16:16 +0100

    🔧 chore(ci.yml): disable parallel execution for test-hasura target to fix test failures 🔧 chore(createAccount.spec.ts): update email address in test case to use a fake domain 🔧 chore(useEventPassOrders.spec.tsx): update test setup and teardown logic to use database functions 🔧 chore(useEventPassOrders.ts): remove console.log statements 🔧 chore(tsconfig.spec.json): add @next/types to types array 🔧 chore(project.json): update entryFile path to .tsx for react-query libraries 🔧 chore(index.tsx): remove logger import and debug statement 🔧 chore(tsconfig.json): add @next/types to types array 🔧 chore(index.tsx): update fetchDataReactQuery function to use window.jwtTestToken instead of useTestJwt 🔧 chore(tsconfig.json): add @next/types to types array 🔧 chore(package.json): update 'all:test-hasura' script to run tests sequentially instead of in parallel for better reliability 🔧 chore(tsconfig.base.json): update file extensions for react-query imports in gql/anonymous and gql/user libraries to '.tsx' for TypeScript compatibility

--- libs/features/cart/src/lib/EventPasses/useEventPassOrders.spec.tsx
+++ libs/features/cart/src/lib/EventPasses/useEventPassOrders.spec.tsx
@@ -3,26 +3,55 @@ import { useEventPassOrders } from './useEventPassOrders';
 import { usersJwt } from '@test-utils/gql';
 import { QueryClientProviderForTest } from '@test-utils/react-query';
 import { Locale } from '@gql/shared/types';
+import {
+  deleteAccounts,
+  createDbClient,
+  deleteTables,
+  seedDb,
+  type PgClient,
+} from '@test-utils/db';
 
+global.window = Object.create(window);
 describe('useEventPassOrders', () => {
+  let client: PgClient;
+  beforeAll(async () => {
+    client = await createDbClient();
+    await deleteAccounts(client);
+    await seedDb(client, './hasura/app/seeds/default/account.sql');
+  });
+  afterAll(async () => {
+    await deleteAccounts(client);
+    await deleteTables(client, '"eventPassPendingOrder", "eventPassPricing"');
+    await client.end();
+  });
+  beforeEach(async () => {
+    await deleteTables(client, '"eventPassPendingOrder", "eventPassPricing"');
+    await seedDb(client, './hasura/app/seeds/default/eventPassPricing.sql');
+    await seedDb(
+      client,
+      './hasura/app/seeds/default/eventPassPendingOrder.sql'
+    );
+  });
   it('should work correctly', async () => {
-    const {
-      result: {
-        current: { eventData, ordersData, upsertOrders, deleteOrders },
-      },
-    } = renderHook(
+    Object.defineProperty(window, 'jwtTestToken', {
+      value: usersJwt.alpha_user,
+    });
+    const { result } = renderHook(
       () =>
         useEventPassOrders({
           organizerSlug: 'test-organizer',
           eventSlug: 'test-event',
           locale: Locale.En,
-          passes: [],
+          passes: [{ id: 'fake-event-pass-1', amount: 1 }],
         }),
-      { wrapper: QueryClientProviderForTest }
+      {
+        wrapper: QueryClientProviderForTest,
+      }
     );
 
     await waitFor(() => {
-      return expect(ordersData).toBeDefined();
+      console.log('checking ordersData:', result.current.ordersData);
+      expect(result.current.ordersData).toBeDefined();
 
       // Add your assertions to test the behavior of the hook
       // ...
