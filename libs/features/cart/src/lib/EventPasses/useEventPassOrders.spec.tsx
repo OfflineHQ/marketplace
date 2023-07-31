7979fa5ce6e408e1787134512333abe71cea5d1c
sebpalluel <sebpalluel@gmail.com>
2023-07-31 16:01:30 +0100

    🐛 fix(EventPassesUser.tsx): remove unused 'localPasses' variable to improve code readability and maintainability ✨ feat(EventPassesUser.tsx): implement optimistic delete functionality to improve user experience and prevent data loss 🐛 fix(useEventPassOrders.spec.tsx): fix import path for 'useGetEventWithPassesQuery' to match actual path ✅ test(useEventPassOrders.spec.tsx): add tests for retrieving existing orders from database and merging with local passes ✅ test(useEventPassOrders.spec.tsx): add tests for updating existing orders with local quantity and inserting new orders ✅ test(useEventPassOrders.spec.tsx): add tests for deleting orders from database and local storage
    🔧 fix(useEventPassOrders.ts): remove unused imports and variables
    🔧 fix(useEventPassOrders.ts): remove unused localPasses prop from useEventPassOrders hook
    🔧 fix(useEventPassOrders.ts): remove console.log statements
    🔧 fix(useEventPassOrders.ts): update deleteOrders function to delete passes from store after mutation
    🔧 fix(index.tsx): remove unused imports and variables

--- libs/features/cart/src/lib/EventPasses/useEventPassOrders.spec.tsx
+++ libs/features/cart/src/lib/EventPasses/useEventPassOrders.spec.tsx
@@ -10,11 +10,27 @@ import {
   seedDb,
   type PgClient,
 } from '@test-utils/db';
+import { act } from 'react-dom/test-utils';
 
-// import { useGetEventWithPassesQuery } from '@gql/user/react-query'; // update this import path as per your actual path
+const mockUpdatePassCart = jest.fn();
+const mockDeletePassesCart = jest.fn();
+jest.mock('@features/organizer/event/store', () => ({
+  usePassPurchaseStore: () => ({
+    updatePassCart: mockUpdatePassCart,
+    deletePassesCart: mockDeletePassesCart,
+  }),
+}));
 
+const mockInsertOrder = jest.fn();
+const mockDeleteOrder = jest.fn();
 jest.mock('@gql/user/react-query', () => ({
   ...jest.requireActual('@gql/user/react-query'),
+  useInsertEventPassPendingOrdersMutation: () => ({
+    mutateAsync: mockInsertOrder,
+  }),
+  useDeleteEventPassPendingOrdersMutation: () => ({
+    mutateAsync: mockDeleteOrder,
+  }),
   useGetEventWithPassesQuery: jest.fn(() => ({
     data: {
       event: {
@@ -77,10 +93,18 @@ describe('useEventPassOrders', () => {
       client,
       './hasura/app/seeds/default/eventPassPendingOrder.sql'
     );
+    mockUpdatePassCart.mockClear();
+    mockDeletePassesCart.mockClear();
+    mockInsertOrder.mockClear();
+    mockDeleteOrder.mockClear();
+  });
+  afterEach(() => {
+    delete window.jwtTestToken;
   });
   it('Should retrieve existing order for an event from database', async () => {
     Object.defineProperty(window, 'jwtTestToken', {
       value: usersJwt.beta_user,
+      configurable: true,
     });
     const { result } = renderHook(
       () =>
@@ -88,7 +112,6 @@ describe('useEventPassOrders', () => {
           organizerSlug: 'test-organizer',
           eventSlug: 'test-event',
           locale: Locale.En,
-          localPasses: [],
         }),
       {
         wrapper: QueryClientProviderForTest,
@@ -107,6 +130,7 @@ describe('useEventPassOrders', () => {
   it('Should retrieve multiple existing order for an event from database', async () => {
     Object.defineProperty(window, 'jwtTestToken', {
       value: usersJwt.alpha_user,
+      configurable: true,
     });
     const { result } = renderHook(
       () =>
@@ -114,7 +138,6 @@ describe('useEventPassOrders', () => {
           organizerSlug: 'test-organizer',
           eventSlug: 'test-event',
           locale: Locale.En,
-          localPasses: [],
         }),
       {
         wrapper: QueryClientProviderForTest,
@@ -125,6 +148,226 @@ describe('useEventPassOrders', () => {
       expect(result.current.eventData).toBeDefined();
       expect(result.current.ordersData).toBeDefined();
       expect(result.current.ordersData?.eventPassPendingOrder.length).toBe(2);
+    });
+  });
+  it("Should retrieve existing orders for an event from database and merge with local passes if it doesn't exist", async () => {
+    Object.defineProperty(window, 'jwtTestToken', {
+      value: usersJwt.beta_user,
+      configurable: true,
+    });
+    const { result } = renderHook(
+      () =>
+        useEventPassOrders({
+          organizerSlug: 'test-organizer',
+          eventSlug: 'test-event',
+          locale: Locale.En,
+        }),
+      {
+        wrapper: QueryClientProviderForTest,
+      }
+    );
+
+    await waitFor(() => {
+      expect(result.current.eventData).toBeDefined();
+      expect(result.current.ordersData).toBeDefined();
+      expect(result.current.ordersData?.eventPassPendingOrder.length).toBe(1);
+    });
+    await act(async () => {
+      await result.current.upsertOrders([]);
+      expect(mockUpdatePassCart).toHaveBeenCalledTimes(1);
+      expect(mockUpdatePassCart).toHaveBeenCalledWith({
+        organizerSlug: 'test-organizer',
+        eventSlug: 'test-event',
+        pass: {
+          id: 'fake-event-pass-1',
+          amount: 12,
+        },
+      });
+    });
+  });
+  it('Should retrieve existing orders for an event from database and update with local quantity if different', async () => {
+    Object.defineProperty(window, 'jwtTestToken', {
+      value: usersJwt.beta_user,
+      configurable: true,
+    });
+    const { result } = renderHook(
+      () =>
+        useEventPassOrders({
+          organizerSlug: 'test-organizer',
+          eventSlug: 'test-event',
+          locale: Locale.En,
+        }),
+      {
+        wrapper: QueryClientProviderForTest,
+      }
+    );
+
+    await waitFor(() => {
+      expect(result.current.eventData).toBeDefined();
+      expect(result.current.ordersData).toBeDefined();
+      expect(result.current.ordersData?.eventPassPendingOrder.length).toBe(1);
+    });
+    await act(async () => {
+      await result.current.upsertOrders([
+        {
+          id: 'fake-event-pass-1',
+          amount: 1,
+        },
+      ]);
+      expect(mockUpdatePassCart).toHaveBeenCalledTimes(0);
+      expect(mockDeleteOrder).toHaveBeenCalledTimes(1);
+      expect(mockDeleteOrder).toHaveBeenCalledWith({
+        eventPassIds: ['fake-event-pass-1'],
+      });
+      expect(mockInsertOrder).toHaveBeenCalledTimes(1);
+      expect(mockInsertOrder).toHaveBeenCalledWith({
+        objects: [
+          {
+            eventPassId: 'fake-event-pass-1',
+            quantity: 1,
+          },
+        ],
+      });
+    });
+  });
+  it('Should insert an order from local cart that is not present in database', async () => {
+    Object.defineProperty(window, 'jwtTestToken', {
+      value: usersJwt.beta_user,
+      configurable: true,
+    });
+    const { result } = renderHook(
+      () =>
+        useEventPassOrders({
+          organizerSlug: 'test-organizer',
+          eventSlug: 'test-event',
+          locale: Locale.En,
+        }),
+      {
+        wrapper: QueryClientProviderForTest,
+      }
+    );
+
+    await waitFor(() => {
+      expect(result.current.eventData).toBeDefined();
+      expect(result.current.ordersData).toBeDefined();
+      expect(result.current.ordersData?.eventPassPendingOrder.length).toBe(1);
+    });
+    await act(async () => {
+      await result.current.upsertOrders([
+        {
+          id: 'fake-event-pass-1',
+          amount: 12,
+        },
+        {
+          id: 'fake-event-pass-2',
+          amount: 1,
+        },
+      ]);
+      expect(mockUpdatePassCart).toHaveBeenCalledTimes(0);
+      expect(mockDeleteOrder).toHaveBeenCalledTimes(0);
+      expect(mockInsertOrder).toHaveBeenCalledTimes(1);
+      expect(mockInsertOrder).toHaveBeenCalledWith({
+        objects: [
+          {
+            eventPassId: 'fake-event-pass-2',
+            quantity: 1,
+          },
+        ],
+      });
+    });
+  });
+  it('Should insert an order form local storage that is not present in database and update the local storage with an order from database that was not present locally', async () => {
+    Object.defineProperty(window, 'jwtTestToken', {
+      value: usersJwt.beta_user,
+      configurable: true,
+    });
+    const { result } = renderHook(
+      () =>
+        useEventPassOrders({
+          organizerSlug: 'test-organizer',
+          eventSlug: 'test-event',
+          locale: Locale.En,
+        }),
+      {
+        wrapper: QueryClientProviderForTest,
+      }
+    );
+
+    await waitFor(() => {
+      expect(result.current.eventData).toBeDefined();
+      expect(result.current.ordersData).toBeDefined();
+      expect(result.current.ordersData?.eventPassPendingOrder.length).toBe(1);
+    });
+    await act(async () => {
+      await result.current.upsertOrders([
+        {
+          id: 'fake-event-pass-2',
+          amount: 1,
+        },
+      ]);
+      expect(mockUpdatePassCart).toHaveBeenCalledTimes(1);
+      expect(mockUpdatePassCart).toHaveBeenCalledWith({
+        organizerSlug: 'test-organizer',
+        eventSlug: 'test-event',
+        pass: {
+          id: 'fake-event-pass-1',
+          amount: 12,
+        },
+      });
+      expect(mockDeleteOrder).toHaveBeenCalledTimes(0);
+      expect(mockInsertOrder).toHaveBeenCalledTimes(1);
+      expect(mockInsertOrder).toHaveBeenCalledWith({
+        objects: [
+          {
+            eventPassId: 'fake-event-pass-2',
+            quantity: 1,
+          },
+        ],
+      });
+    });
+  });
+  it('Should delete orders correctly from database and local storage', async () => {
+    Object.defineProperty(window, 'jwtTestToken', {
+      value: usersJwt.alpha_user,
+      configurable: true,
+    });
+    const { result } = renderHook(
+      () =>
+        useEventPassOrders({
+          organizerSlug: 'test-organizer',
+          eventSlug: 'test-event',
+          locale: Locale.En,
+        }),
+      {
+        wrapper: QueryClientProviderForTest,
+      }
+    );
+
+    await waitFor(() => {
+      expect(result.current.eventData).toBeDefined();
+      expect(result.current.ordersData).toBeDefined();
+      expect(result.current.ordersData?.eventPassPendingOrder.length).toBe(2);
+    });
+    await act(async () => {
+      await result.current.deleteOrders([
+        {
+          id: 'fake-event-pass-1',
+          amount: 12,
+        },
+        {
+          id: 'fake-event-pass-2',
+          amount: 12,
+        },
+      ]);
+      expect(mockDeleteOrder).toHaveBeenCalledTimes(1);
+      expect(mockDeleteOrder).toHaveBeenCalledWith({
+        eventPassIds: ['fake-event-pass-1', 'fake-event-pass-2'],
+      });
+      expect(mockDeletePassesCart).toHaveBeenCalledTimes(1);
+      expect(mockDeletePassesCart).toHaveBeenCalledWith({
+        organizerSlug: 'test-organizer',
+        eventSlug: 'test-event',
+      });
     });
   });
 });
