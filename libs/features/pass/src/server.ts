d2727a416538bf0d968fe87fe9babf162dcb84fc
sebpalluel <sebpalluel@gmail.com>
2023-08-23 16:45:04 +0200

    📦 feat(pass-api): add pass-api library
    📝 docs(pass-api): add README.md
    
    🔧 chore(pass-api): add eslint and swc configuration files
    
    🔧 chore(pass-api): add jest configuration file
    
    🔧 chore(pass-api): add package.json
    
    🔧 chore(pass-api): add project.json
    
    📝 feat(pass-api): add index.ts
    
    📝 feat(pass-api): add common.ts
    
    📝 feat(pass-api): add createNftActivityWebhookForEvent.ts
    
    📝 feat(pass-api): add createSecureStorageForUser.ts
    
    🚀 feat(downloadPass): add downloadPass function to download event passes
    🚀 feat(getEventPassRevealedFilePath): add getEventPassRevealedFilePath function to get the file path of a revealed event pass
    🚀 feat(getSecureStorageForUser): add getSecureStorageForUser function to get the secure storage folder for a user
    🚀 feat(handleSecureStorageForUser): add handleSecureStorageForUser function to handle secure storage for a user
    
    🚀 feat(revealPass.ts): add functionality to check event pass, transfer QR code, and reveal pass
    
    🚧 feat(revealPass.ts): implement eventPassCheck function to check if event pass is owned by user and not already revealed
    
    🚧 feat(revealPass.ts): implement eventPassTransferQRCode function to transfer QR code of event pass to user
    
    🚧 feat(revealPass.ts): implement revealPass function to reveal the event pass
    
    🚧 test(revealPass.spec.ts): add tests for eventPassCheck, eventPassTransferQRCode, and revealPass functions
    
    🚧 test(transferPassQrCode.spec.ts): add tests for transferPassQrCode function to transfer QR code of event pass
    
    📦 chore(server.ts): remove unused import and export statement
    
    🚚 chore(server.ts): move transferPassQrCodeBatch function to a different location
    
    🐛 fix(index.spec.ts): fix type error in eventPassNft1 and eventPassNft2 declarations
    ✨ feat(index.spec.ts): add unit tests for EventPassNftWrapper class methods: getEventPassNftTransfersMetadata, upsertNftTransfers, updateEventPassNftFromNftTransfer, and applyQrCodeBatchTransferForNewOwner
    
    🔀 chore(index.ts): update import path for transferPassQrCodeBatch from '@features/pass/server' to '@features/pass-api' for consistency and clarity
    
    🔀 chore(tsconfig.base.json): add import path for '@features/pass-api' to support the updated import in index.ts

--- libs/features/pass/src/server.ts
+++ libs/features/pass/src/server.ts
@@ -1,3 +1,2 @@
 // Use this file to export React server components
 export { NoUserPass } from './lib/NoUserPass/NoUserPass';
-export { transferPassQrCodeBatch } from './lib/api/transferPassQrCodeBatch';
