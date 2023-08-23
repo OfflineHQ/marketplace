9aefa65a4d4ead6e68cad4529203c2751711ca29
sebpalluel <sebpalluel@gmail.com>
2023-08-23 15:19:28 +0200

    ✨ feat(server.ts): add export for transferPassQrCodeBatch in server.ts to make it accessible in other modules
    ✨ feat(eventPassNft.mutation.gql): add tokenId field to the eventPassNft mutation gql to include tokenId in the response
    
    🔧 chore(nftActivity.ts): refactor nftActivity function to use EventPassNftWrapper class for better organization and readability
    
    🔧 chore(types.ts): remove unused import of NftTransfer from shared types
    
    🔧 chore(.eslintrc.json): add .eslintrc.json file to eventPass directory to extend the root .eslintrc.json configuration
    
    🔧 chore(.swcrc): add .swcrc file to eventPass directory to configure swc compiler options
    
    📝 docs(README.md): add README.md file to eventPass directory with basic information about the library
    
    📦 chore(eventPass): add eventPass library files and configuration
    
    ✨ feat(eventPass): add jest configuration for eventPass library tests
    
    📦 chore(eventPass): add package.json for eventPass library
    
    📦 chore(eventPass): add project.json for eventPass library
    
    ✨ feat(eventPass): add index.ts file for eventPass library
    
    ✨ feat(eventPass): add EventPassNftWrapper class with methods for handling event pass NFT transfers
    
    ✨ feat(eventPass): add tsconfig.json for eventPass library
    
    ✨ feat(eventPass): add tsconfig.lib.json for eventPass library
    
    ✨ feat(eventPass): add tsconfig.spec.json for eventPass library
    
    🔀 chore(libs/nft/types): update imports and types in index.ts
    🔀 chore(tsconfig.base.json): add import path for @nft/eventPass in tsconfig.base.json

--- libs/indexer/alchemy/webhooks/src/lib/nftActivity.ts
+++ libs/indexer/alchemy/webhooks/src/lib/nftActivity.ts
@@ -2,16 +2,11 @@ import {
   isValidSignatureForAlchemyRequest,
   addAlchemyContextToRequest,
 } from './utils';
-import { adminSdk } from '@gql/admin/api';
-import type { Stage, NftTransfer } from '@gql/shared/types';
+import type { NftTransferWithoutMetadata } from '@nft/types';
 import { WebhookType } from 'alchemy-sdk';
-import type {
-  AlchemyNFTActivityEvent,
-  AlchemyRequest,
-  NftTransferWithoutMetadata,
-  NftTransferNotCreated,
-} from './types';
+import type { AlchemyNFTActivityEvent, AlchemyRequest } from './types';
 import { headers } from 'next/headers';
+import { EventPassNftWrapper } from '@nft/eventPass';
 
 export const extractNftTransfersFromEvent = (
   alchemyWebhookEvent: AlchemyNFTActivityEvent
@@ -50,91 +45,6 @@ export const extractNftTransfersFromEvent = (
   return nftTransfers;
 };
 
-export const getEventPassNftTransfersMetadata = async (
-  nftTransfers: NftTransferWithoutMetadata[],
-  contractAddress: string,
-  chainId: string
-) => {
-  const res = await adminSdk.GetEventPassNftByCollectionAndTokenIds({
-    contractAddress,
-    chainId,
-    tokenIds: nftTransfers.map((t) => t.tokenId),
-  });
-  const eventPassNft = res.eventPassNft;
-
-  return nftTransfers.reduce((acc, nft) => {
-    const nftWithMetadata = eventPassNft.find((n) => n.tokenId === nft.tokenId);
-    if (!nftWithMetadata) {
-      console.error(
-        `Metadata not found for this token ! Skipping execution for this transfer: ${nft.transactionHash} in ${nft.chainId} for ${nft.contractAddress} collection, fromAddress ${nft.fromAddress} toAddress ${nft.toAddress} with erc721TokenId ${nft.tokenId}. This is a critical error that should be investigated.`
-      );
-      return acc; // Skip this item and continue with the next one
-    }
-    const { eventId, eventPassId, organizerId } = nftWithMetadata;
-    return [
-      ...acc,
-      {
-        ...nft,
-        eventId,
-        eventPassId,
-        organizerId,
-      } satisfies NftTransferNotCreated,
-    ];
-  }, [] as NftTransferNotCreated[]);
-};
-
-export const upsertNftTransfers = async (
-  nftTransfers: NftTransferNotCreated[]
-) => {
-  const res = await adminSdk.UpsertNftTransfer({
-    objects: nftTransfers,
-  });
-  if (!res.insert_nftTransfer) {
-    throw new Error('Failed to upsert NftTransfer');
-  }
-  return res.insert_nftTransfer.returning satisfies NftTransfer[];
-};
-
-export const UpdateEventPassNftFromNftTransfer = async (
-  nftTransfers: NftTransfer[]
-) => {
-  const res = await adminSdk.UpdateEventPassNftFromNftTransfer({
-    updates: nftTransfers.map((transfer) => {
-      const { chainId, contractAddress, tokenId, toAddress, id } = transfer;
-      return {
-        where: {
-          contractAddress: { _eq: contractAddress },
-          tokenId: { _eq: tokenId },
-          chainId: { _eq: chainId },
-        },
-        _set: {
-          currentOwnerAddress: toAddress,
-          lastNftTransferId: id,
-        },
-      };
-    }),
-  });
-
-  if (!res.update_eventPassNft_many) {
-    throw new Error('Failed to update eventPassNft');
-  }
-
-  return res.update_eventPassNft_many
-    .map((nftRes) => {
-      if (!nftRes?.returning || !nftRes.returning?.length) {
-        console.error(
-          'No returning data for an update on eventPassNft, this is likely an error'
-        );
-        return null;
-      }
-      return nftRes.returning[0];
-    })
-    .filter((item) => item !== null);
-};
-
-// export const applyQrCodeBatchTransfer = async (
-//   eventPassNfts:
-
 export async function nftActivity(
   req: AlchemyRequest,
   contractAddress: string
@@ -159,16 +69,21 @@ export async function nftActivity(
   }
   const chainId = alchemyWebhookEvent.event.activity[0].network; // TODO: convert to chainId hex string
 
+  const eventPassNftWrapper = new EventPassNftWrapper();
   const nftTransfersFromEvent =
     extractNftTransfersFromEvent(alchemyWebhookEvent);
-  const NftTransfersNotCreated = await getEventPassNftTransfersMetadata(
-    nftTransfersFromEvent,
-    contractAddress,
-    chainId
+  const NftTransfersNotCreated =
+    await eventPassNftWrapper.getEventPassNftTransfersMetadata(
+      nftTransfersFromEvent,
+      contractAddress,
+      chainId
+    );
+
+  const nftTransfers = await eventPassNftWrapper.upsertNftTransfers(
+    NftTransfersNotCreated
   );
-
-  const nftTransfers = await upsertNftTransfers(NftTransfersNotCreated);
-  await UpdateEventPassNftFromNftTransfer(nftTransfers);
-
+  const updatedNfts =
+    await eventPassNftWrapper.updateEventPassNftFromNftTransfer(nftTransfers);
+  await eventPassNftWrapper.applyQrCodeBatchTransferForNewOwner(updatedNfts);
   return new Response(null, { status: 200 });
 }
