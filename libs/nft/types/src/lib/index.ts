42519314e72d7122ce7bf3854ee33b44abe89423
sebpalluel <sebpalluel@gmail.com>
2023-08-24 08:43:21 +0200

    🚀 feat(index.spec.ts): update imports and types to match changes in dependencies
    🐛 fix(index.spec.ts): update type references to match changes in dependencies
    
    🚀 feat(index.ts): add support for EventPassNftAfterMutation type and update method signatures
    
    🐛 fix(index.ts): update method implementation to use new type and handle null values correctly

--- libs/nft/types/src/lib/index.ts
+++ libs/nft/types/src/lib/index.ts
@@ -2,6 +2,7 @@ import type {
   NftTransfer as ImportedNftTransfer,
   EventPassNft as ImportedEventPassNft,
 } from '@gql/shared/types';
+import type { UpdateEventPassNftFromNftTransferMutation } from '@gql/admin/types';
 
 export type NftTransfer = ImportedNftTransfer;
 
@@ -16,3 +17,9 @@ export type NftTransferWithoutMetadata = Omit<
 >;
 
 export type EventPassNft = ImportedEventPassNft;
+
+export type EventPassNftAfterMutation = NonNullable<
+  NonNullable<
+    UpdateEventPassNftFromNftTransferMutation['update_eventPassNft_many']
+  >[0]
+>['returning'][0];
