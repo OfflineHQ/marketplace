38b2868917a71c25afc67030765ec1d9c17e5060
sebpalluel <sebpalluel@gmail.com>
2023-08-24 16:21:37 +0200

    ✨ feat(seeds): add default seed data for eventPassNftContract, eventParameters, and nftTransfer tables
    - Add default seed data for the eventPassNftContract table with two entries.
    - Add default seed data for the eventParameters table with one entry.
    - Add default seed data for the nftTransfer table with two entries.

---
+++ hasura/app/seeds/default/3_eventPassNftContract.sql
@@ -0,0 +1,3 @@
+SET check_function_bodies = false;
+INSERT INTO public."eventPassNftContract" (id, "chainId", "contractAddress", "eventId", "eventPassId", "organizerId") VALUES ('19d09cfe-6a5c-4481-a822-7161d5b4c8cf', '11155111', '0xfakecontractaddress1', 'fake-event-1', 'fake-event-pass-1', 'fake-organizer-1');
+INSERT INTO public."eventPassNftContract" (id, "chainId", "contractAddress", "eventId", "eventPassId", "organizerId") VALUES ('6fa834c5-cc89-4861-a1cf-ff4fce62ba20', '11155111', '0xfakecontractaddress2', 'fake-event-1', 'fake-event-pass-2', 'fake-organizer-1');
