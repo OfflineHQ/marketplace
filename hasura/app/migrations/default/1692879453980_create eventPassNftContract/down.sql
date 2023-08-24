02dd05c105f20ad0eb578472275a19be4e8a1d40
sebpalluel <sebpalluel@gmail.com>
2023-08-24 15:04:29 +0200

    📝 docs(README.md): update e2e test user name from 'Seb Google' to 'Google User' for clarity and consistency
    🔨 chore(google_user.session.json): add fixture file for 'Google User' session data to be used in e2e tests
    
    🔧 fix(commands.ts): update user type from 'seb_google' to 'google_user' for better clarity and consistency
    🔧 fix(commands.ts): remove unused code related to public_eventNftCollection table and add new table public_eventParameters with array relationship to eventPassNftContracts
    
    📝 chore: add eventPassNftContract table to database schema
    
    📝 chore: update tables.yaml to include eventPassNftContract table
    
    📝 chore: update remote_schemas.yaml to remove eventNftCollection relationship
    
    📝 chore: create migration to drop eventNftCollection table
    
    📝 chore: create migration to create eventPassNftContract table
    
    📝 chore: create migration to drop eventParameters table
    
    📝 chore: create migration to create eventParameters table
    
    📝 chore: create migration to alter activityWebhookId column in eventParameters table
    
    🐛 fix(server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics
    ✨ feat(server.ts): add support for process.env.PORT environment variable to be able to run app on a configurable port
    🐛 fix(default/0_account.sql): update email in account seed data
    ✨ feat(default/4_nftTransfer.sql): add new seed file for nftTransfer table
    ✨ feat(default/5_eventPassNft.sql): add new seed file for eventPassNft table
    🐛 fix(profile-avatar/examples.tsx): update eoa in normalUserSession data
    
    🔀 chore(test-utils): rename account and jwt variables for better clarity and consistency
    🔀 chore(test-utils): update address value for google_user account in accounts object
    🔀 chore(test-utils): update userId value for google_user account in usersJwt object
    🔀 chore(test-utils): update me value for sebGoogleClient function to use google_user account

---
+++ hasura/app/migrations/default/1692879453980_create eventPassNftContract/down.sql
@@ -0,0 +1,14 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- -- Create the new table eventPassNftContract
+-- CREATE TABLE "public"."eventPassNftContract" (
+--     "id" serial PRIMARY KEY,
+--     "chainId" text NOT NULL,
+--     "contractAddress" text NOT NULL,
+--     "eventId" text NOT NULL,
+--     "eventPassId" text NOT NULL,
+--     "organizerId" text NOT NULL,
+--     UNIQUE ("contractAddress")
+-- );
+--
+-- COMMENT ON TABLE "public"."eventPassNftContract" IS E'The eventPassNftContract model is designed to store metadata associated with NFT contracts linked to specific event passes. This table captures critical, immutable details from the ERC-721 standard, such as the chainId and contractAddress, ensuring accurate tracking and referencing of NFT contracts. Additionally, this table includes information specific to each event pass, like the eventPassId and organizerId, allowing for precise management and interaction with NFT contracts tied to individual event passes. By centralizing this information, our system can effectively manage, reference, and interact with NFT contracts related to particular event passes.';
