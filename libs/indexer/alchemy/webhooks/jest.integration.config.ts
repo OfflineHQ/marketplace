3cf47d372970ba0b66a025b03679c76587f26bcb
sebpalluel <sebpalluel@gmail.com>
2023-08-24 17:56:43 +0200

    🔧 chore(webhooks): ignore integration tests in coverage and test execution ✨ feat(webhooks): add integration test configuration for indexer-alchemy-webhooks 🔧 chore(webhooks): update project.json to include test-hasura configuration ✨ feat(webhooks): add integration test for nftActivity function

--- libs/indexer/alchemy/webhooks/jest.integration.config.ts
+++ libs/indexer/alchemy/webhooks/jest.integration.config.ts
@@ -1,12 +1,14 @@
 /* eslint-disable */
 export default {
-  displayName: 'indexer-alchemy-webhooks',
+  displayName: 'indexer-alchemy-webhooks-integration',
   preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
+  globalSetup: `${process.cwd()}/tools/test/globalSetupHasura.ts`,
+  globalTeardown: `${process.cwd()}/tools/test/globalTeardownHasura.ts`,
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../../coverage/libs/indexer/alchemy/webhooks',
-  testPathIgnorePatterns: ['.*\\.integration\\..*'],
+  testMatch: ['**/*.integration.spec.ts'],
 };
