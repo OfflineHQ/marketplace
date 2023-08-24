d9477c6947db748814b9ec62db51b82896553cee
sebpalluel <sebpalluel@gmail.com>
2023-08-24 16:12:21 +0200

    🔥 chore(migrations): remove unused migration files for eventNftCollection table ✨ feat(migrations): add migration to replace eventPassOwned table with eventPassNft table 🔧 fix(migrations): add missing down migration for eventPassOwned table
    🔥 chore(migrations): remove unnecessary migration files
    
    🔥 chore(migrations): remove down.sql and up.sql files for migration 1692881987066_run_sql_migration
    
    🔥 chore(migrations): remove down.sql and up.sql files for migration 1692882016141_alter_table_public_eventParameters_alter_column_activityWebhookId
    
    🔥 chore(migrations): remove down.sql and up.sql files for migration 1692882016141_create_eventParameters_eventPassNftContract_delete_eventNftCollection
    
    ✨ feat(migrations): add migration 1692882016141_create_eventParameters_eventPassNftContract_delete_eventNftCollection
    
    The migration files for the mentioned migrations have been removed as they are no longer needed. Additionally, a new migration file 1692882016141_create_eventParameters_eventPassNftContract_delete_eventNftCollection has been added. This migration creates a new table eventPassNftContract and drops the table eventNftCollection. The eventParameters table has also been modified to alter the activityWebhookId column to be nullable.

--- hasura/app/migrations/default/1692882016141_create_eventParameters_eventPassNftContract_delete_eventNftCollection/up.sql
+++ hasura/app/migrations/default/1692882016141_create_eventParameters_eventPassNftContract_delete_eventNftCollection/up.sql
@@ -1,3 +1,6 @@
+
+DROP table "public"."eventNftCollection";
+
 -- Create the new table eventPassNftContract
 CREATE TABLE "public"."eventPassNftContract" (
     "id" uuid NOT NULL DEFAULT gen_random_uuid(),
@@ -12,3 +15,16 @@ CREATE TABLE "public"."eventPassNftContract" (
 COMMENT ON TABLE "public"."eventPassNftContract" IS E'The eventPassNftContract model is designed to store metadata associated with NFT contracts linked to specific event passes. This table captures critical, immutable details from the ERC-721 standard, such as the chainId and contractAddress, ensuring accurate tracking and referencing of NFT contracts. Additionally, this table includes information specific to each event pass, like the eventPassId and organizerId, allowing for precise management and interaction with NFT contracts tied to individual event passes. By centralizing this information, our system can effectively manage, reference, and interact with NFT contracts related to particular event passes.';
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE "public"."eventParameters" (
+    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+    "eventId" text NOT NULL,
+    "activityWebhookId" text NOT NULL,
+    PRIMARY KEY ("id"),
+    UNIQUE ("activityWebhookId"),
+    UNIQUE ("eventId")
+);
+
+COMMENT ON TABLE "public"."eventParameters" IS E'The eventParameters model is designed to define properties on an event involving all event passes. This table includes critical details like the eventId and activityWebhookId, which aids in monitoring and processing events or changes related to the event parameters. By centralizing this information, our system can effectively manage and control parameters tied to specific events, enhancing the overall functionality and flexibility of event handling.';
+
+alter table "public"."eventParameters" alter column "activityWebhookId" drop not null;
