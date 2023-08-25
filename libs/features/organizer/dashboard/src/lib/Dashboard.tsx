f2041da058409567a1f4ba69cb920a843166af11
Alexandre G <scipion@Tour>
2023-08-25 11:07:16 +0200

    add: route for  createEventNftCollection

---
+++ libs/features/organizer/dashboard/src/lib/Dashboard.tsx
@@ -0,0 +1,20 @@
+import { getEventsFromOrganizerId } from '@features/organizer/event/server';
+import { defaultLocale } from '@next/i18n';
+import { User } from 'next-auth/core/types';
+import { EventCards } from './EventCard';
+
+/* eslint-disable-next-line */
+interface OrganizerDashboardProps {
+  user: User;
+}
+
+export async function OrganizerDashboard(props: OrganizerDashboardProps) {
+  const user = props.user;
+  const organizerId = user.organizerId || '';
+  const events = await getEventsFromOrganizerId({
+    id: organizerId,
+    locale: defaultLocale,
+  });
+
+  return <EventCards events={events} />;
+}
