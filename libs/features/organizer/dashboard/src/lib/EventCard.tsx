493e932b9b0969a666fdf08a5f7845a4d53bda06
Alexandre G <scipion@Tour>
2023-08-30 13:20:06 +0200

    add: event pass deploy and lazyMint

--- libs/features/organizer/dashboard/src/lib/EventCard.tsx
+++ libs/features/organizer/dashboard/src/lib/EventCard.tsx
@@ -11,14 +11,12 @@ import {
   CardHeader,
   CardTitle,
   CardOverlay,
-  CardSkeleton,
   Button,
   Avatar,
   AvatarImage,
   AvatarFallback,
 } from '@ui/components';
-import nftCollection from '@nft/thirdweb';
-import { useState } from 'react';
+import nftCollection, { nftsMetadata } from '@nft/thirdweb';
 import { ExternalProvider } from '@ethersproject/providers/lib/web3-provider';
 
 type Event = {
@@ -33,6 +31,18 @@ type Event = {
   eventNftCollection?: {
     contractAddress: string;
   };
+  eventPasses?: [
+    {
+      id: string;
+      name: string;
+      description: string;
+      nftName: string;
+      nftImage: {
+        url: string;
+      };
+      nftDescription: string;
+    }
+  ];
 };
 
 /* eslint-disable-next-line */
@@ -51,14 +61,25 @@ export function EventCards(props: EventCardsProps) {
     <div className={styles['container']}>
       {empty ? (
         <p>No events for this organizer yet.</p>
-      ) : safeUser ? (
+      ) : safeUser && provider ? (
         <EventCard events={events} provider={provider} />
       ) : (
-        <></>
+        <p>Provider is not ready.</p>
       )}
     </div>
   );
 }
+
+type EventPass = {
+  id: string;
+  name: string;
+  description: string;
+  nftName: string;
+  nftImage: {
+    url: string;
+  };
+  nftDescription: string;
+};
 
 interface EventCardProps {
   events: [Event];
@@ -70,77 +91,54 @@ function EventCard(props: EventCardProps) {
   const provider = props.provider;
   const sdk = new nftCollection(provider);
 
-  async function deploy(title: string, id: string) {
-    await sdk.deployACollection(title, id);
-    // api
+  async function deploy(title: string, id: string, metadata: nftsMetadata) {
+    await sdk.deployACollection(title, id, metadata);
   }
 
   return (
     <div className="grid grid-cols-4 gap-8 md:grid-cols-2 lg:grid-cols-4">
-      {events.map((event, index) => (
-        <Card
-          className="w-full items-center justify-center md:w-[380px]"
-          key={index}
-        >
-          <CardHeader>
-            <Avatar size="xl">
-              <AvatarImage src={event.heroImage.url} />
-              <AvatarFallback>{event.slug}</AvatarFallback>
-            </Avatar>
-            <CardTitle>{event.title}</CardTitle>
-            <CardDescription>You have 3 unread messages.</CardDescription>
-            <div className="flex items-center space-x-4 rounded-md border p-4">
-              <div className="flex-1 space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  Push Notifications
-                </p>
-                <p className="text-sm text-muted-foreground">
-                  Send notifications to device.
-                </p>
-              </div>
-            </div>
-          </CardHeader>
-          <CardOverflow>
-            <CardContent className="grid gap-4">
-              <div>
-                <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                  <div className="space-y-1">
-                    <p className="text-sm font-medium leading-none">
-                      Your subscription is expiring soon!
-                    </p>
-                    <p className="text-sm text-muted-foreground">2 hours ago</p>
-                  </div>
-                </div>
-              </div>
-            </CardContent>
-          </CardOverflow>
-          <CardOverlay />
-          <CardFooter variant="sticky">
-            {event.eventNftCollection === null ? (
-              <Button
-                className="w-full"
-                onClick={async () => {
-                  await deploy(event.title, event.id);
-                  //console.log('Deploy');
-                }}
-              >
-                Deploy
-              </Button>
-            ) : (
-              <Button
-                className="w-full"
-                onClick={() => {
-                  navigator.clipboard.writeText(
-                    event.eventNftCollection?.contractAddress || ''
-                  );
-                }}
-              >
-                Copy contract address
-              </Button>
-            )}
-          </CardFooter>
-        </Card>
+      {events.map((event) => (
+        <div>
+          {event.eventPasses && event.eventPasses.length
+            ? event.eventPasses.map((eventPass: EventPass, index: number) => (
+                <Card
+                  className="w-full items-center justify-center md:w-[380px]"
+                  key={index}
+                >
+                  <CardHeader>
+                    <Avatar size="xl">
+                      <AvatarImage src={event.heroImage.url} />
+                      <AvatarFallback>{event.slug}</AvatarFallback>
+                    </Avatar>
+                    <CardTitle>{eventPass.name}</CardTitle>
+                    <CardDescription>{eventPass.description}</CardDescription>
+                    <div className="flex items-center space-x-4 rounded-md border p-4">
+                      <div className="flex-1 space-y-1">
+                        <p className="text-sm font-medium leading-none">
+                          {event.title}
+                        </p>
+                      </div>
+                    </div>
+                  </CardHeader>
+                  <CardOverlay />
+                  <CardFooter variant="sticky">
+                    <Button
+                      className="w-full"
+                      onClick={async () => {
+                        await deploy(eventPass.name, eventPass.id, {
+                          name: eventPass.nftName,
+                          description: eventPass.nftDescription,
+                          image: eventPass.nftImage.url,
+                        });
+                      }}
+                    >
+                      Deploy
+                    </Button>
+                  </CardFooter>
+                </Card>
+              ))
+            : null}
+        </div>
       ))}
     </div>
   );
