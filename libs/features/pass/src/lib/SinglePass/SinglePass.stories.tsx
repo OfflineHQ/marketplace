2fbbe8e433ceb63ff709b155f24dc94e6de0f311
sebpalluel <sebpalluel@gmail.com>
2023-08-30 12:13:54 +0200

    🚀 feat(IsPassRevealedAlert.tsx): add component to display alert based on pass reveal status 🚀 feat(SinglePass.stories.tsx): add stories for different pass reveal scenarios 🚀 feat(SinglePass.tsx): add functionality to download or reveal pass based on pass reveal status
    🚀 feat(examples.tsx): add support for additional event passes and owner information
    
    🌐 feat(en.json, fr.json): add translations for SinglePass component buttons and alerts
    
    🎨 chore(icons.tsx): add Reveal and Download icons for SinglePass component buttons

--- libs/features/pass/src/lib/SinglePass/SinglePass.stories.tsx
+++ libs/features/pass/src/lib/SinglePass/SinglePass.stories.tsx
@@ -1,7 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { screen, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 import { SinglePass } from './SinglePass';
-import { eventPassNft1, SinglePassNoUserExample } from './examples';
+import {
+  eventPassNft1,
+  eventPassNft2,
+  SinglePassNoUserExample,
+  SinglePassOwnerExample,
+  owner,
+} from './examples';
 
 // Import the stories you want to reuse
 
@@ -21,6 +29,51 @@ type Story = StoryObj<typeof meta>;
 
 export const NotRevealedPass: Story = {
   render: SinglePassNoUserExample,
+  play: async ({ container }) => {
+    screen.getByText(/unrevealed pass/i);
+    screen.getByRole('button', { name: /event/i });
+    screen.getByRole('button', { name: /Test Organizer/i });
+    userEvent.click(screen.getByRole('button', { name: /VIP Access/i }));
+    userEvent.click(screen.getByRole('button', { name: /Backstage Access/i }));
+  },
+};
+
+export const RevealedPass: Story = {
+  ...NotRevealedPass,
+  args: {
+    eventPassNft: eventPassNft2,
+  },
+  play: async ({ container }) => {
+    screen.getByText(/caution/i);
+    screen.getByRole('button', { name: /event/i });
+    screen.getByRole('button', { name: /Test Organizer 2/i });
+  },
+};
+
+export const OwnerNotRevealedPass: Story = {
+  render: SinglePassOwnerExample,
+  args: {
+    user: owner,
+  },
+  play: async ({ container }) => {
+    screen.getByText(/action required/i);
+    screen.getByRole('button', { name: /event/i });
+    screen.getByRole('button', { name: /Test Organizer/i });
+    screen.getByRole('button', { name: /reveal/i });
+  },
+};
+
+export const OwnerRevealedPass: Story = {
+  ...OwnerNotRevealedPass,
+  args: {
+    user: owner,
+    eventPassNft: eventPassNft2,
+  },
+  play: async ({ container }) => {
+    screen.getByRole('button', { name: /event/i });
+    screen.getByRole('button', { name: /Test Organizer 2/i });
+    screen.getByRole('button', { name: /download/i });
+  },
 };
 
 // export const SectionWithNormalUser: Story = {
