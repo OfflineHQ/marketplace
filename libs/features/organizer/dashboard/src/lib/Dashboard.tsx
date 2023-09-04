c45ed5fb2b7fdf5f143d64486c4d92774aa2b993
sebpalluel <sebpalluel@gmail.com>
2023-09-04 18:11:13 +0200

    🔥 refactor(ProfileNavClient.tsx): remove unused imports and code related to client-side rendering of profile navigation component ✨ feat(layout.tsx): import AppNavLayoutProps type from @features/appNav/ui to improve type safety 🔧 chore(tsconfig.json): add ../../dist/apps/back-office/.next/types/**/*.ts to the list of files to include in the project 🐛 fix(Dashboard.tsx): fix import statement for EventCards component ✨ feat(EventCards.tsx): create EventCards component to render event cards and handle deployment of NFT collections
    🔧 chore(event-types): add missing import for GetEventsFromOrganizerIdQuery in index.ts
    🔧 chore(next-auth): add organizerId field to User type in next-auth.ts
    🔧 chore(thirdweb): update external_url in NftsMetadata in index.ts
    🔧 chore(package.json): add @thirdweb-dev/sdk dependency

--- libs/features/organizer/dashboard/src/lib/Dashboard.tsx
+++ libs/features/organizer/dashboard/src/lib/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { getEventsFromOrganizerId } from '@features/organizer/event/server';
 import { defaultLocale } from '@next/i18n';
 import { User } from 'next-auth/core/types';
-import { EventCards } from './EventCard';
+import { EventCards } from './EventCards';
 
 interface OrganizerDashboardProps {
   user: User;
@@ -11,21 +11,13 @@ export async function OrganizerDashboard(props: OrganizerDashboardProps) {
   const user = props.user;
   const organizerId = user.organizerId;
 
-  const events = (
-    await getEventsFromOrganizerId({
-      id: organizerId,
-      locale: defaultLocale,
-    })
-  ).map((event) => ({
-    ...event,
-    eventPasses: event.eventPasses.map((pass) => ({
-      ...pass,
-      eventPassPricing: pass.eventPassPricing || undefined,
-    })),
-  }));
+  const events = await getEventsFromOrganizerId({
+    id: organizerId as string,
+    locale: defaultLocale,
+  });
 
   return events && events.length > 0 ? (
-    <EventCards events={events} organizerId={organizerId} />
+    <EventCards events={events} organizerId={organizerId as string} />
   ) : (
     <p>No event at the moment for {organizerId}</p>
   );
