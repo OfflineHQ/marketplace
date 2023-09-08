95a9e3ec3e92ad2823f82156a84273d33d1b0356
sebpalluel <sebpalluel@gmail.com>
2023-09-08 17:06:18 +0200

    🚀 feat(batchDownloadOrReveal.ts): add batchDownloadOrReveal function to handle batch downloading or revealing of event passes 🚀 feat(server.ts): export batchDownloadOrReveal function to make it accessible in other modules

---
+++ libs/features/pass/src/lib/actions/batchDownloadOrReveal.ts
@@ -0,0 +1,36 @@
+import { revealPass, downloadPass } from '@features/pass-api';
+import { EventWithEventPassNfts } from '@features/pass-types';
+import { revalidateTag } from 'next/cache';
+
+export async function batchDownloadOrReveal(
+  eventPassNfts: EventWithEventPassNfts['eventPassNftContracts'][0]['eventPassNfts']
+) {
+  let revealCalled = false;
+
+  const promises = eventPassNfts.map((eventPassNft) => {
+    if (!eventPassNft.isRevealed) {
+      revealCalled = true;
+      return revealPass(eventPassNft.id)
+        .then(() => ({ status: 'fulfilled' }))
+        .catch((error) => ({ status: 'rejected', reason: error }));
+    } else {
+      return downloadPass(eventPassNft.id)
+        .then(() => ({ status: 'fulfilled' }))
+        .catch((error) => ({ status: 'rejected', reason: error }));
+    }
+  });
+
+  const results = await Promise.allSettled(promises);
+
+  results.forEach((result, index) => {
+    if (result.status === 'rejected') {
+      console.error(
+        `Error with eventPassNft at index ${index}: ${result.reason}`
+      );
+    }
+  });
+
+  if (revealCalled) {
+    revalidateTag('userEventPassNfts');
+  }
+}
