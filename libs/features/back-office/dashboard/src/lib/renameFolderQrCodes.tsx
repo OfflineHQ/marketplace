eea891030de32b2a8c795c3995fe0722cca4e54f
Alexandre G <agouasmi@offline.live>
2023-09-11 09:24:01 +0200

    add: error handling and front logic with upload files

--- libs/features/back-office/dashboard/src/lib/renameFolderQrCodes.tsx
+++ libs/features/back-office/dashboard/src/lib/renameFolderQrCodes.tsx
@@ -3,10 +3,81 @@
 import { FileWrapper, FolderWrapper } from '@file-upload/admin';
 import { revalidatePath } from 'next/cache';
 
+export async function checkFolderLength(folderPath: string, maxAmount: number) {
+  revalidatePath('/');
+  const folder = new FolderWrapper();
+
+  const list = await folder.listFolder({
+    accountId: 'FW25bfk',
+    folderPath: folderPath,
+  });
+
+  const simplifiedList = list.items
+    .map((item) => {
+      if (item.type === 'File') {
+        return {
+          path: item.filePath,
+        };
+      }
+      return null;
+    })
+    .filter((item) => item !== null);
+
+  return {
+    isEqual: simplifiedList.length === maxAmount,
+    length: simplifiedList.length,
+  };
+}
+
+export async function checkFolder(
+  folderPath: string,
+  eventId: string,
+  eventPassId: string,
+  maxAmount: number
+) {
+  revalidatePath('/');
+  const folder = new FolderWrapper();
+
+  const list = await folder.listFolder({
+    accountId: 'FW25bfk',
+    folderPath: folderPath,
+  });
+
+  const simplifiedList = list.items
+    .map((item) => {
+      if (item.type === 'File') {
+        return {
+          path: item.filePath,
+        };
+      }
+      return null;
+    })
+    .filter((item) => item !== null);
+
+  if (simplifiedList.length !== maxAmount) {
+    return false;
+  }
+
+  for (let i = 0; i < maxAmount; i++) {
+    const tempPath = folderPath + `/${eventId}-${eventPassId}-${i}`;
+
+    if (
+      !simplifiedList.some(
+        (item) => item !== null && item.path.startsWith(tempPath)
+      )
+    ) {
+      console.log(tempPath);
+      return false;
+    }
+  }
+  return true;
+}
+
 export async function renameFolderQrCodes(
   folderPath: string,
   eventId: string,
-  eventPassId: string
+  eventPassId: string,
+  maxAmount: number
 ) {
   revalidatePath('/');
   const folder = new FolderWrapper();
@@ -36,10 +107,14 @@ export async function renameFolderQrCodes(
     })
     .filter((item) => item !== null);
 
+  if (simplifiedList.length !== maxAmount) {
+    return `Error : The amount of files is ${simplifiedList.length} but should be ${maxAmount}`;
+  }
+
   console.log(simplifiedList);
 
   if (simplifiedList === null || simplifiedList.length === 0) {
-    return;
+    return 'Error : Folder is empty';
   }
 
   const nonNullSimplifiedList = simplifiedList.filter(
@@ -62,4 +137,5 @@ export async function renameFolderQrCodes(
       files: oldFiles,
     },
   });
+  return 'All files have been renamed.';
 }
