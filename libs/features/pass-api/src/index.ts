1b5b5e19b5b9d980924e93b50401d8f79ac56786
sebpalluel <sebpalluel@gmail.com>
2023-09-11 11:39:31 +0200

    🔀 chore(create.ts): update import path for createEventPassNftContract function to '@features/back-office/dashboard-api' to match new file location
    🔀 chore(nfts.ts): update import path for createEventPassNfts function to '@features/back-office/dashboard-api' to match new file location
    
    🔀 chore(.eslintrc.json): add new file for eslint configuration in '@features/back-office/dashboard-api' library
    
    🔀 chore(README.md): add README.md file for '@features/back-office/dashboard-api' library
    
    🔀 chore(jest.config.ts): add jest.config.ts file for '@features/back-office/dashboard-api' library
    
    🔀 chore(package.json): add package.json file for '@features/back-office/dashboard-api' library
    
    🔀 chore(project.json): add project.json file for '@features/back-office/dashboard-api' library
    
    🔀 chore(index.ts): add index.ts file for '@features/back-office/dashboard-api' library
    
    🔀 feat(createEventPassNftContract.ts): add createEventPassNftContract function to handle creation of event pass NFT contract
    
    🔀 feat(createEventPassNfts.ts): add createEventPassNfts function to handle creation of event pass NFTs
    
    🔀 feat(getEventsFromOrganizerId.ts): add getEventsFromOrganizerId function to fetch events from organizer ID
    
    🚀 feat(renameFolderQrCodes.ts): add new file to handle folder and file renaming for QR codes
    🚀 feat(tsconfig.json): add new file to configure TypeScript compiler options for the dashboard-api library
    🚀 feat(tsconfig.lib.json): add new file to extend tsconfig.json and configure TypeScript compiler options for the dashboard-api library
    🚀 feat(tsconfig.spec.json): add new file to extend tsconfig.json and configure TypeScript compiler options for running tests in the dashboard-api library
    🚀 feat(Dashboard.tsx): import getEventsFromOrganizerId from the dashboard-api library instead of the server file
    🚀 feat(EventCards.tsx): import getEventPassOrganizerFolderPath from the pass-api library and renameFolderQrCodes from the dashboard-api library
    🚀 feat(server.ts): remove exports from the server file as they are no longer needed
    🚀 feat(index.ts): export getPassOrganizer, getPassUser, and getEventPassOrganizerFolderPath from the common file in the pass-api library
    
    🔧 fix(common.ts): refactor getPassOrganizer and getPassUser functions to improve code readability and maintainability
    ✨ feat(common.ts): add getEventPassOrganizerFolderPath function to generate the folder path for event passes based on organizer, event, and pass IDs
    
    🔧 chore(tsconfig.base.json): update paths in tsconfig.base.json for improved organization and clarity

--- libs/features/pass-api/src/index.ts
+++ libs/features/pass-api/src/index.ts
@@ -1,3 +1,8 @@
 export * from './lib/transferPassQrCodeBatch';
 export { getEventPassNftByTokenReferenceUser } from './lib/getEventPassNftByTokenReferenceUser';
 export { getEventPassNftByTokenReferenceAnonymous } from './lib/getEventPassNftByTokenReferenceAnonymous';
+export {
+  getPassOrganizer,
+  getPassUser,
+  getEventPassOrganizerFolderPath,
+} from './lib/common';
