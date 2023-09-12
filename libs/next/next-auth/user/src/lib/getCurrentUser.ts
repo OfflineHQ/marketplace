e283a888bca3785606a21ddeb6404db1e9155d86
sebpalluel <sebpalluel@gmail.com>
2023-09-12 09:21:30 +0200

    🔧 fix(Dashboard/loading.tsx): TODO comment to replace with correct skeleton component
    🔧 fix(Dashboard/page.tsx): add null checks for user and organizerId variables to prevent errors
    
    🔧 fix(EventCards.tsx): remove unused events variable and fix return statement
    
    🔧 fix(getCurrentUser.ts): add return type annotation to getCurrentUser function

--- libs/next/next-auth/user/src/lib/getCurrentUser.ts
+++ libs/next/next-auth/user/src/lib/getCurrentUser.ts
@@ -2,9 +2,10 @@
 // import { cookies, headers } from 'next/headers';
 import { getServerSession as originalGetServerSession } from 'next-auth';
 import { authOptions } from '@next/next-auth/options';
+import type { User } from 'next-auth';
 
-// This is a workaround for bug with getServerSession not working with server action imported in client component https://github.com/nextauthjs/next-auth/issues/7486#issuecomment-1543747325
-export const getCurrentUser = async () => {
+export const getCurrentUser = async (): Promise<User | undefined> => {
+  // This is a workaround for bug with getServerSession not working with server action imported in client component https://github.com/nextauthjs/next-auth/issues/7486#issuecomment-1543747325
   // const req = {
   //   headers: Object.fromEntries(headers() as any),
   //   cookies: Object.fromEntries(
