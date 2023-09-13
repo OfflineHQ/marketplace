e774dacd310697b08d3c9a5f2a2e090e89788d64
sebpalluel <sebpalluel@gmail.com>
2023-09-13 12:08:34 +0200

    🔧 chore(migrations): add columns dateSaleStart and dateSaleEnd to eventParameters table
    🔧 chore(migrations): add foreign key constraint for timezone column in eventParameters table
    
    🔧 chore(seeds): add values for dateSaleStart and dateSaleEnd columns in eventParameters table

--- hasura/app/migrations/default/1694524551906_alter_eventParameters_dateStart_dateEnd_timezone_organizerId/up.sql
+++ hasura/app/migrations/default/1694524551906_alter_eventParameters_dateStart_dateEnd_timezone_organizerId/up.sql
@@ -1,14 +1,21 @@
 -- Add columns to eventParameters
-ALTER TABLE "public"."eventParameters"
-ADD COLUMN "dateStart" timestamp without time zone,
-ADD COLUMN "dateEnd" timestamp without time zone,
-ADD COLUMN "timezone" text,
-ADD COLUMN "organizerId" text NOT NULL;
+ALTER TABLE
+	"public"."eventParameters"
+ADD
+	COLUMN "dateStart" timestamp without time zone,
+ADD
+	COLUMN "dateEnd" timestamp without time zone,
+ADD
+	COLUMN "dateSaleStart" timestamp without time zone,
+ADD
+	COLUMN "dateSaleEnd" timestamp without time zone,
+ADD
+	COLUMN "timezone" text,
+ADD
+	COLUMN "organizerId" text NOT NULL;
 
 -- Add foreign key constraint
-ALTER TABLE "public"."eventParameters"
-ADD CONSTRAINT "fk_timezone"
-FOREIGN KEY ("timezone")
-REFERENCES "public"."timezone"("value")
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+ALTER TABLE
+	"public"."eventParameters"
+ADD
+	CONSTRAINT "fk_timezone" FOREIGN KEY ("timezone") REFERENCES "public"."timezone"("value") ON DELETE CASCADE ON UPDATE CASCADE;
