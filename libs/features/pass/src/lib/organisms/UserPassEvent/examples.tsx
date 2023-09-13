6eb70a1bd43711bbc174ad0c15ad817120787ddd
sebpalluel <sebpalluel@gmail.com>
2023-09-13 15:55:28 +0200

    🔧 fix(UserPassEvent.tsx): add timezone prop to UserPass component to display event time in the correct timezone 🔧 fix(examples.tsx): update dateStart and dateEnd values to remove unnecessary trailing zeros 🔧 fix(examples.tsx): add timezone property to eventParameters to specify the timezone of the event 🔧 fix(UserPassList.stories.tsx): remove unused play function 🔧 fix(UserPass.stories.tsx): rename WithUserDarkModeMobile to WithUserDarkModeMobileWithTimezoneDialog 🔧 fix(en.json): add "in-your-timezone" translation for the "to" label in DateRange component

--- libs/features/pass/src/lib/organisms/UserPassEvent/examples.tsx
+++ libs/features/pass/src/lib/organisms/UserPassEvent/examples.tsx
@@ -13,13 +13,15 @@ import {
 } from '@features/organizer/event/examples';
 
 export const eventParameters = {
-  dateStart: '2021-01-01T21:00:00.000Z',
-  dateEnd: '2021-01-03T12:00:00.000Z',
+  dateStart: '2021-01-01T21:00:00.000',
+  dateEnd: '2021-01-03T12:00:00.000',
+  timezone: 'Europe/Paris',
   eventPassNftContracts: [
     {
       eventPass: passFamily,
       eventPassNfts: [
         {
+          id: 'afasfkjasnfjkasf',
           isRevealed: false,
           tokenId: '1198',
         },
@@ -29,10 +31,12 @@ export const eventParameters = {
       eventPass: passWeekend,
       eventPassNfts: [
         {
+          id: 'agdagsgwfagsg',
           isRevealed: true,
           tokenId: '3',
         },
         {
+          id: 'gadfgawtwgwq',
           isRevealed: true,
           tokenId: '4',
         },
@@ -44,17 +48,20 @@ export const eventParameters = {
 } satisfies UserPassEventProps['eventParameters'];
 
 export const eventParameters2 = {
-  dateStart: '2021-02-18T12:00:00.000Z',
-  dateEnd: '2021-01-19T12:00:00.000Z',
+  dateStart: '2021-02-18T12:00:00.000',
+  dateEnd: '2021-01-19T12:00:00.000',
+  timezone: 'Europe/London',
   eventPassNftContracts: [
     {
       eventPass: passPremium,
       eventPassNfts: [
         {
+          id: 'hshsasagegsagedgsd',
           isRevealed: false,
           tokenId: '1200',
         },
         {
+          id: 'y7asgdyu3q2jnfwaf',
           isRevealed: true,
           tokenId: '1201',
         },
