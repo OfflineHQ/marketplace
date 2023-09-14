7714b673991f36010805719817bbb1ea23df1cff
sebpalluel <sebpalluel@gmail.com>
2023-09-14 16:55:54 +0200

    🎨 style(DateRange.tsx): organize imports and add missing imports for components used in DateRange component ✨ feat(DateRange.tsx): add DateRangeSkeleton component to display a loading skeleton while DateRangeClient component is being loaded dynamically ♻️ refactor(DateRange.tsx): replace DateRangeClient component with DynamicDateRangeClient component to load it dynamically on the client side 🐛 fix(DateRangeClient.tsx): change DateRangeClient component to a function declaration to improve readability and maintainability 🔥 chore(DateRangeClient.tsx): remove commented out code from DateRangeClient component

--- libs/features/pass/src/lib/molecules/DateRange/DateRange.tsx
+++ libs/features/pass/src/lib/molecules/DateRange/DateRange.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import { useLocale, useTranslations, NextIntlClientProvider } from 'next-intl';
-import { DateRangeClient, type DateRangeClientProps } from './DateRangeClient';
+import { TextSkeleton } from '@ui/components';
+import { Calendar as CalendarIcon } from '@ui/icons';
+import { type DateRangeClientProps } from './DateRangeClient';
+
+export const DateRangeSkeleton: React.FC = () => {
+  return (
+    <div className="my-2 flex items-center space-x-4">
+      <CalendarIcon size="lg" />
+      <div className="flex flex-col space-y-4">
+        <TextSkeleton className="h-6 w-48 md:w-64" />
+        <TextSkeleton className="h-6 w-48 md:w-64" />
+      </div>
+    </div>
+  );
+};
+
+const DynamicDateRangeClient = dynamic(
+  () => import('./DateRangeClient'),
+  { ssr: false, loading: () => <DateRangeSkeleton /> } // This will load the component only on client side to get the correct timezone
+);
 
 export type DateRangeProps = Omit<
   DateRangeClientProps,
@@ -12,7 +32,7 @@ export const DateRange: React.FC<DateRangeProps> = (props) => {
   const t = useTranslations('Pass.UserPass.DateRange');
   return (
     <NextIntlClientProvider locale={locale} messages={undefined}>
-      <DateRangeClient
+      <DynamicDateRangeClient
         {...props}
         fromText={t('from')}
         toText={t('to')}
