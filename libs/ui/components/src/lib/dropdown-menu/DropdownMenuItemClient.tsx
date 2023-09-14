26df1a40a01c034a8674b7825db6642193cd13e1
sebpalluel <sebpalluel@gmail.com>
2023-09-14 11:19:24 +0200

    🔧 chore(index.ts): add export for 'useToast' and 'type ToastT' to improve module exports 🔧 chore(Dialog.tsx): import 'DialogProps' type from '@radix-ui/react-dialog' to improve type safety 🔧 chore(DropdownMenuItemClient.tsx): import 'type ToastT' from '../toast/useToast' to improve type safety 🔧 chore(DropdownMenuItems.tsx): import 'DropdownMenuItemClient' and 'type DropdownMenuItemClientProps' from './DropdownMenuItemClient' to improve code organization and type safety 🔧 chore(useToast.tsx): export 'type ToastT' as 'Toast' to improve module exports 🔧 chore(index.ts): add export for 'getErrorMessage' and 'getErrorCode' to improve module exports 🔧 chore(error.ts): create 'ErrorWithMessageAndCode' type and utility functions 'getErrorMessage' and 'getErrorCode' to improve error handling

---
+++ libs/ui/components/src/lib/dropdown-menu/DropdownMenuItemClient.tsx
@@ -0,0 +1,92 @@
+'use client';
+
+import { DropdownMenuItem, DropdownMenuShortcut } from './DropdownMenu';
+import { useToast, type ToastT } from '../toast/useToast';
+import { iconCVA } from '@ui/icons';
+import { cn } from '@ui/shared';
+import {
+  getErrorMessage,
+  getErrorCode,
+  type ErrorWithMessageAndCode,
+} from '@utils';
+
+export interface HandleActionProps {
+  action?: (() => void) | undefined;
+  toastSuccess?: (res: unknown) => Promise<ToastT>;
+  toastError?: (error: ErrorWithMessageAndCode) => Promise<ToastT>;
+}
+
+export interface MenuItem extends HandleActionProps {
+  type: 'label' | 'separator' | 'item' | 'sub' | 'children';
+  text?: string;
+  children?: React.ReactNode;
+  wrapper?: React.ReactElement;
+  icon?: React.ReactElement;
+  shortcut?: string;
+  disabled?: boolean;
+  subItems?: MenuItem[];
+  current?: boolean;
+  className?: string;
+}
+
+export interface DropdownMenuItemClientProps {
+  icon?: React.ReactElement;
+  iconClasses: string;
+  item: MenuItem;
+  setLoading?: (loading: boolean) => void;
+}
+export const DropdownMenuItemClient: React.FC<DropdownMenuItemClientProps> = ({
+  icon,
+  item,
+  iconClasses,
+  setLoading,
+}) => {
+  const { toast } = useToast();
+
+  const handleAction = async ({
+    action,
+    toastSuccess,
+    toastError,
+  }: HandleActionProps) => {
+    console.log('handleAction', action, toastSuccess, toastError);
+    if (setLoading) setLoading(true);
+    try {
+      let res: any = null;
+      if (action) res = await action();
+      if (toastSuccess) {
+        const toastSuccessRes = await toastSuccess(res);
+        toast(toastSuccessRes);
+      }
+    } catch (error) {
+      if (toastError) {
+        const toastErrorRes = await toastError({
+          message: getErrorMessage(error),
+          code: getErrorCode(error),
+        });
+        toast(toastErrorRes);
+      }
+    } finally {
+      if (setLoading) setLoading(false);
+    }
+  };
+
+  return (
+    <DropdownMenuItem
+      disabled={item.disabled}
+      onSelect={() => handleAction(item)}
+      wrapper={item.wrapper}
+      className={item.className}
+    >
+      {icon && (
+        <icon.type
+          {...icon.props}
+          className={cn(iconClasses, icon.props.className)}
+        />
+      )}
+      <span>{item.text}</span>
+      {item.shortcut && (
+        <DropdownMenuShortcut>{item.shortcut}</DropdownMenuShortcut>
+      )}
+    </DropdownMenuItem>
+  );
+};
