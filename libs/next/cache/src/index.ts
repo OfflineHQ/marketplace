016854cedec1c45aa14a506c7379cdab7705d1d9
Alexandre G <agouasmi@offline.live>
2023-09-28 09:10:42 +0200

    add: libraries next/cache and test-utils/cache
    fix: mset for files and wrapper for main operations redis Cache
    
    fix: lockfile
    
    fix: lockfile
    
    fix: port 8080 to 7070 for redis serverless
    
    fix: port import in no module
    
    fix: isRedisReady with fetch on base url
    
    fix: remove useless env vars
    
    del: useless files
    
    🔧 chore(ci.yml): add 'staging' branch to the list of branches triggering CI workflow
    🔧 chore(preview-apps.yml): ignore 'main' and 'staging' branches when triggering deploy preview apps workflow
    ✨ feat(staging-apps.yml): add workflow for deploying staging apps
    🔧 chore(package.json): add '@vercel/edge-config' package to dependencies
    
    🔒 chore(docker-compose.yaml): update HASURA_GRAPHQL_JWT_SECRET to include "organizer" role in x-hasura-allowed-roles
    
    🔧 chore(.env.local): add NEXT_PUBLIC_WEB3AUTH_NETWORK environment variable to specify the network for web3 authentication
    🐛 fix(safeAuthSetup.tsx): use NEXT_PUBLIC_WEB3AUTH_NETWORK environment variable to set the web3AuthNetwork option in useSafeAuth function
    
    🔒 chore(pnpm-lock.yaml): update lockfileVersion to '6.0' to match the current version of pnpm
    🔧 refactor(pnpm-lock.yaml): remove unnecessary settings from pnpm lockfile
    📦 chore(pnpm-lock.yaml): update '@dinero.js/currencies' dependency specifier to version 2.0.0-alpha.14
    
    🔥 chore(preview-apps-delete.yml): ignore main and staging branches for pull_request closed event to prevent deletion of preview apps on these branches
    🔧 chore(staging-apps.yml): add step to pull Vercel environment information for preview environment before building Vercel project artifacts
    
    🔧 chore(staging-apps.yml): add step to set up environment and cache dependencies for the workflow
    🔧 chore(staging-apps.yml): add step to cache Vercel CLI for the workflow
    
    🐛 fix(staging-apps.yml): update 'vercel pull' command to specify the git branch as 'staging' to pull changes from the correct branch
    
    🔧 chore(staging-apps.yml): remove unnecessary build step in the staging workflow
    🌐 update(en.json): update title in Index component to include "staging" for better clarity
    
    🚀 feat(page.tsx): display the network information from the environment variable NEXT_PUBLIC_WEB3AUTH_NETWORK
    
    ℹ️ The change was made to display the network information on the page. The value is retrieved from the environment variable NEXT_PUBLIC_WEB3AUTH_NETWORK and displayed as "Network: [value]". This provides useful information to the user about the network they are currently connected to.
    
    🔀 chore(page.tsx): remove unused code for NEXT_PUBLIC_WEB3AUTH_NETWORK to improve code cleanliness and readability
    🌐 feat(en.json): update title in Index page to remove "staging" suffix for better consistency and clarity
    
    Merge branch 'staging' of github.com:Offline-Project/marketplace into 94-web-integrate-vercel-kv
    
    fix: signing commit
    
    🔒 chore(pnpm-lock.yaml): update pnpm lockfile settings to include autoInstallPeers and excludeLinksFromLockfile options
    
    📦 chore(pnpm-lock.yaml): update dependencies in pnpm lockfile
    
    - Update '@vercel/analytics' to version 0.1.11 (react@18.2.0)
    - Add '@vercel/edge-config' at version 0.4.1
    - Add '@vercel/edge-config-fs' at version 0.1.0
    
    🔒 chore(pnpm-lock.yaml): update resolution and engines for '@vercel/edge-config' and '@vercel/edge-config-fs' dependencies

---
+++ libs/next/cache/src/index.ts
@@ -0,0 +1,98 @@
+import { createClient } from '@vercel/kv';
+
+export default class Cache {
+  private client;
+
+  constructor(url?: string, token?: string) {
+    const kvUrl = url || process.env.KV_REST_API_URL;
+    const kvToken = token || process.env.KV_REST_API_TOKEN;
+
+    if (!kvUrl || !kvToken) {
+      throw new Error('Error with KV environment variables');
+    }
+
+    this.client = createClient({
+      url: kvUrl,
+      token: kvToken,
+    });
+  }
+
+  private async handleOperation(
+    operation: Promise<unknown>,
+    operationName: string
+  ) {
+    try {
+      return await operation;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(
+          `Error with redis operation ${operationName} : ${error.message}`
+        );
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async get(key: string) {
+    return this.handleOperation(this.client.get(key), 'get');
+  }
+
+  async set(key: string, value: unknown) {
+    return this.handleOperation(this.client.set(key, value), 'set');
+  }
+
+  async del(key: string) {
+    return this.handleOperation(this.client.del(key), 'del');
+  }
+
+  async hset(key: string, value: { [field: string]: unknown }) {
+    return this.handleOperation(this.client.hset(key, value), 'hset');
+  }
+
+  async hget(key: string, field: string) {
+    return this.handleOperation(this.client.hget(key, field), 'hget');
+  }
+
+  async lpush(key: string, ...values: unknown[]) {
+    return this.handleOperation(this.client.lpush(key, ...values), 'lpush');
+  }
+
+  async lrange(key: string, start: number, stop: number) {
+    return this.handleOperation(this.client.lrange(key, start, stop), 'lrange');
+  }
+
+  async sadd(key: string, ...members: string[]) {
+    return this.handleOperation(this.client.sadd(key, ...members), 'sadd');
+  }
+
+  async srem(key: string, ...members: string[]) {
+    return this.handleOperation(this.client.srem(key, ...members), 'srem');
+  }
+
+  async sismember(key: string, member: string) {
+    return this.handleOperation(
+      this.client.sismember(key, member),
+      'sismember'
+    );
+  }
+
+  //async zadd(key: string, scoreMembers: Array<[number, string]>) {
+  //  return this.handleOperation(this.client.zadd(key, scoreMembers), 'zadd'); // TODO: Problem with type
+  //}
+
+  async zrange(key: string, start: number, stop: number) {
+    return this.handleOperation(this.client.zrange(key, start, stop), 'zrange');
+  }
+
+  async setbit(key: string, offset: number, value: 0 | 1) {
+    return this.handleOperation(
+      this.client.setbit(key, offset, value),
+      'setbit'
+    );
+  }
+
+  async getbit(key: string, offset: number) {
+    return this.handleOperation(this.client.getbit(key, offset), 'getbit');
+  }
+}
