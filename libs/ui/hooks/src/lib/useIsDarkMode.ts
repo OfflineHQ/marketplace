1ea9fd197e037166de5ecf7541c6a68eba14cefd
sebpalluel <sebpalluel@gmail.com>
2023-10-02 15:56:31 +0100

    replace deprecated api with useTheme

--- libs/ui/hooks/src/lib/useIsDarkMode.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-export const useIsDarkMode = () => {
-  const isDark = useState<boolean>(() =>
-    document.documentElement.classList.contains('dark')
-  );
-  return isDark[0];
-};
