d23805d6e2f91d54f0f45f75a93010c276fe13cc
Alexandre Gouasmi <agouasmi@offline.live>
2023-10-02 11:24:32 +0200

    fix: writeToFile rates

--- libs/currency/api/src/currency-api.spec.ts
+++ libs/currency/api/src/currency-api.spec.ts
@@ -1,63 +1,20 @@
 import Currency from './index';
 
-jest.mock('fs/promises');
-
-describe('CurrencyApi basics', () => {
+describe('Currency class', () => {
   let currency: Currency;
 
   beforeEach(() => {
     currency = new Currency();
   });
 
-  it('should exist', () => {
-    expect(currency).toBeDefined();
-  });
+  describe('getRate method', () => {
+    it('should get rates from Fixer API and save to local file', async () => {
+      const mockRates = { USD: 1.2, GBP: 0.85 };
+      const baseCurrency = 'EUR';
 
-  it('should work', async () => {
-    await expect(currency.getRate('EUR')).resolves.not.toThrow();
-  });
-});
+      const rates = await currency.getRate(baseCurrency);
 
-describe('Currency API Tests', () => {
-  let currency: Currency;
-
-  beforeEach(() => {
-    currency = new Currency();
-  });
-
-  it('should console warn when rate limit exceeded on Fixer API', async () => {
-    jest.spyOn(currency, 'fetchFromFixer').mockImplementation(() => {
-      throw new Error('Rate limit exceeded on Fixer API');
+      expect(rates).toEqual(mockRates);
     });
-
-    const consoleSpy = jest.spyOn(console, 'warn');
-    await currency.getRate('EUR');
-    expect(consoleSpy).toHaveBeenCalledWith('Rate limit exceeded on Fixer API');
-    consoleSpy.mockRestore();
-  });
-
-  it('should console warn when falling back to ExchangeRate API', async () => {
-    jest.spyOn(currency, 'fetchFromFixer').mockImplementation(() => {
-      throw new Error('Some error');
-    });
-
-    const consoleSpy = jest.spyOn(console, 'warn');
-    await currency.getRate('EUR');
-    expect(consoleSpy).toHaveBeenCalledWith('Falling back to ExchangeRate API');
-    consoleSpy.mockRestore();
-  });
-
-  it('should throw error when both Fixer and ExchangeRate APIs fail', async () => {
-    jest.spyOn(currency, 'fetchFromFixer').mockImplementation(() => {
-      throw new Error('Fixer API error');
-    });
-
-    jest.spyOn(currency, 'fetchFromExchangeRate').mockImplementation(() => {
-      throw new Error('ExchangeRate API error');
-    });
-
-    await expect(currency.getRate('EUR')).rejects.toThrow(
-      'Error fetching rates for base currency : EUR'
-    );
   });
 });
--- libs/currency/api/src/index.ts
+++ libs/currency/api/src/index.ts
@@ -1,3 +1,4 @@
+import { isPreviewOrProduction } from '@utils';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -5,85 +6,81 @@ export default class Currency {
   private FIXER_CURRENCY_API_KEY = process.env.FIXER_CURRENCY_API_KEY;
   private EXCHANGE_RATE_API_KEY = process.env.EXCHANGE_RATE_API_KEY;
 
-  async getRate(baseCurrency: string): Promise<{
-    [key: string]: number;
-  }> {
+  async getRate(baseCurrency: string): Promise<{ [key: string]: number }> {
+    if (!isPreviewOrProduction()) {
+      return await this.fetchFromLocalFile(baseCurrency);
+    }
     try {
-      return await this.fetchFromFixer(baseCurrency);
+      const rates = await this.fetchFromAPI(
+        `http://data.fixer.io/api/latest?access_key=${this.FIXER_CURRENCY_API_KEY}&base=${baseCurrency}`,
+        'Fixer'
+      );
+      await this.saveToLocalFile(baseCurrency, rates);
+      return rates;
     } catch (error) {
-      if ((error as Error).message.includes('rate limit')) {
-        console.warn('Rate limit exceeded on Fixer API');
-      }
       console.warn('Falling back to ExchangeRate API');
       try {
-        return await this.fetchFromExchangeRate(baseCurrency);
-      } catch (error) {
-        throw new Error(
-          `Error fetching rates for base currency : ${baseCurrency}`
+        const rates = await this.fetchFromAPI(
+          `https://v6.exchangerate-api.com/v6/${this.EXCHANGE_RATE_API_KEY}/latest/${baseCurrency}`,
+          'ExchangeRate'
         );
+        await this.saveToLocalFile(baseCurrency, rates);
+        return rates;
+      } catch (error) {
+        console.warn('Falling back to local file');
+        return await this.fetchFromLocalFile(baseCurrency);
       }
     }
   }
 
-  async fetchFromFixer(
-    baseCurrency: string
+  async fetchFromAPI(
+    url: string,
+    apiName: string
   ): Promise<{ [key: string]: number }> {
-    const url = `http://data.fixer.io/api/latest?access_key=${this.FIXER_CURRENCY_API_KEY}&base=${baseCurrency}`;
-
-    const fixerResponse = await fetch(url).catch((error) => {
-      console.error('Error fetching from Fixer API:', error);
-      throw new Error('Failed to fetch data from Fixer API');
+    const response = await fetch(url).catch((error) => {
+      console.error(`Error fetching from ${apiName} API:`, error);
+      throw new Error(`Failed to fetch data from ${apiName} API`);
     });
 
-    if (!fixerResponse.ok) {
+    if (!response.ok) {
       throw new Error(
-        `Fixer API responded with status: ${fixerResponse.status}`
+        `${apiName} API responded with status: ${response.status}`
       );
     }
 
-    const fixerData = await fixerResponse.json().catch((error) => {
-      console.error('Error parsing response from Fixer API:', error);
-      throw new Error('Failed to parse response from Fixer API');
+    const data = await response.json().catch((error) => {
+      console.error(`Error parsing response from ${apiName} API:`, error);
+      throw new Error(`Failed to parse response from ${apiName} API`);
     });
 
-    if (fixerData.error && fixerData.error.code === 104) {
+    if (data.error && data.error.code === 104) {
       throw new Error('Rate limit exceeded on Fixer API');
     }
 
-    return fixerData.rates;
+    return apiName === 'Fixer' ? data.rates : data.conversion_rates;
   }
 
-  async fetchFromExchangeRate(
-    baseCurrency: string
-  ): Promise<{ [key: string]: number }> {
-    const url = `https://v6.exchangerate-api.com/v6/${this.EXCHANGE_RATE_API_KEY}/latest/${baseCurrency}`;
-
-    const exchangeRateResponse = await fetch(url).catch((error) => {
-      console.error('Error fetching from ExchangeRate API:', error);
-      throw new Error('Failed to fetch data from ExchangeRate API');
-    });
-
-    if (!exchangeRateResponse.ok) {
-      throw new Error(
-        `ExchangeRate API responded with status: ${exchangeRateResponse.status}`
-      );
-    }
-
-    const exchangeRateData = await exchangeRateResponse
-      .json()
-      .catch((error) => {
-        console.error('Error parsing response from ExchangeRate API:', error);
-        throw new Error('Failed to parse response from ExchangeRate API');
-      });
-
-    return exchangeRateData.conversion_rates;
-  }
-
-  fetchFromLocalFile(baseCurrency: string): {
-    [key: string]: number;
-  } {
+  async saveToLocalFile(
+    baseCurrency: string,
+    rates: { [key: string]: number }
+  ) {
+    const filePath = path.resolve(__dirname, `../rates/${baseCurrency}.json`);
     try {
-      const data = fs.readFileSync(
+      await fs.promises.writeFile(filePath, JSON.stringify(rates));
+    } catch (error) {
+      console.error(
+        `Failed to write rates for ${baseCurrency} to local file:`,
+        error
+      );
+      throw new Error(`Could not save rates for ${baseCurrency}`);
+    }
+  }
+
+  async fetchFromLocalFile(baseCurrency: string): Promise<{
+    [key: string]: number;
+  }> {
+    try {
+      const data = await fs.promises.readFile(
         path.resolve(__dirname, `../rates/${baseCurrency}.json`),
         'utf8'
       );
