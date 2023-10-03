46506c8326522b24cd13d14a25547f42803e9a7f
sebpalluel <sebpalluel@gmail.com>
2023-10-03 18:07:47 +0100

    upgrade to latest sentry version and update settings for prod and staging

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -11,13 +11,26 @@ const SENTRY_DSN = process.env.SENTRY_AUTH_TOKEN
 
 Sentry.init({
   environment: process.env.NEXT_PUBLIC_VERCEL_ENV || 'development',
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
   enabled: !!SENTRY_DSN,
   dsn: SENTRY_DSN || undefined,
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+  tracesSampleRate:
+    process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? 0.2 : 1,
+  replaysOnErrorSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate:
+    process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ? 0.1 : 1,
+  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+  integrations: [
+    // eslint-disable-next-line import/namespace
+    new Sentry.Replay({
+      // Additional Replay configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
 });
