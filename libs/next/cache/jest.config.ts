d2bd4f6573708a55af9099221ed142092af77288
sebpalluel <sebpalluel@gmail.com>
2023-10-03 15:11:12 +0100

    delete tes-utils/cache test + fix next/cache test. Now test running against the redis test instance.

--- libs/next/cache/jest.config.ts
+++ libs/next/cache/jest.config.ts
@@ -2,6 +2,8 @@
 export default {
   displayName: 'next-cache',
   preset: '../../../jest.preset.js',
+  globalSetup: `${process.cwd()}/tools/test/globalSetupHasura.ts`,
+  globalTeardown: `${process.cwd()}/tools/test/globalTeardownHasura.ts`,
   transform: {
     '^.+\\.[tj]sx?$': [
       '@swc/jest',
