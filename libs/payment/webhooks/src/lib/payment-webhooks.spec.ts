6fbf048ee8f83fcae1d4b01f76dacbf52c68b41b
sebpalluel <sebpalluel@gmail.com>
2023-10-06 19:06:59 +0100

    feat(webhooks): add route for handling Stripe checkout webhook events
    - Added a new file `route.ts` in the `webhooks/payment/stripe/checkout` directory to handle the `POST` request for Stripe checkout webhook events.
    - The `POST` function in `route.ts` calls the `stripeCheckoutStatus` function from the `@payment/webhooks` library to handle the webhook event.
    
    fix(payment-webhooks): remove unnecessary test code
    
    - Removed the test code in the `payment-webhooks.spec.ts` file as it was not being used.
    
    feat(payment-webhooks): implement handling of Stripe checkout webhook events
    
    - Updated the `payment-webhooks.ts` file in the `payment/webhooks` library to handle the following Stripe checkout webhook events: `checkout.session.completed` and `checkout.session.expired`.
    - Added support for verifying the Stripe webhook signature and parsing the webhook event data.
    - Implemented logic to handle different scenarios based on the webhook event type and payment status.
    - Added error handling and logging for any unexpected scenarios or errors encountered during the webhook handling process.

--- libs/payment/webhooks/src/lib/payment-webhooks.spec.ts
+++ libs/payment/webhooks/src/lib/payment-webhooks.spec.ts
@@ -1,7 +1,1 @@
-import { paymentWebhooks } from './payment-webhooks';
-
-describe('paymentWebhooks', () => {
-  it('should work', () => {
-    expect(paymentWebhooks()).toEqual('payment-webhooks');
-  });
-});
+// describe('paymentWebhooks', () => {});
