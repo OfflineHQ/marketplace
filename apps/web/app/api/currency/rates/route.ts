322f3518eefb08a803e84046cb59b58391b9c9e0
Alexandre Gouasmi <agouasmi@offline.live>
2023-10-09 17:01:54 +0200

    fix: getRates in currency provider with rfetch

--- apps/web/app/api/currency/rates/route.ts
+++ apps/web/app/api/currency/rates/route.ts
@@ -1,13 +1,9 @@
 import { getRates } from '@next/currency-cache';
-import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   try {
-    revalidatePath('/');
-    console.log('I was in the route');
     const rates = await getRates();
-    console.log('Rates', rates);
 
     return new NextResponse(JSON.stringify(rates), {
       status: 200,
