e1190df3c453ea244e19ea259e1c7904717b3b4e
Alexandre Gouasmi <agouasmi@offline.live>
2023-10-11 13:03:41 +0200

    fix: fix tests with import.meta and new EUR.json

--- libs/next/currency-cache/jest.config.ts
+++ libs/next/currency-cache/jest.config.ts
@@ -3,9 +3,6 @@ export default {
   displayName: 'next-currency-cache',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/libs/next/currency-cache',
 };
