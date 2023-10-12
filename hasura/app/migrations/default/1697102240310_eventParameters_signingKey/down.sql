8e3ce7fb5a37472acea20964836bee99507a0c88
sebpalluel <sebpalluel@gmail.com>
2023-10-12 15:04:55 +0100

    🔥 chore(migrations): remove unnecessary migration files for altering the "eventParameters" table and its "signingKey" column 🔧 chore(migrations): add new migration files for adding the "signingKey" column to the "eventParameters" table and creating a unique constraint on it

--- hasura/app/migrations/default/1697102240310_eventParameters_signingKey/down.sql
+++ hasura/app/migrations/default/1697102240310_eventParameters_signingKey/down.sql
@@ -1,3 +1,6 @@
+
+alter table "public"."eventParameters" drop constraint "eventParameters_signingKey_key";
+
 -- Could not auto-generate a down migration.
 -- Please write an appropriate down migration for the SQL below:
 -- alter table "public"."eventParameters" add column "signingKey" text
--- hasura/app/migrations/default/1697102240310_eventParameters_signingKey/up.sql
+++ hasura/app/migrations/default/1697102240310_eventParameters_signingKey/up.sql
@@ -1,1 +1,5 @@
+
+alter table "public"."eventParameters" add column "signingKey" text
+ null;
+
 alter table "public"."eventParameters" add constraint "eventParameters_signingKey_key" unique ("signingKey");
