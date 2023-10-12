296c6214c18f00b4d5f00ce5d94c3252ba7981da
Alexandre Gouasmi <agouasmi@offline.live>
2023-10-12 15:30:48 +0200

    MERGE gasless-open-zeppelin cherry-pick into gasless-definitive

--- libs/nft/thirdweb-organizer/src/index.ts
+++ libs/nft/thirdweb-organizer/src/index.ts
@@ -4,6 +4,11 @@ import env from '@env/client';
 import { ExternalProvider } from '@ethersproject/providers';
 import { ThirdwebSDK } from '@thirdweb-dev/sdk';
 import { ethers } from 'ethers';
+import {
+  createEventParametersAndWebhook,
+  createEventPassNftContract,
+  createEventPassNfts,
+} from './action';
 
 export type NftsMetadata = {
   name: string;
@@ -19,6 +24,11 @@ class NftCollection {
     const signer = web3Provider.getSigner();
     this.sdk = ThirdwebSDK.fromSigner(signer, env.NEXT_PUBLIC_CHAIN, {
       clientId: '***REMOVED***',
+      gasless: {
+        openzeppelin: {
+          relayerUrl: env.NEXT_PUBLIC_OPENZEPPELIN_URL,
+        },
+      },
     });
   }
 
@@ -31,74 +41,81 @@ class NftCollection {
     eventSlug: string,
     metadata: NftsMetadata
   ) {
-    if (maxAmount === 0) {
-      return;
-    }
-    const address = await this.sdk.wallet.getAddress();
-    const chainIdNumber = await this.sdk.wallet.getChainId();
-    const chainId = chainIdNumber.toString();
+    try {
+      const address = await this.sdk.wallet.getAddress();
+      const chainIdNumber = await this.sdk.wallet.getChainId();
+      const chainId = chainIdNumber.toString();
 
-    const txResult = await this.sdk.deployer.deployBuiltInContract('nft-drop', {
-      name,
-      primary_sale_recipient: address,
-      voting_token_address: address,
-    });
+      const txResult = await this.sdk.deployer.deployBuiltInContract(
+        'nft-drop',
+        {
+          name,
+          primary_sale_recipient: address,
+          voting_token_address: address,
+        }
+      );
 
-    const resCreate = await fetch('/api/nft/create', {
-      method: 'POST',
-      body: JSON.stringify({
-        props: {
-          contractAddress: txResult,
-          id: eventPassId,
-          chainId: chainId,
-          eventId: eventId,
-          organizerId: organizerId,
+      await createEventPassNftContract({
+        contractAddress: txResult,
+        eventPassId: eventPassId,
+        chainId: chainId,
+        eventId: eventId,
+        organizerId: organizerId,
+      });
+
+      const contract = await this.sdk.getContract(txResult);
+
+      await contract.erc721.claimConditions.set([
+        {
+          metadata: {
+            name: 'Phase de claim',
+          },
+          startTime: new Date(),
+          maxClaimablePerWallet: 0,
+          snapshot: [
+            {
+              address: env.NEXT_PUBLIC_THIRDWEB_MASTER_ADDRESS,
+              maxClaimable: maxAmount,
+            },
+          ],
         },
-      }),
-    });
+      ]);
 
-    if (resCreate.status !== 200) {
-      return;
-    }
-
-    const contract = await this.sdk.getContract(txResult);
-
-    const metadatas = Array.from({ length: maxAmount }).map((_, i) => {
-      return {
-        name: metadata.name,
-        description: metadata.description,
-        image: metadata.image,
-        external_url: `https://www.offline.live/pass/organizer/${organizerId}/event/${eventId}/eventPass/${eventPassId}/${i}`,
-      };
-    });
-    const results = await contract.erc721.lazyMint(metadatas);
-    console.log(txResult);
-    const hasuraMetadatas = await Promise.all(
-      metadatas.map(async (m, i) => {
-        const data = await results[i].data();
+      const metadatas = Array.from({ length: maxAmount }).map((_, i) => {
         return {
-          metadata: m,
-          chainId: chainId,
-          tokenId: data.id,
-          tokenUri: data.uri,
-          organizerId: organizerId,
-          eventId: eventId,
-          eventPassId: eventPassId,
-          contractAddress: txResult,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+          external_url: `https://www.offline.live/pass/organizer/${organizerId}/event/${eventId}/eventPass/${eventPassId}/${i}`,
         };
-      })
-    );
-    console.log('hasuraMetadas : ', hasuraMetadatas);
+      });
+      const results = await contract.erc721.lazyMint(metadatas);
 
-    const resNfts = await fetch('/api/nft/nfts', {
-      method: 'POST',
-      body: JSON.stringify({
-        props: {
-          hasuraMetadatas,
-        },
-      }),
-    });
-    console.log(resNfts);
+      const hasuraMetadatas = await Promise.all(
+        metadatas.map(async (m, i) => {
+          const data = await results[i].data();
+          return {
+            metadata: m,
+            chainId: chainId,
+            tokenId: data.id,
+            tokenUri: data.uri,
+            organizerId: organizerId,
+            eventId: eventId,
+            eventPassId: eventPassId,
+            contractAddress: txResult,
+          };
+        })
+      );
+
+      await createEventPassNfts(hasuraMetadatas);
+      await createEventParametersAndWebhook({
+        eventId,
+        nftCollectionAddresses: [txResult],
+        organizerId,
+      });
+    } catch (error) {
+      console.error('Error deploying a collection:', error);
+    }
   }
 }
 
