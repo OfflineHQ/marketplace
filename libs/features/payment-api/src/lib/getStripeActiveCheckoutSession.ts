4761761d985367c9e9f9718eaac32c686ef92252
sebpalluel <sebpalluel@gmail.com>
2023-10-12 20:26:08 +0100

    🚚 chore(server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics
    🚀 feat(server.ts): add support for process.env.PORT environment variable to be able to run app on a configurable port
    
    🚚 chore(preview.tsx): update import path for CurrencyProvider from '@next/currency' to '@next/currency-provider'
    
    🚚 chore(layout.tsx): update import path for CurrencyProvider from '@next/currency' to '@next/currency-provider'
    
    🔥 remove(api/currency/rates/route.ts): remove unused file
    
    🚚 chore(EventPasses.tsx): update import path for useCurrency from '@next/currency' to '@next/currency-provider'
    
    🔥 remove(.babelrc): remove unused file
    
    🚚 chore(project.json): remove unused glob pattern for types directory
    
    🚚 chore(PassCard.tsx): update import path for useCurrency and formatCurrency from '@next/currency' to '@next/currency-provider' and '@next/currency' respectively
    
    🚚 chore(PassTotal.tsx): update import path for useCurrency and formatCurrency from '@next/currency' to '@next/currency-provider' and '@next/currency' respectively
    
    🚚 chore(SinglePass.tsx): update import path for formatCurrency from '@next/currency' to '@next/currency-provider' and add import for ConvertedCurrency from '@next/currency'
    
    🚚 chore(createStripeCheckoutSession.ts): update import path for getCurrencyPreference from '@next/currency/server' to '@next/currency'
    
    🚚 chore(getStripeActiveCheckoutSession.ts): update import path for getCurrencyPreference from '@next/currency/server' to '@next/currency'
    
    🚚 chore(tsconfig.base.json): update import path for '@next/currency/server' to '@next/currency-provider'

--- libs/features/payment-api/src/lib/getStripeActiveCheckoutSession.ts
+++ libs/features/payment-api/src/lib/getStripeActiveCheckoutSession.ts
@@ -7,7 +7,9 @@ import { Payment } from '@payment/admin';
 export async function getStripeActiveCheckoutSession() {
   const payment = new Payment();
   const user = await getCurrentUser();
+  if (!user) throw new Error('User not found');
   const customer = await payment.getOrCreateStripeCustomer({ user });
+  if (!customer) throw new Error('Customer not found');
   return payment.getStripeActiveCheckoutSessionForUser({
     stripeCustomerId: customer.stripeCustomerId,
   });
