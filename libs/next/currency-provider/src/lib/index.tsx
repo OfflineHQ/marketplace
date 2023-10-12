ec0e23b7c8fe45017cb5f852226c6d5e881457c2
sebpalluel <sebpalluel@gmail.com>
2023-10-12 18:18:41 +0100

    📦 chore(currency-provider): add currency-provider library
    ✨ feat(currency-provider): add CurrencyProvider component to provide currency rates and loading state to children components
    
    🔧 chore(currency-provider): configure ESLint for currency-provider library
    
    🔧 chore(currency-provider): configure Jest for currency-provider library
    
    🔧 chore(currency-provider): configure TypeScript for currency-provider library
    
    📝 docs(currency-provider): add README.md for currency-provider library
    
    🚚 chore(currency-provider): move index.ts to lib folder in currency-provider library
    
    🔧 chore(currency-provider): configure tsconfig for currency-provider library

---
+++ libs/next/currency-provider/src/lib/index.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { getRates } from '@next/currency-cache';
+
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+const CurrencyContext = createContext({ rates: {}, isLoading: true });
+
+export const CurrencyProvider = ({ children }: { children: ReactNode }) => {
+  const [rates, setRates] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getRates()
+      .then((data) => {
+        setRates(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching rates:', error);
+      });
+  }, []);
+
+  return (
+    <CurrencyContext.Provider
+      value={{
+        rates,
+        isLoading,
+      }}
+    >
+      {children}
+    </CurrencyContext.Provider>
+  );
+};
+
+export const useCurrency = () => useContext(CurrencyContext);
