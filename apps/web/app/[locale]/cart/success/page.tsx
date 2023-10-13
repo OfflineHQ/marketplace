5c75e7d7891a203dbb83cd9d276ccf2901e6849d
sebpalluel <sebpalluel@gmail.com>
2023-10-13 11:40:17 +0100

    🐛 fix(page.tsx): redirect to root path instead of '/cart' when user is not KYC validated to improve user experience
    The redirect was changed from '/cart' to '/' in both `cancelled/page.tsx` and `success/page.tsx` files. This change ensures that when a user is not KYC validated, they are redirected to the root path instead of the '/cart' page. This improves the user experience by preventing them from accessing the cart pages when their KYC validation is not complete.

--- apps/web/app/[locale]/cart/success/page.tsx
+++ apps/web/app/[locale]/cart/success/page.tsx
@@ -30,7 +30,7 @@ export default async function CartPurchase({
   params: { locale },
 }: CartSectionProps) {
   const user = await getCurrentUser();
-  if (!isUserKycValidated(user)) redirect('/cart');
+  if (!isUserKycValidated(user)) redirect('/');
 
   return <div>Cart Purchase Successfull</div>;
   // let session = await getStripeActiveCheckoutSession();
