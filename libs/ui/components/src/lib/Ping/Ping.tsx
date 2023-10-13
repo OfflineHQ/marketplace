6ec2410b36b39307a4d5ecc6b26b1299ab8f3a6e
sebpalluel <sebpalluel@gmail.com>
2023-10-13 11:15:03 +0100

    🐛 fix(@cartNav): add console.log statements to log userPassPendingOrders and userPassConfirmedOrders for debugging purposes 🐛 fix(getEventPassOrdersConfirmed.ts): add cache: 'no-store' option to GetEventPassOrdersConfirmed API call to prevent caching until a proper cache invalidation strategy is implemented 🐛 fix(getEventPassPendingOrdersMinimal.tsx): add cache: 'no-store' option to GetEventPassPendingOrdersMinimal API call to prevent caching until a proper cache invalidation strategy is implemented 🐛 fix(next-currency-cache.ts): refactor populateCacheIfEmpty function to only populate cache for EUR currency if it is empty 🐛 fix(Ping.tsx): refactor Ping component to only render number and animation if number is greater than 0

--- libs/ui/components/src/lib/Ping/Ping.tsx
+++ libs/ui/components/src/lib/Ping/Ping.tsx
@@ -17,24 +17,31 @@ export const Ping: React.FC<PingProps> = ({
   const numberSize = isDoubleDigit ? 'h-4 w-4' : 'h-3.5 w-3.5';
   const size = number ? numberSize : 'h-2.5 w-2.5';
   const position = number ? 'top-0.5 right-0.5' : 'top-1 right-1';
+
   return (
     <div className={cn('relative inline-block max-w-fit', className)}>
-      <span
-        className={cn(
-          `absolute z-10 ${position} ${isActive ? 'flex' : 'hidden'} ${size}`
-        )}
-      >
-        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75"></span>
+      {number && number > 0 && (
         <span
           className={cn(
-            `relative inline-flex ${size} items-center justify-center rounded-full bg-primary`
+            `absolute z-10 ${position} flex ${size} items-center justify-center`
           )}
         >
-          {!!number && (
+          <span
+            className={cn(
+              `absolute inline-flex h-full w-full rounded-full bg-primary opacity-75 ${
+                isActive ? 'animate-ping' : ''
+              }`
+            )}
+          ></span>
+          <span
+            className={cn(
+              `relative inline-flex ${size} items-center justify-center rounded-full bg-primary`
+            )}
+          >
             <span className="text-xs text-primary-foreground">{number}</span>
-          )}
+          </span>
         </span>
-      </span>
+      )}
       {children}
     </div>
   );
