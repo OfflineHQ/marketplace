96df54b4a1698e0ab4a9c18c651755c489d89b87
sebpalluel <sebpalluel@gmail.com>
2023-10-18 15:34:24 +0100

    🐛 fix(purchase/page.tsx): remove unused imports and variables to improve code readability and maintainability ✨ feat(purchase/page.tsx): remove unnecessary API calls and data fetching to improve performance and reduce network requests 🔧 chore(index.ts): add missing exports for event API functions to fix import errors 🔧 chore(getEventPassCart.ts): add missing imports and fix typo in variable name 🔧 chore(getEventPassOrderSums.ts): add missing imports and fix typo in variable name 🔧 chore(getEventPassOrdersConfirmedOrCompletedForEventPassId.ts): add missing imports and fix typo in variable name 🐛 fix(PassCardSelect.tsx): fix imports and remove unused imports to improve code readability and maintainability ✨ feat(PassCardSelect.tsx): add support for existing event passes and limit the number of tickets that can be bought per user

---
+++ libs/features/organizer/event-api/src/lib/getEventPassCart.ts
@@ -0,0 +1,35 @@
+import { EventSlugs } from '@features/organizer/event-types';
+import { PassCache } from '@features/pass-cache';
+import { userSdk } from '@gql/user/api';
+import { getCurrentUser } from '@next/next-auth/user';
+import { cache } from 'react';
+
+const passeCache = new PassCache();
+
+interface getEventPassCartProps extends EventSlugs {
+  eventPassId: string;
+}
+
+export const getEventPassCart = cache(
+  async ({ organizerSlug, eventSlug, eventPassId }: getEventPassCartProps) => {
+    const user = await getCurrentUser();
+    if (!user) {
+      return await passeCache.getPassCart({
+        organizerSlug,
+        eventSlug,
+        eventPassId,
+      });
+    }
+    const data = await userSdk.GetEventPassPendingOrderForEventPass(
+      {
+        eventPassId,
+      },
+      {
+        next: {
+          tags: [`getEventPassCart-${eventPassId}`],
+        },
+      },
+    );
+    return data?.eventPassPendingOrder?.[0];
+  },
+);
--- libs/features/organizer/event/src/lib/molecules/PassCard/PassCardSelect.tsx
+++ libs/features/organizer/event/src/lib/molecules/PassCard/PassCardSelect.tsx
@@ -1,57 +1,86 @@
-'use client';
-import { BoundedNumericStepper, ButtonSkeleton, Badge } from '@ui/components';
-import { useTranslations } from 'next-intl';
-import { usePassPurchaseStore } from '../../store/index';
-import { useStore } from '@next/store';
+import { Badge, BoundedNumericStepper, ButtonSkeleton } from '@ui/components';
 
+import {
+  getEventPassCart,
+  getEventPassOrderSums,
+  getEventPassOrdersConfirmedOrCompletedForEventPassId,
+} from '@features/organizer/event-api';
 import type { EventPass, EventSlugs } from '@features/organizer/event-types';
+import { useLocale } from 'next-intl';
+import { getTranslator } from 'next-intl/server';
+import { Suspense } from 'react';
 
 export interface PassCardSelectProps
   extends Omit<EventPass, 'description' | 'name' | 'passOptions' | 'nftImage'>,
     EventSlugs {}
 
-export const PassCardSelect: React.FC<PassCardSelectProps> = ({
+export const PassCardSelect: React.FC<PassCardSelectProps> = (props) => {
+  return (
+    <Suspense fallback={<PassCardSelectSkeleton />}>
+      <PassCardSelectContent {...props} />
+    </Suspense>
+  );
+};
+
+// export interface PassCardSelectContentProps extends PassCardSelectProps {
+//   eventPassOrderSums: EventPassOrderSums;
+//   eventPassCart: EventPassCart | null;
+//   existingEventPass: EventPassCart | null;
+// }
+
+export const PassCardSelectContent: React.FC<PassCardSelectProps> = async ({
   eventPassPricing,
-  eventPassOrderSums,
   organizerSlug,
   eventSlug,
   id,
   ...props
 }) => {
-  const t = useTranslations('Organizer.Event.PassPurchase.Pass');
-  const store = useStore(usePassPurchaseStore, (state) => state);
-  // directly set possible existing passes in the store at page load
-  const getPassCart = usePassPurchaseStore((state) => state.getPassCart);
-
-  const passCart = getPassCart({
+  const locale = useLocale();
+  const t = await getTranslator(locale, 'Organizer.Event.PassPurchase.Pass');
+  const eventPassOrderSums = await getEventPassOrderSums({ eventPassId: id });
+  const eventPassCart = await getEventPassCart({
     organizerSlug,
     eventSlug,
     eventPassId: id,
   });
-  // here mean we are not yet in client side so wait for hydration and localstorage
-  if (!store) {
-    return <PassCardSelectSkeleton />;
-  }
-  const handleOnChange = (newNumTickets: number) => {
-    store.updatePassCart({
-      organizerSlug,
-      eventSlug,
-      pass: {
-        id,
-        amount: newNumTickets,
-      },
+  const existingEventPasses =
+    await getEventPassOrdersConfirmedOrCompletedForEventPassId({
+      eventPassId: id,
     });
-  };
+  // const handleOnChange = (newNumTickets: number) => {
+  //   store.updatePassCart({
+  //     organizerSlug,
+  //     eventSlug,
+  //     pass: {
+  //       eventPassId: id,
+  //       quantity: newNumTickets,
+  //     },
+  //   });
+  // };
 
+  // here compute the max amount of tickets that can be bought
   const totalReserved = eventPassOrderSums?.totalReserved ?? 0;
   const maxAvailableTickets =
     (eventPassPricing?.maxAmount || 0) - totalReserved;
+  // if don't have a limit by customer simply return the max available
   let maxVal = maxAvailableTickets;
   if (
     eventPassPricing?.maxAmountPerUser &&
     eventPassPricing.maxAmountPerUser <= maxAvailableTickets
   ) {
-    maxVal = eventPassPricing.maxAmountPerUser;
+    // check if customer already have some passes bought or in purchase process and deduce it from the max amount per user
+    if (existingEventPasses?.length) {
+      const existingEventPassesSum = existingEventPasses.reduce(
+        (sum, pass) => sum + pass.quantity,
+        0,
+      );
+      maxVal =
+        eventPassPricing.maxAmountPerUser - existingEventPassesSum > 0
+          ? eventPassPricing.maxAmountPerUser - existingEventPassesSum
+          : 0;
+    }
+    // if no existing passes simply return the max amount per user
+    else maxVal = eventPassPricing.maxAmountPerUser;
   }
   return (
     <div className="flex gap-1">
@@ -59,8 +88,8 @@ export const PassCardSelect: React.FC<PassCardSelectProps> = ({
         <Badge variant="secondary">{t('sold-out')}</Badge>
       ) : (
         <BoundedNumericStepper
-          onChange={handleOnChange}
-          initialValue={passCart?.amount || 0}
+          // onChange={handleOnChange}
+          initialValue={eventPassCart?.quantity || 0}
           maxVal={maxVal}
         />
       )}
