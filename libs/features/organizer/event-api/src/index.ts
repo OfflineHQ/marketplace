f98f4eb260eee39c5fe3f227c37ab094fda26925
sebpalluel <sebpalluel@gmail.com>
2023-10-18 14:02:16 +0100

    🐛 fix(purchase/page.tsx): import missing functions and types from event-api to fix compilation errors ✨ feat(purchase/page.tsx): add support for fetching event pass cart and existing event passes to display in the purchase section 📦 chore(event-api): add new function to fetch event pass orders confirmed or completed for specific event pass ids 📦 chore(user/api): add new query to fetch event pass orders confirmed or completed for specific event pass ids

--- libs/features/organizer/event-api/src/index.ts
+++ libs/features/organizer/event-api/src/index.ts
@@ -1,3 +1,4 @@
 export { getEvent } from './lib/getEvent';
+export { getEventPassOrdersConfirmedOrCompletedForEventPassIds } from './lib/getEventPassOrdersConfirmedOrCompletedForEventPassIds';
 export { getEventPasses } from './lib/getEventPasses';
 export { getEventPassesCart } from './lib/getEventPassesCart';
