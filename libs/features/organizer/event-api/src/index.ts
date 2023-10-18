e4877d6c2743e369a37984c3f62960c08b60b8dc
sebpalluel <sebpalluel@gmail.com>
2023-10-18 15:34:24 +0100

    🐛 fix(purchase/page.tsx): remove unused imports and variables to improve code readability and maintainability ✨ feat(purchase/page.tsx): remove unnecessary API calls and data fetching to improve performance and reduce network requests 🔧 chore(index.ts): add missing exports for event API functions to fix import errors 🔧 chore(getEventPassCart.ts): add missing imports and fix typo in variable name 🔧 chore(getEventPassOrderSums.ts): add missing imports and fix typo in variable name 🔧 chore(getEventPassOrdersConfirmedOrCompletedForEventPassId.ts): add missing imports and fix typo in variable name 🐛 fix(PassCardSelect.tsx): fix imports and remove unused imports to improve code readability and maintainability ✨ feat(PassCardSelect.tsx): add support for existing event passes and limit the number of tickets that can be bought per user

--- libs/features/organizer/event-api/src/index.ts
+++ libs/features/organizer/event-api/src/index.ts
@@ -1,4 +1,7 @@
 export { getEvent } from './lib/getEvent';
+export { getEventPassCart } from './lib/getEventPassCart';
+export { getEventPassOrderSums } from './lib/getEventPassOrderSums';
+export { getEventPassOrdersConfirmedOrCompletedForEventPassId } from './lib/getEventPassOrdersConfirmedOrCompletedForEventPassId';
 export { getEventPassOrdersConfirmedOrCompletedForEventPassIds } from './lib/getEventPassOrdersConfirmedOrCompletedForEventPassIds';
 export { getEventPasses } from './lib/getEventPasses';
 export { getEventPassesCart } from './lib/getEventPassesCart';
