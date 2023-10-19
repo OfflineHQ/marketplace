3e6b6f239f674979441eba9946e3c32c81512c49
sebpalluel <sebpalluel@gmail.com>
2023-10-19 11:17:59 +0100

    🚀 feat(purchaseSheet/page.tsx): add new page component for the purchase sheet feature 🚀 feat(purchaseSheet/layout.tsx): add new layout component for the purchase sheet feature 🔥 chore(purchaseSheet/layout.tsx): remove unused layout component for the purchase sheet feature 🔥 chore(purchaseSheet/loading.tsx): remove unused loading component for the purchase sheet feature 🔥 chore(purchaseSheet/page.tsx): remove unused page component for the purchase sheet feature 🔥 chore(purchaseSheet/[...catchAll]/page.tsx): remove unused catchAll component for the purchase sheet feature 🔥 chore(purchaseSheet/default.tsx): remove unused default component for the purchase sheet feature 🔄 refactor(layout.tsx): update layout component to use Sheet component from UI library 🔄 refactor(PassPurchaseSheetContainer.tsx): remove Sheet component wrapper and directly use SheetContent component from UI library 🔄 refactor(safeAuthSetup.tsx): remove console.log statement

---
+++ apps/web/app/[locale]/organizer/[organizerSlug]/event/[eventSlug]/(purchaseSheet)/@purchase/(.)purchase/page.tsx
@@ -0,0 +1,56 @@
+import {
+  PassPurchaseSheet,
+  PassPurchaseSheetContainer,
+  PassPurchaseSheetSkeleton,
+} from '@features/organizer/event';
+import { getEventPasses } from '@features/organizer/event-api';
+import { getTranslator } from 'next-intl/server';
+import { Suspense } from 'react';
+import EventSection from '../../../page';
+
+interface PurchaseSectionProps {
+  params: {
+    eventSlug: string;
+    organizerSlug: string;
+    locale: string;
+  };
+}
+
+export default function PurchaseSection({ params }: PurchaseSectionProps) {
+  return (
+    <>
+      <EventSection params={params} />
+      <PassPurchaseSheetContainer>
+        <Suspense fallback={<PassPurchaseSheetSkeleton />}>
+          <PurchaseSectionContent {...params} />
+        </Suspense>
+      </PassPurchaseSheetContainer>
+    </>
+  );
+}
+
+interface PurchaseSectionContentProps {
+  eventSlug: string;
+  organizerSlug: string;
+  locale: string;
+}
+
+export const PurchaseSectionContent: React.FC<
+  PurchaseSectionContentProps
+> = async ({ eventSlug, organizerSlug, locale }) => {
+  const t = await getTranslator(locale, 'Organizer.Event.PassPurchase');
+  const passes = await getEventPasses({ eventSlug, locale });
+  return (
+    <PassPurchaseSheet
+      passes={passes}
+      organizerSlug={organizerSlug}
+      eventSlug={eventSlug}
+      size={'lg'}
+      title={t('title')}
+      description={t('description')}
+      goPaymentText={t('Footer.purchase-button')}
+      goPaymentLink={{ href: '/cart' }}
+      backButtonText={t('go-back-button')}
+    />
+  );
+};
