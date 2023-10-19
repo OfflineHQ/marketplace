2d13f5e582446365e4f960df5c89cd627a30e591
sebpalluel <sebpalluel@gmail.com>
2023-10-19 18:31:17 +0100

    🔧 chore(tsconfig): remove unused paths and update lib configuration
    🔧 chore(server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics
    
    🔥 refactor(cart): remove unused files and dependencies
    
    🔥 refactor(EventPasses): remove unused components and dependencies
    
    🔥 refactor(useEventPassOrders): remove unused imports and dependencies

--- libs/features/cart/src/index.ts
+++ libs/features/cart/src/index.ts
@@ -1,3 +1,1 @@
-export { EventPassesAnonymous } from './lib/EventPasses/EventPassesAnonymous';
-export { EventPassesUser } from './lib/EventPasses/EventPassesUser';
 export { EventPassListSkeleton } from './lib/EventPassList/EventPassList';
