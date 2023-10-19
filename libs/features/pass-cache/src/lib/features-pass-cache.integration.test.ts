eea995622bcd5636dee27a579e98cd4bccf6aef4
sebpalluel <sebpalluel@gmail.com>
2023-10-19 18:52:33 +0100

    🔥 refactor(server.ts): remove unused import and variable 'port' to improve code readability and maintainability 🔥 refactor(createEventPassesSlice.ts): remove unused imports and variables to improve code readability and maintainability 🔥 refactor(passPurchaseStore.ts): remove unused import and variable 'createDbClient' to improve code readability and maintainability 🔥 refactor(features-pass-cache.integration.test.ts): remove unused import to improve code readability and maintainability 🔥 refactor(tsconfig.lib.json): remove unused include path to improve build performance 🔥 refactor(eventPassPendingOrder.spec.ts): remove unused imports and variables to improve code readability and maintainability 🔥 refactor(payment-admin.integration.test.ts): remove unused import to improve code readability and maintainability 🔥 refactor(tsconfig.base.json): remove unused paths to improve build performance

--- libs/features/pass-cache/src/lib/features-pass-cache.integration.test.ts
+++ libs/features/pass-cache/src/lib/features-pass-cache.integration.test.ts
@@ -1,4 +1,3 @@
-import { getUnauthenticatedUserCookie } from '@next/next-auth/user';
 import { PassCache } from './features-pass-cache';
 
 jest.mock('@next/next-auth/user');
@@ -9,18 +8,14 @@ describe.skip('PassCache Integration Test', () => {
   beforeEach(() => {
     passCache = new PassCache();
   });
-
-  it('should get, set, and delete pass cart', async () => {
-    const userId = 'test-user-id';
-    const passCart = { id: 'test-pass-cart-id', amount: 1 };
-    (getUnauthenticatedUserCookie as jest.Mock).mockReturnValue(userId);
-
-    await passCache.setPassCart(passCart);
-    let result = await passCache.getPassCart();
-    expect(result).toEqual(passCart);
-
-    await passCache.deletePassCart();
-    result = await passCache.getPassCart();
-    expect(result).toBeNull();
+  it('should get passes cart', async () => {
+    const passesCart = await passCache.getPassesCart({
+      organizerSlug: 'test-organizer',
+      eventSlug: 'test-event',
+    });
+    expect(passesCart).toEqual({
+      eventPasses: [],
+      total: 0,
+    });
   });
 });
