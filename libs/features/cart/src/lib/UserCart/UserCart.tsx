de9a03aec5b58eacf71d3e473af49aebeed0bf59
sebpalluel <sebpalluel@gmail.com>
2023-10-19 17:07:16 +0100

    🔥 refactor(server.ts): remove unused imports and exports to improve code cleanliness and organization

--- libs/features/cart/src/lib/UserCart/UserCart.tsx
+++ libs/features/cart/src/lib/UserCart/UserCart.tsx
@@ -1,23 +1,50 @@
-import { UserCartSection, type UserCartSectionProps } from './UserCartSection';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardOverflow,
+  CardOverlay,
+  CardTitle,
+} from '@ui/components';
+import { useTranslations } from 'next-intl';
+import React from 'react';
+import {
+  EventPassesCart,
+  type EventPassesCartProps,
+} from '../EventPassesCart/EventPassesCart';
 
-export type UserCartProps = UserCartSectionProps;
-
-export async function UserCart({
-  EventPassesFetcher,
-  locale,
-  noCartImage,
-  user,
-  children,
-  userPassPendingOrders,
-}: UserCartProps) {
-  return (
-    <UserCartSection
-      EventPassesFetcher={EventPassesFetcher}
-      userPassPendingOrders={userPassPendingOrders}
-      noCartImage={noCartImage}
-      user={user}
-      locale={locale}
-      children={children}
-    />
-  );
+export interface UserCartProps extends EventPassesCartProps {
+  children: React.ReactNode;
 }
+
+export const UserCart: React.FC<UserCartProps> = ({
+  userPassPendingOrders,
+  noCartImage,
+  children,
+}) => {
+  const t = useTranslations('Cart.UserCart');
+  return (
+    <section className="container">
+      <Card variant="stickyFooter" noBorder>
+        <CardOverflow>
+          <CardHeader>
+            <CardTitle>{t('title')}</CardTitle>
+            <CardDescription>{t('description')}</CardDescription>
+          </CardHeader>
+          <CardContent className="px-1">
+            <EventPassesCart
+              userPassPendingOrders={userPassPendingOrders}
+              noCartImage={noCartImage}
+            />
+          </CardContent>
+        </CardOverflow>
+        <CardOverlay />
+        <CardFooter className="justify-center" variant="sticky">
+          {children}
+        </CardFooter>
+      </Card>
+    </section>
+  );
+};
