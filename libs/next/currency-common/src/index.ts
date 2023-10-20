3439167567e8c18995132bc0c9ffd3d1392f8ccd
Alexandre Gouasmi <agouasmi@offline.live>
2023-10-20 12:19:36 +0200

    fix: currency cookies

--- libs/next/currency-common/src/index.ts
+++ libs/next/currency-common/src/index.ts
@@ -1,4 +1,3 @@
 export { formatCurrency } from './lib/formatCurrency';
 export { getCurrencyPreference } from './lib/getCurrencyPreference';
-export { setCurrencyPreference } from './lib/setCurrencyPreference';
 export { toUserCurrency } from './lib/toUserCurrency';
--- libs/next/currency-common/src/lib/getCurrencyPreference.ts
+++ libs/next/currency-common/src/lib/getCurrencyPreference.ts
@@ -1,10 +1,11 @@
 import { defaultCurrency } from '@currency/types';
 import { Currency_Enum } from '@gql/shared/types';
-import { getCookie } from 'cookies-next';
+import { cookies } from 'next/headers';
 
 // Get user's currency preference from a cookie
 export const getCurrencyPreference = (): Currency_Enum => {
   return (
-    (getCookie('NEXT_CURRENCY') as unknown as Currency_Enum) || defaultCurrency
+    (cookies().get('NEXT_CURRENCY')?.value as unknown as Currency_Enum) ||
+    defaultCurrency
   );
 };
