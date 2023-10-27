4ba135b70aaca54e6ec2ff5361806eadc1680542
sebpalluel <sebpalluel@gmail.com>
2023-10-27 15:06:41 +0100

    🚀 feat(not-found.tsx): replace custom Not Found component with UINotFound component from @features/navigation to improve code reusability and maintainability
    🚀 feat(navigation): add new library @features/navigation to handle navigation-related features
    
    📝 docs(navigation): add README.md file to provide documentation for @features/navigation library
    
    🚀 feat(navigation): add Jest configuration file jest.config.ts for unit testing
    
    🚀 feat(navigation): add package.json file for @features/navigation library
    
    🚀 feat(navigation): add project.json file for @features/navigation library
    
    🚀 feat(navigation): add index.ts file to export NotFound component from @features/navigation library
    
    🚀 feat(NotFound.stories.tsx): add storybook example for NotFound component from @features/navigation library
    
    🚀 feat(NotFound.tsx): create NotFound component in @features/navigation library to display a not found error message and a button to go back to the homepage
    
    🚀 feat(examples.tsx): add example component to showcase NotFound component from @features/navigation library
    
    🚀 feat(tsconfig.json): add tsconfig.json file for @features/navigation library
    
    🚀 feat(tsconfig.lib.json): add tsconfig.lib.json file for @features/navigation library
    
    🚀 feat(tsconfig.spec.json): add tsconfig.spec.json file for @features/navigation library
    
    🚀 feat(en.json): add translation for 404-not-found and back-to-home in English language
    
    🚀 feat(fr.json): add translation for 404-not-found and back-to-home in French language
    
    🚀 feat(tsconfig.base.json): add path mappings for @features/navigation library

--- apps/web/app/[locale]/not-found.tsx
+++ apps/web/app/[locale]/not-found.tsx
@@ -1,13 +1,5 @@
-// import Image from "next/image";
-// import NotFound from "../public/not-found.png";
+import { NotFound as UINotFound } from '@features/navigation';
 
 export default function NotFound() {
-  return (
-    <section className="container">
-      {/* TODO add traduction and 404 image + button to come back to last route */}
-      {/* <Image src={NotFound} alt='Not found error graphic' /> */}
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-    </section>
-  );
+  return <UINotFound />;
 }
