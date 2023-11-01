98eca848f25fff6c3066bb051b8430a525f9f04f
sebpalluel <sebpalluel@gmail.com>
2023-11-01 15:55:59 +0000

    🚀 feat(default.tsx): add new component ProfileNavSection to handle profile navigation in the app 🚀 feat(layout.tsx): add CurrencyProvider to provide currency context to the app 🔥 chore(ProfileNavClient.tsx): remove unused component ProfileNavClient 🔧 chore(next.config.js): update import paths for appNav related features

---
+++ apps/back-office/app/[locale]/@profileNav/default.tsx
@@ -0,0 +1,27 @@
+import { isConnected } from '@next/next-auth/user';
+import { useLocale } from 'next-intl';
+import { getTranslator } from 'next-intl/server';
+import { ProfileNavClient } from '@features/app-nav';
+
+export default async function ProfileNavSection() {
+  const locale = useLocale();
+  const t = await getTranslator(locale, 'AppNav.Profile');
+  const isNextAuthConnected = await isConnected();
+  return (
+    <ProfileNavClient
+      signInText={t('sign-in')}
+      profileSectionsText={{
+        myAccount: t('sections-text.my-account'),
+        support: t('sections-text.support'),
+        supportTitle: t('sections-text.support-title'),
+        supportDescription: t('sections-text.support-description'),
+        signOut: t('sections-text.sign-out'),
+        signOutTitle: t('sections-text.sign-out-title'),
+        signOutDescription: t('sections-text.sign-out-description'),
+        signIn: t('sections-text.sign-in'),
+        settings: t('sections-text.settings'),
+      }}
+      isNextAuthConnected={isNextAuthConnected}
+    />
+  );
+}
