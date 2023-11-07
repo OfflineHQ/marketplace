06ecefd09420659a4f0774e3db9a43325ff8f618
Alexandre Gouasmi <agouasmi@offline.live>
2023-11-07 10:50:21 +0100

    add: generateMetadata in page.tsx for organizer/organizerSlug/event/eventSlug

--- apps/web/app/[locale]/organizer/[organizerSlug]/event/[eventSlug]/page.tsx
+++ apps/web/app/[locale]/organizer/[organizerSlug]/event/[eventSlug]/page.tsx
@@ -1,13 +1,77 @@
+import { NotFound } from '@features/navigation';
 import { Event } from '@features/organizer/event';
 import { getEvent } from '@features/organizer/event-api';
 import type { Event as TEvent } from '@features/organizer/event-types';
+import { getNextAppURL } from '@shared/server';
+import type { Metadata } from 'next';
 import { useTranslations } from 'next-intl';
-import { NotFound } from '@features/navigation';
+
 interface EventSectionProps {
   params: {
     eventSlug: string;
     organizerSlug: string;
     locale: string;
+  };
+}
+
+export async function generateMetadata({
+  params,
+}: EventSectionProps): Promise<Metadata> {
+  const { eventSlug, organizerSlug, locale } = params;
+
+  const event = await getEvent({ eventSlug, locale });
+
+  if (!event || event.organizer?.slug !== organizerSlug) {
+    return {
+      title: 'Offline',
+      description:
+        'We offers a user-friendly experience, removing the complexity of digital asset creation, sale, and management. ​',
+    };
+  }
+  const firstParagraph = event.description.json.children.find(
+    (child) => child.type === 'paragraph',
+  );
+
+  return {
+    title: event.title,
+    description: firstParagraph.children[0].text,
+    openGraph: {
+      title: event.title,
+      description: firstParagraph.children[0].text,
+      url: `${getNextAppURL()}/${locale}/organizer/${organizerSlug}/event/${eventSlug}`,
+      siteName: 'Offline',
+      images: [
+        {
+          url: event.heroImage.url,
+          ...(event.heroImage.width ? { width: event.heroImage.width } : {}),
+          ...(event.heroImage.height ? { height: event.heroImage.height } : {}),
+          alt: event.title,
+        },
+      ],
+      locale: 'en',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      images: [
+        {
+          url: event.heroImage.url,
+          ...(event.heroImage.width ? { width: event.heroImage.width } : {}),
+          ...(event.heroImage.height ? { height: event.heroImage.height } : {}),
+          alt: event.title,
+        },
+      ],
+      title: event.title,
+      description: firstParagraph.children[0].text,
+      creator: '@offline_live',
+    },
+    alternates: {
+      canonical: `${getNextAppURL()}/${locale}/organizer/${organizerSlug}/event/${eventSlug}`,
+      languages: {
+        en: `${getNextAppURL()}/en/organizer/${organizerSlug}/event/${eventSlug}`,
+        fr: `${getNextAppURL()}/fr/organizer/${organizerSlug}/event/${eventSlug}`,
+      },
+    },
   };
 }
 
