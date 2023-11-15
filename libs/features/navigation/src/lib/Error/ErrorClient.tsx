9eaadb4631859e831827af32edb8b09727de51b3
sebpalluel <sebpalluel@gmail.com>
2023-11-15 13:19:41 +0100

    🔧 chore(global-error.tsx): remove unused import and console.log statements to improve code cleanliness and readability 🔧 chore(global-error.tsx): remove unused import to improve code cleanliness and readability 🔧 chore(ErrorClient.tsx): comment out Alert component to fix breaking issue in next apps 🔧 chore(ErrorClient.tsx): refactor Alert component to a div to fix breaking issue in next apps

--- libs/features/navigation/src/lib/Error/ErrorClient.tsx
+++ libs/features/navigation/src/lib/Error/ErrorClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Alert, Button, Card, CardContent, CardFooter } from '@ui/components';
+import { Button, Card, CardContent, CardFooter } from '@ui/components';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
@@ -18,10 +18,15 @@ export function ErrorClient({ error, reset, customMessage }: ErrorProps) {
         <div className="relative h-80 w-80 grow">
           <Image fill src="/error.svg" alt={t('error-message')} />
         </div>
-        <Alert variant="destructive" className="w-fit">
+        {/* IMPORTANT: no idea why but putting Alert in this component is breaking the next apps with `TypeError: Cannot destructure property 'parallelRouterKey' of 'param' as it is null.` */}
+        {/* <Alert variant="destructive" className="w-fit">
           {t('error-message')}
           {customMessage && <p>{customMessage}</p>}
-        </Alert>
+        </Alert> */}
+        <div className="w-fit">
+          {t('error-message')}
+          {customMessage && <p>{customMessage}</p>}
+        </div>
       </CardContent>
       <CardFooter className="flex flex-col items-center" variant="sticky">
         <Button
