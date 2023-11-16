92095000c068a985ed1af9dbd8f8aec37300e155
sebpalluel <sebpalluel@gmail.com>
2023-11-16 15:56:55 +0100

    📦 chore(DataTable): add DataTable.stories.tsx file to showcase the DataTable component 🐛 fix(DataTableRowActions): remove unused import and remove unused row prop from DataTableRowActions component 📦 chore(DataTable): add columns.tsx file to define the columns for the DataTable component 📦 chore(DataTable): add data.tsx file to provide example data for the DataTable component 📦 chore(DataTable): add schema.ts file to define the schema for the example data in the DataTable component 📦 chore(DataTable): add tasks.tsx file to provide example tasks data for the DataTable component 📝 docs(tsconfig): exclude examples files from the build and test process

---
+++ libs/ui/components/src/lib/data-table/examples/columns.tsx
@@ -0,0 +1,179 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+
+import { Badge } from '../../badge/Badge';
+import { Checkbox } from '../../checkbox/Checkbox';
+
+import {
+  DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+} from '@radix-ui/react-dropdown-menu';
+import { DropdownMenuShortcut } from '../../dropdown-menu/DropdownMenu';
+import { DataTableColumnHeader } from '../DataTableColumnHeader';
+import { DataTableRowActions } from '../DataTableRowActions';
+import { labels, priorities, statuses } from './data/data';
+import { Task, taskSchema } from './data/schema';
+
+const controlText = {
+  asc: 'Sort ascending',
+  desc: 'Sort descending',
+  hide: 'Hide column',
+};
+
+export const columns: ColumnDef<Task>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'id',
+    header: ({ column }) => (
+      <DataTableColumnHeader
+        column={column}
+        title="Task"
+        controlText={controlText}
+      />
+    ),
+    cell: ({ row }) => <div className="w-[80px]">{row.getValue('id')}</div>,
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'title',
+    header: ({ column }) => (
+      <DataTableColumnHeader
+        column={column}
+        title="Title"
+        controlText={controlText}
+      />
+    ),
+    cell: ({ row }) => {
+      const label = labels.find((label) => label.value === row.original.label);
+
+      return (
+        <div className="flex space-x-2">
+          {label && <Badge variant="outline">{label.label}</Badge>}
+          <span className="max-w-[500px] truncate font-medium">
+            {row.getValue('title')}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: ({ column }) => (
+      <DataTableColumnHeader
+        column={column}
+        title="Status"
+        controlText={controlText}
+      />
+    ),
+    cell: ({ row }) => {
+      const status = statuses.find(
+        (status) => status.value === row.getValue('status'),
+      );
+
+      if (!status) {
+        return null;
+      }
+
+      return (
+        <div className="flex w-[100px] items-center">
+          {status.icon && (
+            <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+          )}
+          <span>{status.label}</span>
+        </div>
+      );
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+  {
+    accessorKey: 'priority',
+    header: ({ column }) => (
+      <DataTableColumnHeader
+        column={column}
+        title="Priority"
+        controlText={controlText}
+      />
+    ),
+    cell: ({ row }) => {
+      const priority = priorities.find(
+        (priority) => priority.value === row.getValue('priority'),
+      );
+
+      if (!priority) {
+        return null;
+      }
+
+      return (
+        <div className="flex items-center">
+          {priority.icon && (
+            <priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+          )}
+          <span>{priority.label}</span>
+        </div>
+      );
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const task = taskSchema.parse(row.original);
+      return (
+        <DataTableRowActions controlText={{ openMenu: 'Open' }}>
+          <DropdownMenuItem>Edit</DropdownMenuItem>
+          <DropdownMenuItem>Make a copy</DropdownMenuItem>
+          <DropdownMenuItem>Favorite</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Labels</DropdownMenuSubTrigger>
+            <DropdownMenuSubContent>
+              <DropdownMenuRadioGroup value={task.label}>
+                {labels.map((label) => (
+                  <DropdownMenuRadioItem key={label.value} value={label.value}>
+                    {label.label}
+                  </DropdownMenuRadioItem>
+                ))}
+              </DropdownMenuRadioGroup>
+            </DropdownMenuSubContent>
+          </DropdownMenuSub>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            Delete
+            <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DataTableRowActions>
+      );
+    },
+  },
+];
