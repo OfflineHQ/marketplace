ce5ce7efbe661cc49c88b3d33bcaa9d015387d77
sebpalluel <sebpalluel@gmail.com>
2023-11-17 17:56:29 +0100

    🐛 fix(AppNavLayout.tsx): add overflow-hidden to prevent content overflow on smaller screens ✨ feat(AppNavLayout.tsx): add support for process.env.PORT environment variable to be able to run app on a configurable port 🐛 fix(AppNavLayoutMobile.tsx): add flex to fix layout issue on mobile screens 🐛 fix(SinglePass.tsx): add variant prop to CardContent to handle sticky footer behavior 🐛 fix(Card.tsx): adjust sticky footer styles to fix layout issue on smaller screens

--- libs/features/app-nav/src/lib/AppNavLayout/AppNavLayout.tsx
+++ libs/features/app-nav/src/lib/AppNavLayout/AppNavLayout.tsx
@@ -12,10 +12,10 @@ export function AppNavLayout(props: AppNavLayoutProps) {
   const { children, ...appNavLayout } = props;
 
   return (
-    <div className="flex h-full w-full flex-col-reverse md:flex-col">
+    <div className="flex h-full w-full flex-col-reverse overflow-hidden md:flex-col">
       {/* Navigation for larger screens */}
       <AppNavLayoutDesktop {...appNavLayout} />
-      <div className="flex h-full w-full md:mb-0">{children}</div>
+      <div className="flex h-full w-full">{children}</div>
       {/* Navigation for mobile screens */}
       <AppNavLayoutMobile {...appNavLayout} />
     </div>
