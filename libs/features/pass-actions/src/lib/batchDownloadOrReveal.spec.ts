470237c7b120323305a8bc98f6a09c6778025629
Alexandre Gouasmi <agouasmi@offline.live>
2023-11-27 11:33:02 +0100

    fix: tests for batchDownloadOrReveal

--- libs/features/pass-actions/src/lib/batchDownloadOrReveal.spec.ts
+++ libs/features/pass-actions/src/lib/batchDownloadOrReveal.spec.ts
@@ -1,6 +1,7 @@
-import { batchDownloadOrReveal } from './batchDownloadOrReveal';
+import * as Bytescale from '@bytescale/sdk';
 import { revealPass } from '@features/pass-api';
 import { revalidateTag } from 'next/cache';
+import { batchDownloadOrReveal } from './batchDownloadOrReveal';
 
 jest.mock('next/cache', () => ({
   revalidateTag: jest.fn(),
@@ -8,7 +9,18 @@ jest.mock('next/cache', () => ({
 
 jest.mock('@features/pass-api', () => ({
   revealPass: jest.fn(),
-  downloadPass: jest.fn(),
+}));
+
+jest.mock('@bytescale/sdk', () => ({
+  AuthManager: {
+    beginAuthSession: jest.fn(),
+    endAuthSession: jest.fn(),
+  },
+  FileApi: jest.fn().mockImplementation(() => ({
+    downloadFile: jest.fn().mockResolvedValue({
+      blob: jest.fn().mockResolvedValue(new Blob()),
+    }),
+  })),
 }));
 
 describe('batchDownloadOrReveal', () => {
@@ -16,70 +28,61 @@ describe('batchDownloadOrReveal', () => {
     jest.resetAllMocks();
   });
 
-  it('should call revealPass for unrevealed eventPassNfts', async () => {
+  it('should call revealPass for unrevealed eventPassNfts and downloadPass for all eventPassNfts', async () => {
     const eventPassNfts = [
-      { id: '1', isRevealed: false },
-      { id: '2', isRevealed: true },
+      { id: '1', isRevealed: false, tokenId: '1001' },
+      { id: '2', isRevealed: true, tokenId: '1002' },
     ];
 
     (revealPass as jest.Mock).mockResolvedValue({});
-    (downloadPass as jest.Mock).mockResolvedValue({});
 
-    await batchDownloadOrReveal(eventPassNfts);
+    await batchDownloadOrReveal('slug', eventPassNfts);
 
     expect(revealPass).toHaveBeenCalledWith('1');
-    expect(downloadPass).toHaveBeenCalledWith('2');
-    expect(revalidateTag).toHaveBeenCalled();
+    expect(revalidateTag).toHaveBeenCalledWith('userEventPassNfts');
   });
 
-  it("shouldn't call revalidateTag for downloadPass", async () => {
+  it("shouldn't call revealPass or revalidateTag for already revealed eventPassNfts", async () => {
     const eventPassNfts = [
-      { id: '1', isRevealed: true },
-      { id: '2', isRevealed: true },
+      { id: '1', isRevealed: true, tokenId: '1001' },
+      { id: '2', isRevealed: true, tokenId: '1002' },
     ];
 
     (revealPass as jest.Mock).mockResolvedValue({});
-    (downloadPass as jest.Mock).mockResolvedValue({});
 
-    await batchDownloadOrReveal(eventPassNfts);
+    await batchDownloadOrReveal('slug', eventPassNfts);
 
     expect(revealPass).not.toHaveBeenCalled();
-    expect(downloadPass).toHaveBeenCalledWith('1');
-    expect(downloadPass).toHaveBeenCalledWith('2');
     expect(revalidateTag).not.toHaveBeenCalled();
   });
 
   it('should handle errors from revealPass', async () => {
     const eventPassNfts = [
-      { id: '1', isRevealed: false },
-      { id: '2', isRevealed: true },
+      { id: '1', isRevealed: false, tokenId: '1001' },
+      { id: '2', isRevealed: true, tokenId: '1002' },
     ];
 
     const error = new Error('FileNotFound');
     (revealPass as jest.Mock).mockRejectedValue(error);
-    (downloadPass as jest.Mock).mockResolvedValue({});
 
-    await expect(batchDownloadOrReveal(eventPassNfts)).rejects.toThrow(
+    await expect(batchDownloadOrReveal('slug', eventPassNfts)).rejects.toThrow(
       'FileNotFound',
     );
 
     expect(revalidateTag).not.toHaveBeenCalled();
   });
 
-  it('should handle errors from downloadPass', async () => {
+  it('should call Bytescale.AuthManager.beginAuthSession and Bytescale.AuthManager.endAuthSession', async () => {
     const eventPassNfts = [
-      { id: '1', isRevealed: false },
-      { id: '2', isRevealed: true },
+      { id: '1', isRevealed: false, tokenId: '1001' },
+      { id: '2', isRevealed: true, tokenId: '1002' },
     ];
 
-    const error = new Error('FileNotFound');
     (revealPass as jest.Mock).mockResolvedValue({});
-    (downloadPass as jest.Mock).mockRejectedValue(error);
 
-    await expect(batchDownloadOrReveal(eventPassNfts)).rejects.toThrow(
-      'FileNotFound',
-    );
+    await batchDownloadOrReveal('slug', eventPassNfts);
 
-    expect(revalidateTag).not.toHaveBeenCalled();
+    expect(Bytescale.AuthManager.beginAuthSession).toHaveBeenCalled();
+    expect(Bytescale.AuthManager.endAuthSession).toHaveBeenCalled();
   });
 });
