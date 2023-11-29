0ccfdc4c3db39aebd3dae6aa721912ae18392d2c
sebpalluel <sebpalluel@gmail.com>
2023-11-29 21:38:33 +0000

    🔧 chore(lint-staged.config.js): update lint-staged config to use pnpm command for format:check instead of nx affected command

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '{apps,libs,tools}/**/*.{js,ts,tsx,json,sql,gql,graphql}': (files) => {
     const fileList = files.join(',');
     return [
-      `nx affected --target=typecheck --files=${fileList}`,
+      `pnpm nx format:check --files=${fileList}`,
       `nx affected:lint --fix --parallel=6 --eslintConfig=.eslintrc.ci.json --files=${fileList}`,
       `nx format:write --files=${fileList}`,
     ];
