de98c8d52ecd9b08168da67941d4abc0d89b31a5
sebpalluel <sebpalluel@gmail.com>
2023-11-30 09:43:07 +0000

    🔧 chore(.eslintrc.json): enable @typescript-eslint/no-unused-vars rule with warn severity to catch unused variables 🔧 chore(.gitignore): remove trailing slash from playwright-report entry to match the actual directory name 🔧 chore(lint-staged.config.js): reorder lint-staged commands to first format files, then run lint with fix, and finally write the formatted files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,9 +2,8 @@ module.exports = {
   '{apps,libs,tools}/**/*.{js,ts,tsx,json,sql,gql,graphql}': (files) => {
     const fileList = files.join(',');
     return [
-      `pnpm nx format:check --files=${fileList}`,
-      `nx affected:lint --fix --parallel=6 --eslintConfig=.eslintrc.ci.json --files=${fileList}`,
-      `nx format:write --files=${fileList}`,
+      `pnpm nx format:write --files=${fileList}`,
+      `pnpm nx affected:lint --fix --parallel=6 --eslintConfig=.eslintrc.ci.json --files=${fileList}`,
     ];
   },
 };
