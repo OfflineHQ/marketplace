c487e4b6f6b4d9ab1000611d7df76b4530736f3f
sebpalluel <sebpalluel@gmail.com>
2023-12-04 18:13:55 +0000

    📦 chore(EventPassTimeBeforeDeletion): add EventPassTimeBeforeDeletion component 📦 chore(EventPassTimeBeforeDeletionClient): add EventPassTimeBeforeDeletionClient component 🔨 refactor(EventPasses): remove unused import and refactor layout styles 🔨 refactor(EventPasses): move EventPassTimeBeforeDeletion component to AccordionContentWrapper 🔨 refactor(EventPasses): pass timeRemainingDeletion prop to AccordionContentWrapper 🔨 refactor(EventPasses): remove unused imports and functions 🌐 chore(i18n): add translation for time-before-deletion in en.json and fr.json 📦 chore(icons): add Timer icon component

---
+++ libs/features/cart/src/lib/EventPassTimeBeforeDeletion/EventPassTimeBeforeDeletionClient.tsx
@@ -0,0 +1,33 @@
+'use client';
+import { Badge } from '@ui/components';
+import { Timer } from '@ui/icons';
+import { useFormatter, useNow, useTranslations } from 'next-intl';
+
+export type EventPassTimeBeforeDeletionClientProps = {
+  timeBeforeDelete: number;
+  created_at: string;
+};
+
+export const EventPassTimeBeforeDeletionClient: React.FC<
+  EventPassTimeBeforeDeletionClientProps
+> = ({ timeBeforeDelete, created_at }) => {
+  const t = useTranslations('Cart.List.Event.TimeBeforeDeletion');
+  const now = useNow({
+    // Update every 10 seconds
+    updateInterval: 1000 * 10,
+  });
+  const format = useFormatter();
+
+  // Calculate the deletion time
+  const deletionTime = new Date(
+    new Date(created_at).getTime() + timeBeforeDelete * 1000,
+  );
+
+  return (
+    <Badge variant="orange" icon={<Timer />}>
+      {t('time-before-deletion', {
+        relativeTime: format.relativeTime(deletionTime, now),
+      })}
+    </Badge>
+  );
+};
