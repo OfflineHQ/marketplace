3467b94fad2017dd89dc015819837b65f407241d
sebpalluel <sebpalluel@gmail.com>
2023-12-05 17:25:25 +0000

    📝 chore(public_eventPassNftContract.yaml): add new columns 'isAirdrop', 'isDelayedRevealed', and 'type' to the select permissions 📝 feat(public_eventPassNftContractType.yaml): add new table 'eventPassNftContractType' to store contract types 📝 chore(tables.yaml): include 'public_eventPassNftContractType.yaml' in the list of tables 📝 feat(1701796170434_eventPassNftContractType_update_eventPassNftContract/up.sql): create 'eventPassNftContractType' table and add new columns to 'eventPassNftContract' table 📝 fix(1701796170434_eventPassNftContractType_update_eventPassNftContract/down.sql): add down migration for creating 'eventPassNftContractType' table and adding new columns to 'eventPassNftContract' table 📝 chore(4_eventPassNftContract.sql): insert new records with 'normal' type into 'eventPassNftContract' table
    🔧 chore(eventPassNftContractType_update_eventPassNftContract): add new columns to eventPassNftContract table
    📝 docs(eventPassNftContractType_update_eventPassNftContract): update eventPassNftContract table to include new columns for improved functionality
    
    🔧 chore(build-and-e2e-web-tests.yml): set fetch-depth to 0 to improve performance and ensure full repository history is available for checkout
    🔧 chore(ci.yml): refactor CI workflow to simplify and improve readability, remove unused environment variables, and update job names and steps for clarity and consistency

--- hasura/app/seeds/default/4_eventPassNftContract.sql
+++ hasura/app/seeds/default/4_eventPassNftContract.sql
@@ -1,40 +1,65 @@
 SET
-	check_function_bodies = false;
+    check_function_bodies = false;
 
-INSERT INTO
-	public."eventPassNftContract" (
-		id,
-		"chainId",
-		"contractAddress",
-		"eventId",
-		"eventPassId",
-		"organizerId"
-	)
+-- Insert first record with 'normal' type
+INSERT INTO public."eventPassNftContract" (
+    id,
+    "chainId",
+    "contractAddress",
+    "eventId",
+    "eventPassId",
+    "organizerId",
+    "type",
+    "password",
+    "isDelayedRevealed",
+    "isAirdrop",
+    "created_at",
+    "updated_at"
+)
 VALUES
-	(
-		'19d09cfe-6a5c-4481-a822-7161d5b4c8cf',
-		'5',
-		'0xfakecontractaddress1',
-		'clizzpvidao620buvxit1ynko',
-		'clj8raobj7g8l0aw3bfw6dny4',
-		'clizzky8kap2t0bw7wka9a2id'
-	);
+    (
+        '19d09cfe-6a5c-4481-a822-7161d5b4c8cf',
+        '5',
+        '0xfakecontractaddress1',
+        'clizzpvidao620buvxit1ynko',
+        'clj8raobj7g8l0aw3bfw6dny4',
+        'clizzky8kap2t0bw7wka9a2id',
+        'normal',
+        NULL,
+        false,
+        false,
+        '2023-07-19 12:58:46.636737+00',
+        '2023-07-19 12:58:46.636737+00'
+    );
 
-INSERT INTO
-	public."eventPassNftContract" (
-		id,
-		"chainId",
-		"contractAddress",
-		"eventId",
-		"eventPassId",
-		"organizerId"
-	)
+-- Insert second record with 'normal' type
+INSERT INTO public."eventPassNftContract" (
+    id,
+    "chainId",
+    "contractAddress",
+    "eventId",
+    "eventPassId",
+    "organizerId",
+    "type",
+    "password",
+    "isDelayedRevealed",
+    "isAirdrop",
+    "created_at",
+    "updated_at"
+)
 VALUES
-	(
-		'6fa834c5-cc89-4861-a1cf-ff4fce62ba20',
-		'5',
-		'0xfakecontractaddress2',
-		'clizzpvidao620buvxit1ynko',
-		'fake-event-pass-2',
-		'clizzky8kap2t0bw7wka9a2id'
-	);
+    (
+        '6fa834c5-cc89-4861-a1cf-ff4fce62ba20',
+        '5',
+        '0xfakecontractaddress2',
+        'clizzpvidao620buvxit1ynko',
+        'fake-event-pass-2',
+        'clizzky8kap2t0bw7wka9a2id',
+        'normal',
+        NULL,
+        false,
+        false,
+        '2023-07-19 15:58:46.636737+00',
+        '2023-07-19 15:58:46.636737+00'
+    );
+;
