7eb54b3861d410585b715f9d9fe75da0eee094ce
sebpalluel <sebpalluel@gmail.com>
2023-12-05 17:25:25 +0000

    📝 chore(public_eventPassNftContract.yaml): add new columns 'isAirdrop', 'isDelayedRevealed', and 'type' to the select permissions 📝 feat(public_eventPassNftContractType.yaml): add new table 'eventPassNftContractType' to store contract types 📝 chore(tables.yaml): include 'public_eventPassNftContractType.yaml' in the list of tables 📝 feat(1701796170434_eventPassNftContractType_update_eventPassNftContract/up.sql): create 'eventPassNftContractType' table and add new columns to 'eventPassNftContract' table 📝 fix(1701796170434_eventPassNftContractType_update_eventPassNftContract/down.sql): add down migration for creating 'eventPassNftContractType' table and adding new columns to 'eventPassNftContract' table 📝 chore(4_eventPassNftContract.sql): insert new records with 'normal' type into 'eventPassNftContract' table
    🔧 chore(eventPassNftContractType_update_eventPassNftContract): add new columns to eventPassNftContract table
    📝 docs(eventPassNftContractType_update_eventPassNftContract): update eventPassNftContract table to include new columns for improved functionality
    
    🔧 chore(build-and-e2e-web-tests.yml): set fetch-depth to 0 to improve performance and ensure full repository history is available for checkout
    🔧 chore(ci.yml): refactor CI workflow to simplify and improve readability, remove unused environment variables, and update job names and steps for clarity and consistency

---
+++ hasura/app/migrations/default/1701796170434_eventPassNftContractType_update_eventPassNftContract/up.sql
@@ -0,0 +1,39 @@
+-- Create the eventPassNftContractType table to store the contract types
+CREATE TABLE "public"."eventPassNftContractType" (
+    "value" text NOT NULL,
+    PRIMARY KEY ("value")
+);
+COMMENT ON TABLE "public"."eventPassNftContractType" IS 'Contract types representing the nature of the event pass NFT contract.';
+
+-- Insert default values into eventPassNftContractType
+INSERT INTO "public"."eventPassNftContractType" ("value") VALUES 
+('normal'), 
+('delayed_reveal');
+
+-- Add comments to describe each type in eventPassNftContractType
+COMMENT ON COLUMN "public"."eventPassNftContractType"."value" IS 'Type name for event pass NFT contract.';
+COMMENT ON CONSTRAINT "eventPassNftContractType_pkey" ON "public"."eventPassNftContractType" IS 'normal: Standard event pass NFT contract.
+delayed_reveal: Event pass NFT contract with delayed reveal functionality.';
+
+-- Alter the eventPassNftContract table to include new fields
+ALTER TABLE "public"."eventPassNftContract"
+ADD COLUMN "type" text  NOT NULL DEFAULT 'normal' REFERENCES "eventPassNftContractType"("value"),
+ADD COLUMN "password" text,
+ADD COLUMN "isDelayedRevealed" boolean NOT NULL DEFAULT false,
+ADD COLUMN "isAirdrop" boolean NOT NULL DEFAULT false,
+ADD COLUMN "created_at" timestamptz NOT NULL DEFAULT current_timestamp,
+ADD COLUMN "updated_at" timestamptz NOT NULL DEFAULT current_timestamp;
+
+-- Add comments to the new columns in eventPassNftContract
+COMMENT ON COLUMN "public"."eventPassNftContract"."type" IS 'Type of the event pass NFT contract.';
+COMMENT ON COLUMN "public"."eventPassNftContract"."password" IS 'Password for the delayed reveal functionality. Nullable and only applicable for delayed_reveal type.';
+COMMENT ON COLUMN "public"."eventPassNftContract"."isDelayedRevealed" IS 'Flag indicating whether the delayed reveal functionality is active. Can be set to true only if type is delayed_reveal.';
+COMMENT ON COLUMN "public"."eventPassNftContract"."isAirdrop" IS 'Flag indicating whether the event pass NFT is airdropped.';
+COMMENT ON COLUMN "public"."eventPassNftContract"."created_at" IS 'Timestamp of when the record was created.';
+COMMENT ON COLUMN "public"."eventPassNftContract"."updated_at" IS 'Timestamp of the last update to the record.';
+
+-- Create trigger for updating updated_at in eventPassNftContract
+CREATE TRIGGER set_eventPassNftContract_updated_at 
+BEFORE UPDATE ON "public"."eventPassNftContract" 
+FOR EACH ROW 
+EXECUTE FUNCTION public.set_current_timestamp_updated_at();
