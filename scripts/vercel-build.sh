23da39633345f6c9f1e1de9a0acd8a399ace5abb
sebpalluel <sebpalluel@gmail.com>
2023-12-06 18:21:32 +0000

    🔧 chore(vercel.json): add ignoreCommand to prevent deployments on non-main and non-staging branches and pull requests 🔥 chore(vercel-build.sh): remove vercel-build.sh script as it is no longer needed for deployment configuration

--- scripts/vercel-build.sh
+++
@@ -1,7 +0,0 @@
-if [ "$VERCEL_GIT_COMMIT_REF" = "main" ] || [ "$VERCEL_GIT_COMMIT_REF" = "staging" ] || [[ "$VERCEL_GIT_COMMIT_MESSAGE" =~ "pull request" ]]; then
-  # Proceed with the build
-  exit 1
-else
-  # Skip the build
-  exit 0
-fi
