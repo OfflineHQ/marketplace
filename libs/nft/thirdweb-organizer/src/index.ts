f7838c1b41b7b73318956839c757add7d92f59ae
AlexandreG-tech <141121647+AlexandreG-tech@users.noreply.github.com>
2023-12-07 12:58:43 +0100

    add: e2e basic tests for back-office and ci (#205)
    * add: e2e basic tests for back-office and ci
    
    * fix: jest.config.ts to ignore e2e directory in back-office
    
    * fix: PR comments, i18n and Role_Enum in loadAccountAndRole and switch to english with no exact

--- libs/nft/thirdweb-organizer/src/index.ts
+++ libs/nft/thirdweb-organizer/src/index.ts
@@ -23,7 +23,7 @@ class NftCollection {
     const web3Provider = new ethers.providers.Web3Provider(provider);
     const signer = web3Provider.getSigner();
     this.sdk = ThirdwebSDK.fromSigner(signer, env.NEXT_PUBLIC_CHAIN, {
-      clientId: '***REMOVED***',
+      clientId: env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
       gasless: {
         openzeppelin: {
           relayerUrl: env.NEXT_PUBLIC_OPENZEPPELIN_URL,
