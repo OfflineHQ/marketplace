e6f5f19bc009498f19c0eb065b446b8523e440f7
sebpalluel <sebpalluel@gmail.com>
2023-12-08 18:17:52 +0000

    🐛 fix(EventPassCardFooter.tsx): fix missing import for getEventPassNftFiles function ✨ feat(EventPassCardFooter.tsx): add support for fetching event pass NFT files and logging them for debugging purposes

--- libs/features/back-office/events/src/lib/molecules/EventPassCardFooter/EventPassCardFooter.tsx
+++ libs/features/back-office/events/src/lib/molecules/EventPassCardFooter/EventPassCardFooter.tsx
@@ -1,30 +1,72 @@
+import { getEventPassNftFiles } from '@features/back-office/events-api';
 import { EventFromOrganizerWithPasses } from '@features/back-office/events-types';
+import { GetEventPassOrganizerFolderPath } from '@features/pass-common';
 import {
   BlockchainAddress,
   Button,
+  ButtonSkeleton,
   CardFooter,
   HelperText,
 } from '@ui/components';
 import { useTranslations } from 'next-intl';
+import { Suspense } from 'react';
 
-export interface EventPassCardFooterProps {
+export interface EventPassCardFooterProps
+  extends GetEventPassOrganizerFolderPath {
   eventPass: EventFromOrganizerWithPasses['eventPasses'][0];
 }
 
 function EventPassContractDeployButton({
   eventPass,
+  ...props
 }: EventPassCardFooterProps) {
-  const isDisabled = !eventPass.eventPassPricing?.maxAmount;
-  const isDisabledReasons: string[] = [];
   const t = useTranslations(
     'OrganizerEvents.Sheet.EventPassCard.EventPassCardFooter',
   );
+  const texts = {
+    deployContract: t('deploy-contract'),
+    noPricingSet: t('no-pricing-set'),
+    numFilesDoesNotMatch: t('num-files-does-not-match'),
+  };
+  return (
+    <Suspense fallback={<ButtonSkeleton className="w-full" />}>
+      <EventPassContractDeployButtonContent
+        eventPass={eventPass}
+        texts={texts}
+        {...props}
+      />
+    </Suspense>
+  );
+}
+
+interface EventPassContractDeployButtonContentProps
+  extends EventPassCardFooterProps {
+  texts: {
+    deployContract: string;
+    noPricingSet: string;
+    numFilesDoesNotMatch: string;
+  };
+}
+
+async function EventPassContractDeployButtonContent({
+  eventPass,
+  texts: { deployContract, noPricingSet, numFilesDoesNotMatch },
+  ...props
+}: EventPassContractDeployButtonContentProps) {
+  const isDisabled = !eventPass.eventPassPricing?.maxAmount;
+  const isDisabledReasons: string[] = [];
+
   if (!eventPass.eventPassPricing?.maxAmount)
-    isDisabledReasons.push(t('no-pricing-set'));
+    isDisabledReasons.push(noPricingSet);
+  else {
+    const maxAmount = eventPass.eventPassPricing.maxAmount;
+    const nftFiles = await getEventPassNftFiles(props);
+    console.log('nftFiles', nftFiles?.length, maxAmount);
+  }
   return (
     <div className="w-full flex-col">
       <Button block disabled={isDisabled}>
-        {t('deploy-contract')}
+        {deployContract}
       </Button>
       <HelperText message={isDisabledReasons} variant="warning" />
     </div>
@@ -43,13 +85,16 @@ function EventPassContractDeployed({ eventPass }: EventPassCardFooterProps) {
   );
 }
 
-export function EventPassCardFooter({ eventPass }: EventPassCardFooterProps) {
+export function EventPassCardFooter({
+  eventPass,
+  ...props
+}: EventPassCardFooterProps) {
   return (
     <CardFooter>
       {!eventPass.eventPassNftContract ? (
-        <EventPassContractDeployButton eventPass={eventPass} />
+        <EventPassContractDeployButton eventPass={eventPass} {...props} />
       ) : (
-        <EventPassContractDeployed eventPass={eventPass} />
+        <EventPassContractDeployed eventPass={eventPass} {...props} />
       )}
     </CardFooter>
   );
