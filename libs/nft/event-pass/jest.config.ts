f3e514f70f2a28a3a0645e8887f74216d69cdcdc
sebpalluel <sebpalluel@gmail.com>
2023-12-22 10:07:46 +0100

    🔀 chore(nft-event-pass): rename eventPass library to event-pass for consistency and clarity

--- libs/nft/event-pass/jest.config.ts
+++ libs/nft/event-pass/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'nft-eventPass',
+  displayName: 'nft-event-pass',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/nft/eventPass',
+  coverageDirectory: '../../../coverage/libs/nft/event-pass',
 };
