5621a54209a398e8c2bca9b52a10d9a50dae3bd7
sebpalluel <sebpalluel@gmail.com>
2023-12-22 12:24:14 +0100

    🐛 fix(revealDelayedContract.ts): throw an error if contract address is undefined to prevent unexpected behavior 🐛 fix(nft-thirdweb-api.ts): throw an error if event pass NFT contract is not found for the given address to handle the case when the contract does not exist or is not accessible

--- libs/features/back-office/events/src/lib/actions/revealDelayedContract.ts
+++ libs/features/back-office/events/src/lib/actions/revealDelayedContract.ts
@@ -2,6 +2,9 @@
 import { NftClaimable } from '@nft/thirdweb-admin';
 
 export async function revealDelayedContract(contractAddress: string) {
+  if (!contractAddress) {
+    throw new Error('Contract address is undefined');
+  }
   const sdk = new NftClaimable();
   await sdk.revealDelayedContract(contractAddress);
 }
