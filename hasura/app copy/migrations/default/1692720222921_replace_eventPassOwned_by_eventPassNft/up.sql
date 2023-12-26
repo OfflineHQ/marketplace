a13b541b666ea90de925207adefedd7be14b637d
sebpalluel <sebpalluel@gmail.com>
2023-12-26 12:48:21 +0100

    format + reset hasura

--- hasura/app copy/migrations/default/1692720222921_replace_eventPassOwned_by_eventPassNft/up.sql
+++ hasura/app copy/migrations/default/1692720222921_replace_eventPassOwned_by_eventPassNft/up.sql
@@ -1,8 +1,7 @@
-
 ALTER TABLE "public"."nftTransfer"
-ADD CONSTRAINT nft_transfer_unique_transfer UNIQUE ("contractAddress", "transactionHash", "tokenId");
+  ADD CONSTRAINT nft_transfer_unique_transfer UNIQUE ("contractAddress", "transactionHash", "tokenId");
 
-CREATE TABLE "public"."eventPassNft" (
+CREATE TABLE "public"."eventPassNft"(
   "contractAddress" text NOT NULL,
   "tokenId" bigint NOT NULL,
   "metadata" jsonb NOT NULL,
@@ -25,7 +24,8 @@ CREATE TABLE "public"."eventPassNft" (
 COMMENT ON TABLE "public"."eventPassNft" IS E'The eventPassNft model is designed to consolidate and store the metadata associated with each event pass NFT. It centralizes fixed metadata, enabling the system to retrieve NFT details without frequently querying external APIs. It integrates with the existing nftTransfer model, providing a holistic view of each event pass NFT\'s journey and characteristics within the platform.';
 
 CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
-RETURNS TRIGGER AS $$
+  RETURNS TRIGGER
+  AS $$
 DECLARE
   _new record;
 BEGIN
@@ -33,32 +33,44 @@ BEGIN
   _new."updated_at" = NOW();
   RETURN _new;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
 CREATE TRIGGER "set_public_eventPassNft_updated_at"
-BEFORE UPDATE ON "public"."eventPassNft"
-FOR EACH ROW
-EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+  BEFORE UPDATE ON "public"."eventPassNft"
+  FOR EACH ROW
+  EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
 
-COMMENT ON TRIGGER "set_public_eventPassNft_updated_at" ON "public"."eventPassNft"
-IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+COMMENT ON TRIGGER "set_public_eventPassNft_updated_at" ON "public"."eventPassNft" IS 'trigger to set value of column "updated_at" to current timestamp on row update';
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 COMMENT ON COLUMN "public"."eventPassNft"."contractAddress" IS E'Identifies the smart contract associated with the event pass NFT. This provides a direct link to the NFT\'s origin and behavior on the blockchain.';
+
 COMMENT ON COLUMN "public"."eventPassNft"."tokenId" IS E'The unique identifier of the event pass NFT within its specific collection or contract. This remains constant across various platforms.';
+
 COMMENT ON COLUMN "public"."eventPassNft"."metadata" IS E'The structured metadata parsed from the token URI. This contains a variety of details regarding the event pass NFT.';
+
 COMMENT ON COLUMN "public"."eventPassNft"."error" IS E'Contains any error message related to metadata retrieval, ensuring transparency in the data extraction process.';
+
 COMMENT ON COLUMN "public"."eventPassNft"."tokenUri" IS E'The designated URI for the event pass NFT\'s metadata blob, providing a stable reference for data extraction.';
+
 COMMENT ON COLUMN "public"."eventPassNft"."chainId" IS E'Denotes the specific blockchain or network of the event pass NFT';
+
 COMMENT ON COLUMN "public"."eventPassNft"."eventId" IS E'A reference to the event associated with the event pass NFT';
+
 COMMENT ON COLUMN "public"."eventPassNft"."eventPassId" IS E'Directly relates to a specific Event Pass within the system';
+
 COMMENT ON COLUMN "public"."eventPassNft"."organizerId" IS E'Ties the event pass NFT to a specific organizer within the platform';
+
 COMMENT ON COLUMN "public"."eventPassNft"."currentOwnerAddress" IS E'The address currently holding the event pass NFT, allowing tracking of ownership';
+
 COMMENT ON COLUMN "public"."eventPassNft"."lastNftTransferId" IS E'Reference `id` to the latest `nftTransfer` entry, detailing the most recent transaction for this event pass NFT.';
+
 COMMENT ON COLUMN "public"."eventPassNft"."isRevealed" IS E'Indicates whether the QR code pass for the event pass NFT has been revealed by the owner. This field is essential for tracking and managing the reveal status within the platform.';
 
 ALTER TABLE "public"."eventPassNft"
-ADD CONSTRAINT event_pass_nft_unique_nft UNIQUE ("contractAddress", "tokenId", "chainId");
+  ADD CONSTRAINT event_pass_nft_unique_nft UNIQUE ("contractAddress", "tokenId", "chainId");
 
-DROP table "public"."eventPassOwned";
+DROP TABLE "public"."eventPassOwned";
+
--- hasura/app copy/migrations/default/1702546195591_create_pack_nft_contract_ticket_pk_02/up.sql
+++ hasura/app copy/migrations/default/1702546195591_create_pack_nft_contract_ticket_pk_02/up.sql
@@ -1,6 +1,24 @@
-CREATE TABLE "public"."packNftContract" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "chainId" text NOT NULL, "contractAddress" text NOT NULL, "rewardsPerPack" integer NOT NULL DEFAULT 1, "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "organizerId" text NOT NULL, "eventId" text NOT NULL, "packId" text NOT NULL, "eventPassIds" jsonb NOT NULL, PRIMARY KEY ("id") , UNIQUE ("id"), UNIQUE ("contractAddress", "chainId"));COMMENT ON TABLE "public"."packNftContract" IS E'packNftContract model to manage the NFTs associated with each pack.';
+CREATE TABLE "public"."packNftContract"(
+  "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "chainId" text NOT NULL,
+  "contractAddress" text NOT NULL,
+  "rewardsPerPack" integer NOT NULL DEFAULT 1,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now(),
+  "organizerId" text NOT NULL,
+  "eventId" text NOT NULL,
+  "packId" text NOT NULL,
+  "eventPassIds" jsonb NOT NULL,
+  PRIMARY KEY ("id"),
+  UNIQUE ("id"),
+  UNIQUE ("contractAddress", "chainId")
+);
+
+COMMENT ON TABLE "public"."packNftContract" IS E'packNftContract model to manage the NFTs associated with each pack.';
+
 CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
-RETURNS TRIGGER AS $$
+  RETURNS TRIGGER
+  AS $$
 DECLARE
   _new record;
 BEGIN
@@ -8,20 +26,21 @@ BEGIN
   _new."updated_at" = NOW();
   RETURN _new;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
+
 CREATE TRIGGER "set_public_packNftContract_updated_at"
-BEFORE UPDATE ON "public"."packNftContract"
-FOR EACH ROW
-EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
-COMMENT ON TRIGGER "set_public_packNftContract_updated_at" ON "public"."packNftContract"
-IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+  BEFORE UPDATE ON "public"."packNftContract"
+  FOR EACH ROW
+  EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+
+COMMENT ON TRIGGER "set_public_packNftContract_updated_at" ON "public"."packNftContract" IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-alter table "public"."eventPassNft" add column "packNftContractId" uuid
- null;
+ALTER TABLE "public"."eventPassNft"
+  ADD COLUMN "packNftContractId" uuid NULL;
 
-alter table "public"."eventPassNft"
-  add constraint "eventPassNft_packNftContractId_fkey"
-  foreign key ("packNftContractId")
-  references "public"."packNftContract"
-  ("id") on update restrict on delete restrict;
+ALTER TABLE "public"."eventPassNft"
+  ADD CONSTRAINT "eventPassNft_packNftContractId_fkey" FOREIGN KEY ("packNftContractId") REFERENCES "public"."packNftContract"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
+
