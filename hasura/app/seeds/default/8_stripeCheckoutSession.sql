d272b06ffd2a4bad48ee8228552a4f974a6d4ee1
sebpalluel <sebpalluel@gmail.com>
2023-12-26 13:40:40 +0100

    📝 chore(seeds): add default data for eventPassOrder, eventPassNft, nftTransfer, roleAssignments, follow, packNftContract, pendingOrder, eventPassNftContract, eventParameters, stripeCustomer, stripeCheckoutSession, and order tables
    🔀 Merge branch 'seeds' into 'main'

---
+++ hasura/app/seeds/default/8_stripeCheckoutSession.sql
@@ -0,0 +1,2 @@
+SET check_function_bodies = false;
+INSERT INTO public."stripeCheckoutSession" ("stripeSessionId", "stripeCustomerId", created_at, updated_at, type) VALUES ('cs_test_a17kYy8IpmWsLecscKe5pRQNP5hir8ysWC9sturzdXMfh7Y94gYJIAyePN', 'cus_OnE9GqPxIIPYtB', '2023-10-10 14:12:41.471881+00', '2023-10-10 14:12:41.471881+00', 'event_pass_order');
