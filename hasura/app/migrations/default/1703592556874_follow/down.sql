228253360be56c723e7dec16751555ad9cf04742
sebpalluel <sebpalluel@gmail.com>
2023-12-26 13:39:21 +0100

    📦 chore(follow): add follow table to store follow relationships between accounts and organizers
    🔀 merge(follow): add follow table to tables.yaml to include it in the database schema
    
    📄 docs(follow): add up.sql and down.sql files for follow table migration
    
    The follow table is added to the database schema to store follow relationships between accounts and organizers. It has two columns: accountId (references the unique identifier of the account that is following an organizer) and organizerSlug (represents the unique slug of the organizer being followed). The table also includes a composite primary key constraint to ensure that an account can follow a specific organizer only once. Additionally, an index is created on the organizerSlug column for faster queries.

---
+++ hasura/app/migrations/default/1703592556874_follow/down.sql
@@ -0,0 +1,22 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- -- Create the follow table to store follow relationships between accounts and organizers
+-- CREATE TABLE "public"."follow"(
+--   "accountId" uuid NOT NULL REFERENCES "public"."account"("id"),
+--   "organizerSlug" text NOT NULL,
+--   created_at timestamptz NOT NULL DEFAULT now(),
+--   PRIMARY KEY ("accountId", "organizerSlug")
+-- );
+--
+-- COMMENT ON TABLE "public"."follow" IS 'Stores follow relationships. Each row represents an account following an organizer.';
+--
+-- -- Add comments to describe each column
+-- COMMENT ON COLUMN "public"."follow"."accountId" IS 'References the unique identifier of the account that is following an organizer.';
+--
+-- COMMENT ON COLUMN "public"."follow"."organizerSlug" IS 'Represents the unique slug of the organizer being followed. Slugs are user-friendly identifiers that uniquely identify organizers.';
+--
+-- -- Comment on the primary key constraint
+-- COMMENT ON CONSTRAINT "follow_pkey" ON "public"."follow" IS 'Composite primary key ensuring that an account can follow a specific organizer only once.';
+--
+-- -- Optional: Create an index on the organizerSlug column for faster queries
+-- CREATE INDEX "idx_follow_organizerSlug" ON "public"."follow"("organizerSlug");
