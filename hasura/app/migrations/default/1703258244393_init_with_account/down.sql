b1737b267ea080bcabeca9321f891287461fa0d4
sebpalluel <sebpalluel@gmail.com>
2023-12-26 11:11:31 +0100

    🔧 chore(public_account.yaml): remove unused object and array relationships to simplify table configuration 🔧 chore(0_init_with_account.sql): remove unused migration files to clean up project structure 🔧 chore(1689588908232_create_table_public_eventPassPricing/up.sql): remove unused migration file for creating eventPassPricing table 🔧 chore(1689698037535_create_eventPassOrderSums/up.sql): remove unused migration file for creating eventPassOrderSums table 🔧 chore(1703258244393_init_with_account/up.sql): remove duplicate migration file for initializing account table 🔧 chore(1703258244393_init_with_account/down.sql): remove unused down migration file for initializing account table

---
+++ hasura/app/migrations/default/1703258244393_init_with_account/down.sql
@@ -0,0 +1,37 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- SET check_function_bodies = false;
+--
+-- CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
+-- LANGUAGE plpgsql
+-- AS $$
+-- DECLARE
+--   _new record;
+-- BEGIN
+--   _new := NEW;
+--   _new."updated_at" = NOW();
+--   RETURN _new;
+-- END;
+-- $$;
+--
+-- CREATE TABLE public.account (
+--     id uuid DEFAULT gen_random_uuid() NOT NULL,
+--     address text NOT NULL,
+--     "scwAddress" text,
+--     email text,
+--     phone text,
+--     created_at timestamp with time zone DEFAULT now(),
+--     updated_at timestamp with time zone DEFAULT now()
+-- );
+--
+-- COMMENT ON TABLE public.account IS 'An account can represent a user or a role on an organizer. It stores essential information and is used as the root class for relationships with other tables';
+--
+-- ALTER TABLE ONLY public.account
+--     ADD CONSTRAINT account_address_key UNIQUE (address);
+--
+-- ALTER TABLE ONLY public.account
+--     ADD CONSTRAINT account_pkey PRIMARY KEY (id);
+--
+-- CREATE TRIGGER set_public_account_updated_at BEFORE UPDATE ON public.account FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
+--
+-- COMMENT ON TRIGGER set_public_account_updated_at ON public.account IS 'trigger to set value of column "updated_at" to current timestamp on row update';
