888ab2daecb70a64b262811db23b3170f5d5534a
sebpalluel <sebpalluel@gmail.com>
2023-12-26 13:40:30 +0100

    ✨ feat(seeds): add default account, kyc, passAmount, and passPricing data to seed the database
    📝 Add default account, kyc, passAmount, and passPricing data to seed the database. These seed files contain SQL statements to insert initial data into the respective tables. This will provide a starting point for the application's functionality and allow for testing and development.

---
+++ hasura/app/seeds/default/0_account.sql
@@ -0,0 +1,14 @@
+SET check_function_bodies = FALSE;
+
+INSERT INTO public.account(id, address, "scwAddress", email, phone, created_at, updated_at)
+  VALUES ('ac542c34-1907-451c-94be-5df69a959080', '0x1bBEdB07706728A19c9dB82d3c420670D8040592', NULL, 'googl_user@gmail.com', NULL, '2023-06-20 13:54:30.361524+00', '2023-06-20 14:15:07.524094+00');
+
+INSERT INTO public.account(id, address, "scwAddress", email, phone, created_at, updated_at)
+  VALUES ('679f92d6-a01e-4ab7-93f8-10840d22b0a5', '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D', NULL, 'alpha_user@test.io', NULL, '2023-06-20 14:15:56.908195+00', '2023-06-20 14:15:56.908195+00');
+
+INSERT INTO public.account(id, address, "scwAddress", email, phone, created_at, updated_at)
+  VALUES ('76189546-6368-4325-8aad-220e03837b7e', '0x1B8bD7C7f656290071E52D1aA617D9cB4469BB9F', NULL, '', NULL, '2023-06-20 14:16:43.827815+00', '2023-06-20 14:16:43.827815+00');
+
+INSERT INTO public.account(id, address, "scwAddress", email, phone, created_at, updated_at)
+  VALUES ('9660cf3b-65ec-4ac5-a671-7eac4d93a842', '0x2CDE8fb599b7c656e7594959960DbeA1bC2e15F2', NULL, '', NULL, '2023-07-31 16:00:33.157878+00', '2023-07-31 16:00:33.157878+00');
+
---
+++ hasura/app/seeds/default/1_kyc.sql
@@ -0,0 +1,3 @@
+SET check_function_bodies = false;
+INSERT INTO public.kyc ("applicantId", "externalUserId", "createDate", "reviewStatus", "levelName", updated_at) VALUES ('64d5f7a58f241166d756ba33', '679f92d6-a01e-4ab7-93f8-10840d22b0a5', '2023-09-19 16:08:38+00', 'completed', 'advanced_kyc_level', '2023-09-19 16:09:08.540391+00');
+INSERT INTO public.kyc ("applicantId", "externalUserId", "createDate", "reviewStatus", "levelName", updated_at) VALUES ('74d4f7e59f241066d756ca30', '76189546-6368-4325-8aad-220e03837b7e', '2023-09-19 16:09:31+00', 'pending', 'basic_kyc_level', '2023-09-19 16:10:13.415191+00');
