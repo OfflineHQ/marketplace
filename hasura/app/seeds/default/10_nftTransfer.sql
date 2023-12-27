e67b287ec807fce705f1b89cded0ee05fe5ab9c4
sebpalluel <sebpalluel@gmail.com>
2023-12-27 16:21:04 +0100

    🔧 chore(seeds): update SQL scripts for nftTransfer and eventPassNft tables
    🐛 fix(10_nftTransfer.sql): change column name "packId" to "packAmount" to improve clarity and consistency
    🐛 fix(10_nftTransfer.sql): remove unnecessary columns "id" and "created_at" from the first INSERT statement
    
    🐛 fix(11_eventPassNft.sql): remove unnecessary columns "updated_at" and "error" from all INSERT statements
    🐛 fix(11_eventPassNft.sql): change column name "packId" to "packAmount" in the last three INSERT statements
    ✨ feat(11_eventPassNft.sql): add NULL values for "packId" and "packAmount" in the first three INSERT statements to match the table schema

--- hasura/app/seeds/default/10_nftTransfer.sql
+++ hasura/app/seeds/default/10_nftTransfer.sql
@@ -1,8 +1,8 @@
 SET check_function_bodies = FALSE;
 
-INSERT INTO public."nftTransfer"("contractAddress", "fromAddress", "toAddress", "transactionHash", "chainId", "blockNumber", "eventId", "organizerId", "eventPassId", "tokenId", id, created_at)
-  VALUES ('0xfakecontractaddress1', '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D', '0x1B8bD7C7f656290071E52D1aA617D9cB4469BB9F', '0xfaketransactionhash2', '5', 1441241242534215, 'clizzpvidao620buvxit1ynko', 'clizzky8kap2t0bw7wka9a2id', 'fake-event-pass-2', 1512512512, 'dc062a26-7c0f-4b3e-8561-6867183ff35c', '2023-08-24 08:49:33.867723+01');
-
-INSERT INTO public."nftTransfer"("contractAddress", "fromAddress", "toAddress", "transactionHash", "chainId", "blockNumber", "eventId", "organizerId", "eventPassId", "tokenId", id, created_at)
-  VALUES ('0xfakecontractaddress1', '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D', '0x1B8bD7C7f656290071E52D1aA617D9cB4469BB9F', '0xfaketransactionhash1', '5', 144124124253424, 'clizzpvidao620buvxit1ynko', 'clizzky8kap2t0bw7wka9a2id', 'clj8raobj7g8l0aw3bfw6dny4', 11234514, 'c9898849-1682-49a1-9698-baf506bf2a99', '2023-08-24 08:54:15.930612+01');
+INSERT INTO public."nftTransfer"("contractAddress", "fromAddress", "toAddress", "transactionHash", "chainId", "blockNumber", "eventId", "organizerId", "eventPassId", "packId", "packAmount", "tokenId", "id", "created_at")
+  VALUES ('0xfakecontractaddress1', '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D', '0x1B8bD7C7f656290071E52D1aA617D9cB4469BB9F', '0xfaketransactionhash2', '5', 1441241242534215, 'clizzpvidao620buvxit1ynko', 'clizzky8kap2t0bw7wka9a2id', 'fake-event-pass-2', NULL, NULL, 1512512512, 'dc062a26-7c0f-4b3e-8561-6867183ff35c', '2023-08-24 08:49:33.867723+01'),
+('0xfakecontractaddress1', '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D', '0x1B8bD7C7f656290071E52D1aA617D9cB4469BB9F', '0xfaketransactionhash1', '5', 144124124263424, 'clizzpvidao620buvxit1ynko', 'clizzky8kap2t0bw7wka9a2id', 'clj8raobj7g8l0aw3bfw6dny4', NULL, NULL, 11234514, 'c9898849-1682-49a1-9698-baf506bf2a99', '2023-08-24 08:54:15.930612+01'),
+('0xFakeDelayedReveal', '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D', '0x1B8bD7C7f656290071E52D1aA617D9cB4469BB9F', '0xfaketransactionhash3', '5', 144124124553424, 'clizzpvidao620buvxit1ynko', 'clizzky8kap2t0bw7wka9a2id', 'fakeEventPassDelayedRevealId', NULL, NULL, 0, '67ebd11a-d75f-4c8d-9b6e-7d4014fe41df', '2023-08-24 08:54:15.930612+01'),
+('0xFakePack', '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D', '0x1B8bD7C7f656290071E52D1aA617D9cB4469BB9F', '0xfaketransactionhash4', '5', 144124924553424, 'clizzpvidao620buvxit1ynko', 'clizzky8kap2t0bw7wka9a2id', NULL, '"fakePackId"', 2, 0, '5e8ef4c6-3994-4803-891a-ccac35f67557', '2023-08-24 08:54:15.930612+01');
 
