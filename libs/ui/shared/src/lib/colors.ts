7161060b0241214fa72268b38168ec6115d4967b
sebpalluel <sebpalluel@gmail.com>
2024-01-04 16:16:31 +0100

    🎨 style(PopoverInfo.tsx): update background color of popover info component to improve visibility in dark mode 🎨 style(colors.ts): update text and background hover colors to improve consistency and visibility in dark mode

--- libs/ui/shared/src/lib/colors.ts
+++ libs/ui/shared/src/lib/colors.ts
@@ -1,8 +1,8 @@
 export const textColors = {
-  blue: 'text-blue-600 dark:text-blue-300',
+  blue: 'text-blue-800 dark:text-blue-100',
   dark: 'text-gray-600 dark:text-gray-300',
   red: 'text-red-600 dark:text-red-300',
-  green: 'text-green-600 dark:text-green-300',
+  green: 'text-green-800 dark:text-green-300',
   yellow: 'text-yellow-600 dark:text-yellow-300',
   orange: 'text-orange-600 dark:text-orange-300',
   indigo: 'text-indigo-600 dark:text-indigo-300',
@@ -20,4 +20,16 @@ export const backgroundColors = {
   indigo: 'bg-indigo-100 dark:bg-indigo-900',
   purple: 'bg-purple-100 dark:bg-purple-900',
   pink: 'bg-pink-100 dark:bg-pink-900',
+};
+
+export const backgroundHoverColors = {
+  blue: 'hover:bg-blue-200 dark:hover:bg-blue-800',
+  dark: 'hover:bg-gray-200 dark:hover:bg-gray-600',
+  red: 'hover:bg-red-200 dark:hover:bg-red-800',
+  green: 'hover:bg-green-200 dark:hover:bg-green-800',
+  yellow: 'hover:bg-yellow-200 dark:hover:bg-yellow-800',
+  orange: 'hover:bg-orange-200 dark:hover:bg-orange-800',
+  indigo: 'hover:bg-indigo-200 dark:hover:bg-indigo-800',
+  purple: 'hover:bg-purple-200 dark:hover:bg-purple-800',
+  pink: 'hover:bg-pink-200 dark:hover:bg-pink-800',
 };
