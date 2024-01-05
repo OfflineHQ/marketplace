8debb475e23f1c35f8d9795e46bd93612006c65d
sebpalluel <sebpalluel@gmail.com>
2024-01-05 13:29:39 +0100

    🐛 fix(PassFooterSheet.tsx): remove unnecessary "mt-auto" class from AutoAnimate component 🔥 chore(PassList.stories.tsx): remove unused PassListBoundaryMaxExample component and related code

--- libs/features/organizer/event/src/lib/organisms/PassList/examples.tsx
+++ libs/features/organizer/event/src/lib/organisms/PassList/examples.tsx
@@ -1,23 +1,9 @@
 // examples.tsx
 import { passTotalProps } from '../../molecules/PassTotal/examples';
-import { PassList, PassListProps } from './PassList';
+import { PassListProps } from './PassList';
 
 export const passListProps = {
   passes: passTotalProps['passesData'],
   eventSlug: 'event-slug',
   organizerSlug: 'organizer-slug',
 } satisfies PassListProps;
-
-export const PassListBoundaryMaxExample = ({
-  organizerSlug,
-  eventSlug,
-  ...props
-}: PassListProps) => {
-  // updatePassCart({ organizerSlug, eventSlug, pass: passWithMaxAmountCart });
-  // updatePassCart({
-  //   organizerSlug,
-  //   eventSlug,
-  //   pass: passWithMaxAmountPerUserCart,
-  // });
-  return <PassList {...{ eventSlug, organizerSlug, ...props }} />;
-};
