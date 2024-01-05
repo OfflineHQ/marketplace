2758e47fda06089f84a5c5de65a86e1f4e52baeb
sebpalluel <sebpalluel@gmail.com>
2024-01-05 14:25:19 +0100

    🚀 feat(NoUserCart.stories.tsx): add render prop to meta object to improve storybook documentation 🔨 refactor(NoUserCart.stories.tsx): remove unused import and variable declaration to improve code readability 🔨 refactor(NoUserCart.stories.tsx): remove unused render prop from SectionWithNoUserNoCart story to improve code readability 🔨 refactor(NoUserCart.stories.tsx): remove unused render prop from SectionWithNoUserLoading story to improve code readability 🔨 refactor(NoUserCart.stories.tsx): add args object to SectionWithNoUserLoading story to improve code readability 🔨 refactor(NoUserCart.stories.tsx): update play function in SectionWithNoUserLoading story to improve code readability 🔨 refactor(examples.tsx): update NoUserCartExample function to accept args object instead of passCache parameter to improve code flexibility 🔨 refactor(examples.tsx): remove NoUserCartLoadingExample function as it is no longer used

--- libs/features/cart/src/lib/NoUserCart/examples.tsx
+++ libs/features/cart/src/lib/NoUserCart/examples.tsx
@@ -1,28 +1,14 @@
 import { AppNavLayout } from '@features/app-nav';
 import { WithNoUser } from '@features/app-nav/stories';
-import { PassCache } from '@features/pass-cache';
 import { AuthProvider, NextAuthProvider } from '@next/auth';
-import { EventPassListSkeleton } from '../EventPassList/EventPassList';
 import { NoUserCart } from './NoUserCart';
 
-export function NoUserCartExample(passCache: PassCache) {
+export function NoUserCartExample(args: any) {
   return (
     <NextAuthProvider session={null}>
       <AuthProvider session={null} isConnected={() => false}>
         <AppNavLayout {...WithNoUser.args}>
-          <NoUserCart noCartImage="/empty-cart.svg" />
-        </AppNavLayout>
-      </AuthProvider>
-    </NextAuthProvider>
-  );
-}
-
-export function NoUserCartLoadingExample() {
-  return (
-    <NextAuthProvider session={null}>
-      <AuthProvider session={null} isConnected={() => false}>
-        <AppNavLayout {...WithNoUser.args}>
-          <EventPassListSkeleton />
+          <NoUserCart {...args} noCartImage="/empty-cart.svg" />
         </AppNavLayout>
       </AuthProvider>
     </NextAuthProvider>
