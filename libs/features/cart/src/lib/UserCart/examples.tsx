c1fbc6232117fb4822919da5f041854ebc32eb2c
sebpalluel <sebpalluel@gmail.com>
2024-01-05 18:16:41 +0100

    🔧 fix(NoUserCart.stories.tsx): replace getAllPassesCart function call with allPassesCart variable to improve code readability and performance 🔧 fix(UserCart.stories.tsx): remove unused imports and variables to improve code cleanliness 🔧 fix(UserCart.tsx): remove unused props and spread eventPassesCartProps to improve code readability and maintainability ✨ feat(UserCart.stories.tsx): add new story sections for user with no cart and user with no cart on mobile view ✨ feat(UserCart.stories.tsx): add allPassesCartUser variable to provide all passes cart data for the user with opened cart ✨ feat(UserCart.examples.tsx): add allPassesCartUser variable to provide all passes cart data for the user with opened cart

--- libs/features/cart/src/lib/UserCart/examples.tsx
+++ libs/features/cart/src/lib/UserCart/examples.tsx
@@ -7,24 +7,22 @@ import { Cart } from '@ui/icons';
 import { useTranslations } from 'next-intl';
 import { passOrder1, passOrderWithEvent2 } from '../CartSuccessful/examples';
 import { UserCart, UserCartProps } from './UserCart';
+import { allPassesCart } from '../NoUserCart/examples';
 
 export const userPassPendingOrders1: UserPassPendingOrder[] = [
   passOrder1,
   passOrderWithEvent2,
 ];
 
-export function UserCartExample({
-  userPassPendingOrders,
-}: Pick<UserCartProps, 'userPassPendingOrders'>) {
+export const allPassesCartUser = allPassesCart;
+
+export function UserCartExample(args: UserCartProps) {
   const t = useTranslations('Cart.UserCart');
   return (
     <NextAuthProvider session={null}>
       <AuthProvider session={null} isConnected={() => true}>
         <AppNavLayout {...WithNormalUser.args}>
-          <UserCart
-            noCartImage="/empty-cart.svg"
-            userPassPendingOrders={userPassPendingOrders}
-          >
+          <UserCart {...args} noCartImage="/empty-cart.svg">
             <Button block className="w-full md:w-1/6" icon={<Cart />}>
               {t('finalize-button')}
             </Button>
