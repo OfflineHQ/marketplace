71909359f0111c763471cb5d9fed6845af59ad52
sebpalluel <sebpalluel@gmail.com>
2024-01-08 16:56:02 +0100

    🚀 feat(getEvent.spec.ts): add unit tests for getEvent function to ensure correct behavior for different event statuses 🐛 fix(getEvent.ts): update getEvent function to check event status and return undefined if not published 📝 chore(event.query.gql): add new query GetEventWithParametersMinimal to retrieve event parameters for minimal data retrieval 🌐 chore(en.json): fix typo in OrganizerEventsSection title from "Lastest Events" to "Latest Events" for better readability

--- libs/features/organizer/event-api/src/lib/getEvent.ts
+++ libs/features/organizer/event-api/src/lib/getEvent.ts
@@ -1,7 +1,7 @@
-import { cache } from 'react';
-import { adminSdk } from '@gql/admin/api';
-import type { Locale, Stage } from '@gql/shared/types';
 import env from '@env/server';
+import { adminSdk } from '@gql/admin/api';
+import { EventStatus_Enum, type Locale, type Stage } from '@gql/shared/types';
+import { cache } from 'react';
 
 interface GetEventProps {
   eventSlug: string;
@@ -9,6 +9,16 @@ interface GetEventProps {
 }
 
 export const getEvent = cache(async ({ eventSlug, locale }: GetEventProps) => {
+  const eventMinimal = await adminSdk.GetEventWithParametersMinimal({
+    slug: eventSlug,
+    locale: locale as Locale,
+    stage: env.HYGRAPH_STAGE as Stage,
+  });
+  if (
+    eventMinimal?.event?.eventParameters?.status !== EventStatus_Enum.Published
+  ) {
+    return;
+  }
   const data = await adminSdk.GetEvent({
     slug: eventSlug,
     locale: locale as Locale,
