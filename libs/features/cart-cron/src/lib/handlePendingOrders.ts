6a79f5cca078f904b174b21c2a6246bc732bc2ac
sebpalluel <sebpalluel@gmail.com>
2024-01-12 17:06:02 +0100

    🐛 fix(handlePendingOrders.spec.ts): adjust import paths and mock response to match changes in handlePendingOrders.ts ✨ feat(handlePendingOrders.ts): add functions to filter orders to delete, check if order is past deletion time, and prepare accounts to notify 🔧 chore(handlePendingOrders.ts): adjust import path for UTCDate from date-fns/utc 🔧 chore(handlePendingOrders.ts): adjust import path for NextResponse from next/server

--- libs/features/cart-cron/src/lib/handlePendingOrders.ts
+++ libs/features/cart-cron/src/lib/handlePendingOrders.ts
@@ -1,66 +1,123 @@
+import { UTCDate } from '@date-fns/utc';
 import { adminSdk } from '@gql/admin/api';
 import { NextResponse } from 'next/server';
+
+type Order = Awaited<
+  ReturnType<typeof adminSdk.GetPendingOrders>
+>['pendingOrder'][0];
+
+interface AccountNotification {
+  address: string;
+  email?: string | null;
+  eventPassIds: string[];
+  packIds: string[];
+}
+
+export async function filterOrdersToDelete(
+  orders: Order[],
+  currentTime: number,
+): Promise<string[]> {
+  const ordersToDelete: string[] = [];
+
+  // TODO : do the same for packs with the packId and packParameters
+  // extract all eventPassIds from the orders and remove duplicates
+  const eventPassIds = Array.from(
+    new Set(
+      orders
+        .filter((order) => order.eventPassId)
+        .map((order) => order.eventPassId as string),
+    ),
+  );
+  const eventPassWithParametersResponse =
+    await adminSdk.GetEventParametersFromEventPassIds({ eventPassIds });
+  const eventPassIdIsSaleOngoing =
+    eventPassWithParametersResponse.eventPasses.reduce(
+      (acc, eventPass) => {
+        acc[eventPass.id] = !!eventPass.event?.eventParameters?.isSaleOngoing;
+        return acc;
+      },
+      {} as Record<string, boolean>,
+    );
+  for (const order of orders) {
+    const isSaleOngoing = order.eventPassId
+      ? eventPassIdIsSaleOngoing[order.eventPassId]
+      : true; // TODO: change "true" for "packIdIsSaleOngoing[order.packId]" when packs are implemented
+    if (!isSaleOngoing || isOrderPastDeletionTime(order, currentTime)) {
+      ordersToDelete.push(order.id);
+    }
+  }
+
+  return ordersToDelete;
+}
+
+export function isOrderPastDeletionTime(
+  order: Order,
+  currentTime: number,
+): boolean {
+  const orderCreationTime = new UTCDate(order.created_at).getTime() / 1000; // Convert to UNIX timestamp
+  const deletionTime =
+    orderCreationTime +
+    (order.passAmount?.timeBeforeDelete ||
+      order.packAmount?.timeBeforeDelete ||
+      14400); // Default to 4 hours
+  return currentTime >= deletionTime;
+}
+
+export function prepareAccountsToNotify(
+  orders: Order[],
+  ordersToDelete: string[],
+): Record<string, AccountNotification> {
+  const accountsToNotify: Record<string, AccountNotification> = {};
+
+  for (const orderId of ordersToDelete) {
+    const order = orders.find((o) => o.id === orderId);
+    if (order && order.account?.address) {
+      const account = order.account;
+
+      if (!accountsToNotify[account.address]) {
+        accountsToNotify[account.address] = {
+          address: account.address,
+          email: account.email || null,
+          eventPassIds: order.eventPassId ? [order.eventPassId] : [],
+          packIds: order.packId ? [order.packId] : [],
+        };
+      } else {
+        if (order.eventPassId) {
+          accountsToNotify[account.address].eventPassIds.push(
+            order.eventPassId,
+          );
+        }
+        if (order.packId) {
+          accountsToNotify[account.address].packIds.push(order.packId);
+        }
+      }
+    }
+  }
+
+  return accountsToNotify;
+}
 
 export default async function handler() {
   try {
     // Fetch all pending orders
+    const currentTime = Math.floor(UTCDate.now() / 1000); // Current UNIX timestamp
     const response = await adminSdk.GetPendingOrders();
     const pendingOrders = response.pendingOrder;
 
-    // Filter out the orders that need to be deleted
-    const currentTime = Math.floor(Date.now() / 1000); // Current UNIX timestamp
-
-    const ordersToDelete: string[] = [];
-    const accountsToNotify: Record<
-      string,
-      {
-        address: string;
-        email: string | null;
-        eventPassIds: string[];
-        packIds: string[];
-      }
-    > = {};
-
-    for (const order of pendingOrders) {
-      const orderCreationTime = new Date(order.created_at).getTime() / 1000; // Convert to UNIX timestamp
-
-      const deletionTime =
-        orderCreationTime +
-        (order?.passAmount?.timeBeforeDelete ||
-          order?.packAmount?.timeBeforeDelete ||
-          14400); // default to 4 hours
-      if (currentTime >= deletionTime) {
-        ordersToDelete.push(order.id);
-        if (order.account?.address) {
-          if (!accountsToNotify[order.account.address]) {
-            accountsToNotify[order.account.address] = {
-              address: order.account.address,
-              email: order.account.email || null,
-              eventPassIds: order.eventPassId ? [order.eventPassId] : [],
-              packIds: order.packId ? [order.packId] : [],
-            };
-          } else {
-            if (order.eventPassId) {
-              accountsToNotify[order.account.address].eventPassIds.push(
-                order.eventPassId,
-              );
-            }
-            if (order.packId) {
-              accountsToNotify[order.account.address].packIds.push(
-                order.packId,
-              );
-            }
-          }
-        }
-      }
-    }
+    const ordersToDelete = await filterOrdersToDelete(
+      pendingOrders,
+      currentTime,
+    );
+    // Return the accounts to be notified along with the orders deleted
+    const accountsToNotify = prepareAccountsToNotify(
+      pendingOrders,
+      ordersToDelete,
+    );
+    // TODO : send email to accountsToNotify if authorized in settings and add notification to inform the user has been deleted and why (sale ended or time expired)
 
     // Delete the expired orders
     await adminSdk.DeletePendingOrders({ ids: ordersToDelete });
 
-    // TODO : send email to accountsToNotify if authorized in settings and add notification
-
-    // Return the accounts to be notified along with the orders deleted
     const res = {
       accountsToNotify: Object.values(accountsToNotify),
       ordersToDelete,
