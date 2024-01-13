1d9b989e83dcc442628b5cf5d659a5468d6efa6d
sebpalluel <sebpalluel@gmail.com>
2024-01-13 13:57:55 +0100

    🐛 fix(EventSaleDates/examples.tsx): correct sale dates in eventParametersSaleOngoing and eventParametersSaleEnded to reflect accurate time and timezone information

--- libs/features/organizer/event/src/lib/molecules/EventSaleDates/examples.tsx
+++ libs/features/organizer/event/src/lib/molecules/EventSaleDates/examples.tsx
@@ -7,13 +7,13 @@ export const eventParametersSaleNotStarted = {
 } satisfies EventParameters;
 
 export const eventParametersSaleOngoing = {
-  dateSaleStart: '2023-06-05T04:00:00',
-  dateSaleEnd: '2023-06-05T15:00:00',
+  dateSaleStart: '2023-06-05T03:00:00',
+  dateSaleEnd: '2023-06-05T17:00:00',
   timezone: 'America/New_York',
 } satisfies EventParameters;
 
 export const eventParametersSaleEnded = {
   dateSaleStart: '2023-06-04T04:00:00',
-  dateSaleEnd: '2023-06-05T00:00:00',
+  dateSaleEnd: '2023-06-04T12:00:00',
   timezone: 'Europe/Paris',
 } satisfies EventParameters;
