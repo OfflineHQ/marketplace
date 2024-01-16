a6e8d422b9ce4bb62de43a50fdd03de360e1e7f9
sebpalluel <sebpalluel@gmail.com>
2024-01-16 18:33:44 +0100

    🐛 fix(deployCollectionWrapper.ts): fix import statement for env variable to use default import instead of named import to match the module export 🔧 chore(package.json): update 'all:e2e' script to run e2e tests sequentially instead of in parallel to avoid race conditions

--- libs/features/back-office/events/src/lib/actions/deployCollectionWrapper.ts
+++ libs/features/back-office/events/src/lib/actions/deployCollectionWrapper.ts
@@ -1,5 +1,5 @@
 'use client';
-import { env } from '@env/client';
+import env from '@env/client';
 import { EventPass } from '@features/back-office/events-types';
 import { EventPassNftContractType_Enum } from '@gql/shared/types';
 import { NftCollection } from '@nft/thirdweb-organizer';
