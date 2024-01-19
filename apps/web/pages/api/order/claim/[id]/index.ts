35c2a106ce1ed633872e13571b9698b0eb58383d
Alexandre Gouasmi <agouasmi@offline.live>
2024-01-19 16:01:17 +0100

    pages/api: change claim order route to use nextApi type instead

--- apps/web/pages/api/order/claim/[id]/index.ts
+++ apps/web/pages/api/order/claim/[id]/index.ts
@@ -1,22 +1,27 @@
 import { adminSdk } from '@gql/admin/api';
 import { NftClaimable } from '@nft/thirdweb-admin';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export const maxDuration = 300;
 
-// This route has been moved to pages/api regarding a Thirdweb and NextJS 14 error, it should be moved back into app/api when the error will be fix https://github.com/AlexandreG-tech/Server-Action-Error
-export default async function handler(req: Request, { params: { id } }) {
-  const order = (await adminSdk.GetOrderFromId({ id })).order_by_pk;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { id } = req.query;
+  const order = (await adminSdk.GetOrderFromId({ id: id as string }))
+    .order_by_pk;
   const nft = new NftClaimable();
 
   if (!order) {
-    return Response.error();
+    return res.status(400).json({ error: 'Order not found' });
   }
 
   try {
     await nft.claimOrder(order);
-    return Response.json({ status: 'success', id });
+    return res.status(200).json({ status: 'success', id });
   } catch (e) {
     console.error(e);
-    return Response.error();
+    return res.status(500).json({ error: 'An error occurred' });
   }
 }
