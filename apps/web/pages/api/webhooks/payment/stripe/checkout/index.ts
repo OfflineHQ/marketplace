eece8a8ffeb4bf7b606386f772359b233438ff60
Alexandre Gouasmi <agouasmi@offline.live>
2024-01-24 16:59:31 +0100

    stripe-webhook-route: duration fix to 30 seconds

--- apps/web/pages/api/webhooks/payment/stripe/checkout/index.ts
+++ apps/web/pages/api/webhooks/payment/stripe/checkout/index.ts
@@ -6,7 +6,7 @@ export const config = {
   api: {
     bodyParser: false,
   },
-  maxDuration: 300,
+  maxDuration: 30,
 };
 
 // This route has been moved to pages/api regarding a Thirdweb and NextJS 14 error, it should be moved back into app/api when the error will be fix https://github.com/AlexandreG-tech/Server-Action-Error
