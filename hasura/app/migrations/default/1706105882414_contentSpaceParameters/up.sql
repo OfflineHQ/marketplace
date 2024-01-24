9e6f7dc172ea93f51bf95e3859a3ea8941f8e1eb
sebpalluel <sebpalluel@gmail.com>
2024-01-24 15:19:45 +0100

    📦 chore(contentSpaceParameters): add contentSpaceParameters table to the database
    📦 chore(contentSpaceStatus): add contentSpaceStatus table to the database as an enum
    
    📦 chore(tables): include contentSpaceParameters and contentSpaceStatus tables in tables.yaml
    
    📦 chore(contentSpaceParameters): add up and down migrations for contentSpaceParameters table
    
    🔧 chore(public_contentSpaceParameters.yaml): add remote relationship to contentSpace table to improve data fetching efficiency
    🔧 chore(remote_schemas.yaml): add relationship to contentSpaceParameters table in ContentSpace type to enable fetching related data
    🔧 chore(ContentSpaceFields.fragment.gql): add fragment for ContentSpace fields to improve code reusability
    🔧 chore(contentSpace.query.gql): add queries to fetch content space data based on organizer ID and slug
    🔧 chore(contentSpaceParameters.mutation.gql): add mutation to create content space parameters
    🔧 chore(eventParameters.query.gql): add query to fetch event parameters based on event ID
    🔧 chore(eventParameters.query.gql): add comment to investigate issue with stage DRAFT on production

---
+++ hasura/app/migrations/default/1706105882414_contentSpaceParameters/up.sql
@@ -0,0 +1,31 @@
+-- Create contentSpaceStatus table
+CREATE TABLE public."contentSpaceStatus"(
+  value text NOT NULL
+);
+
+ALTER TABLE ONLY public."contentSpaceStatus"
+  ADD CONSTRAINT "contentSpaceStatus_pkey" PRIMARY KEY (value);
+
+INSERT INTO public."contentSpaceStatus"(value)
+  VALUES ('DRAFT'),
+('PUBLISHED');
+
+-- Create contentSpaceParameters table
+CREATE TABLE "public"."contentSpaceParameters" (
+  "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  "contentSpaceId" text NOT NULL UNIQUE,
+  "status" text DEFAULT 'DRAFT' REFERENCES "public"."contentSpaceStatus"("value") ON UPDATE NO action ON DELETE NO action,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now(),
+  "organizerId" text NOT NULL
+);
+
+COMMENT ON TABLE "public"."contentSpaceParameters" IS E'The contentSpaceParameters model is designed to define properties specifically for content spaces. This table includes essential details like the contentSpaceId, which links to the specific content space. By centralizing this information, our system can effectively manage and control parameters tied to each content space, enhancing functionality and flexibility.';
+
+COMMENT ON COLUMN "public"."contentSpaceParameters"."contentSpaceId" IS E'It stores the identifier for the content space. This ID is crucial for managing and linking specific parameters to each content space, ensuring accurate and efficient handling of content space-related data.';
+
+-- Create trigger for contentSpaceParameters
+CREATE TRIGGER set_contentSpaceParameters_updated_at
+  BEFORE UPDATE ON "public"."contentSpaceParameters"
+  FOR EACH ROW
+  EXECUTE FUNCTION public.set_current_timestamp_updated_at();
