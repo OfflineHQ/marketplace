78496e65c88d676fa9c95732de767e0d32d9f010
sebpalluel <sebpalluel@gmail.com>
2024-02-22 19:57:33 +0000

    setup unlock next app

---
+++ apps/unlock/.storybook/preview.tsx
@@ -0,0 +1,51 @@
+import { Preview } from '@storybook/react';
+import { initialize, mswLoader } from 'msw-storybook-addon';
+import {
+  DarkModeDecorator,
+  I18nextStoryDecorator,
+  LocalStorageResetDecorator,
+  mockDateDecorator,
+} from '../../../storybook/storybook.nextjs.decorators';
+import { parameters } from '../../../storybook/storybook.preview.base';
+import '../styles/globals.css';
+
+// Initialize MSW
+initialize();
+window.STORYBOOK_ENV = true;
+// Create a global variable called locale in storybook
+// and add a dropdown in the toolbar to change your locale
+export const globalTypes = {
+  locale: {
+    defaultValue: 'en',
+    name: 'Locale',
+    description: 'Internationalization locale',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', right: '🇺🇸', title: 'English' },
+        { value: 'fr', right: '🇫🇷', title: 'Francais' },
+      ],
+      showName: true,
+    },
+  },
+};
+
+document.body.classList.add('font-sans');
+
+const preview: Preview = {
+  parameters: {
+    ...parameters,
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  decorators: [
+    mockDateDecorator,
+    DarkModeDecorator,
+    I18nextStoryDecorator,
+    LocalStorageResetDecorator,
+  ],
+  // Provide the MSW addon loader globally
+  loaders: [mswLoader],
+};
+export default preview;
---
+++ apps/unlock/app/[locale]/not-found.tsx
@@ -0,0 +1,5 @@
+import { NotFound as UINotFound } from '@features/navigation';
+
+export default function NotFound() {
+  return <UINotFound />;
+}
