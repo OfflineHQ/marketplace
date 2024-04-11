e1f750e758f1ae4700457e1604039d9d7891a833
sebpalluel <sebpalluel@gmail.com>
2024-04-11 11:42:47 +0100

    ✨ Add new font files for HelveticaNeue, Inter, OpenSans, and Roboto families 🔥 Remove OpenSans VariableFont files in favor of WOFF2 formats
    ✨ (context.tsx, types.ts, provider.tsx): enhance font loading mechanism and add Helvetica Neue support
    
    - Refactor font loading in `context.tsx` to support dynamic loading of multiple font families and styles, improving flexibility and code reuse.
    - Introduce `Helvetica Neue` font family in `types.ts`, expanding the available font options.
    - Modify `ReactQueryProviders` in `provider.tsx` to conditionally render React Query Devtools based on a new `devTools` prop, enhancing configurability for development environments.

--- libs/next/iframe/src/lib/context.tsx
+++ libs/next/iframe/src/lib/context.tsx
@@ -58,42 +58,196 @@ export const IFrameProvider: React.FC<IFrameProviderProps> = ({ children }) => {
     setIFrameParent((window as any).parentIFrame);
   };
 
+  function loadFontFamily(
+    fontFamily: string,
+    fontVariations: {
+      path: string;
+      descriptors?: FontFaceDescriptors;
+    }[],
+  ) {
+    const fontPromises = fontVariations.map((variation) =>
+      new FontFace(
+        fontFamily,
+        `url(${variation.path})`,
+        variation.descriptors,
+      ).load(),
+    );
+
+    Promise.all(fontPromises)
+      .then((fonts) => {
+        fonts.forEach((font) => {
+          // @ts-expect-error
+          document.fonts.add(font);
+        });
+        setUiReady(true);
+      })
+      .catch((error) => {
+        console.error(`Error loading ${fontFamily} fonts`, error);
+      });
+    return fontFamily;
+  }
+
   function getFont(fontFamily: FontFamily) {
     switch (fontFamily) {
       case FontFamily.OPEN_SANS:
-        // Load local Open Sans font files
-        Promise.all([
-          new FontFace(
-            fontFamily,
-            `url(/fonts/OpenSans-VariableFont_wdth,wght.ttf)`,
-            { style: 'normal' },
-          ).load(),
-          new FontFace(
-            fontFamily,
-            `url(/fonts/OpenSans-Italic-VariableFont_wdth,wght.ttf)`,
-            { style: 'italic' },
-          ).load(),
-        ])
-          .then((fonts) => {
-            fonts.forEach((font) => {
-              // @ts-expect-error
-              document.fonts.add(font);
-            });
-            setUiReady(true);
-          })
-          .catch((error) => {
-            console.error('Error loading Open Sans fonts', error);
-          });
-
-        return fontFamily; // Return the font family name to be used in CSS
+        return loadFontFamily(fontFamily, [
+          {
+            path: '/fonts/OpenSans-VariableFont_wdth,wght.woff2',
+            descriptors: {
+              style: 'normal',
+            },
+          },
+          {
+            path: '/fonts/OpenSans-Italic-VariableFont_wdth,wght.woff2',
+            descriptors: {
+              style: 'italic',
+            },
+          },
+        ]);
       case FontFamily.ROBOTO:
-        // Similar approach for Roboto or any other local fonts
-        break;
+        return loadFontFamily('Roboto', [
+          {
+            path: '/fonts/Roboto-Black.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: '900',
+            },
+          },
+          {
+            path: '/fonts/Roboto-BlackItalic.woff2',
+            descriptors: {
+              style: 'italic',
+              weight: '900',
+            },
+          },
+          {
+            path: '/fonts/Roboto-Bold.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: 'bold',
+            },
+          },
+          {
+            path: '/fonts/Roboto-BoldItalic.woff2',
+            descriptors: {
+              style: 'italic',
+              weight: 'bold',
+            },
+          },
+          {
+            path: '/fonts/Roboto-Italic.woff2',
+            descriptors: {
+              style: 'italic',
+              weight: 'normal',
+            },
+          },
+          {
+            path: '/fonts/Roboto-Light.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: '300',
+            },
+          },
+          {
+            path: '/fonts/Roboto-LightItalic.woff2',
+            descriptors: {
+              style: 'italic',
+              weight: '300',
+            },
+          },
+          {
+            path: '/fonts/Roboto-Medium.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: '500',
+            },
+          },
+          {
+            path: '/fonts/Roboto-MediumItalic.woff2',
+            descriptors: {
+              style: 'italic',
+              weight: '500',
+            },
+          },
+          {
+            path: '/fonts/Roboto-Regular.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: 'normal',
+            },
+          },
+          {
+            path: '/fonts/Roboto-Thin.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: '100',
+            },
+          },
+          {
+            path: '/fonts/Roboto-ThinItalic.woff2',
+            descriptors: {
+              style: 'italic',
+              weight: '100',
+            },
+          },
+        ]);
       case FontFamily.INTER:
-        // INTER is already used by default, no action needed
-        return '';
-      // Add more cases as needed
+        return loadFontFamily(fontFamily, [
+          {
+            path: '/fonts/Inter-VariableFont_slnt,wght.woff2',
+          },
+        ]);
+      case FontFamily.HELVETICA_NEUE:
+        return loadFontFamily('Helvetica Neue', [
+          {
+            path: '/fonts/HelveticaNeue.woff2',
+            descriptors: {
+              style: 'normal',
+            },
+          },
+          {
+            path: '/fonts/HelveticaNeue-Bold.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: 'bold',
+            },
+          },
+          {
+            path: '/fonts/HelveticaNeue-Italic.woff2',
+            descriptors: {
+              style: 'italic',
+            },
+          },
+          {
+            path: '/fonts/HelveticaNeue-Light.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: '300',
+            },
+          },
+          {
+            path: '/fonts/HelveticaNeue-Medium.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: '500',
+            },
+          },
+          {
+            path: '/fonts/HelveticaNeue-Thin.woff2',
+            descriptors: {
+              style: 'normal',
+              weight: '100',
+            },
+          },
+          {
+            path: '/fonts/HelveticaNeueRegular.woff2',
+            descriptors: {
+              style: 'normal',
+            },
+          },
+        ]);
       default:
+        console.error(`Font family ${fontFamily} not found`);
         return '';
     }
   }
@@ -113,14 +267,8 @@ export const IFrameProvider: React.FC<IFrameProviderProps> = ({ children }) => {
         // eslint-disable-next-line no-case-declarations
         const { cssVariables, classes, fontFamily } =
           value as ReceiveMessageValues[ReceiveMessageType.UPDATE_CSS_VARIABLES_AND_CLASSES];
-        console.log('fontFamily', fontFamily);
-        if (fontFamily !== FontFamily.INTER) {
-          const font = getFont(fontFamily);
-          if (font) {
-            cssVariables['--font-family'] =
-              `${font}, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`;
-          }
-        }
+        cssVariables['--font-family'] =
+          `${getFont(fontFamily)}, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`;
         setCssVariables(cssVariables);
         setClasses(`${classes} font-${fontFamily}`);
         break;
